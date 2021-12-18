b326837e00bd346f06517014c1e7364487d137e9
Peter Watts <pwattsmail@gmail.com>
2021-12-18 23:14:39 +1100

    experiment with generating response models

--- src/api/endpoints/collections.ts
+++ src/api/endpoints/collections.ts
@@ -4,6 +4,42 @@ import Joi from "joi";
 import { logger } from "@/common/logger";
 import * as queries from "@/entities/collections";
 
+const getCollectionsResponse = Joi.object({
+  collections: Joi.array().items(Joi.object({
+      collection: Joi.object({
+        id: Joi.string(),
+        name: Joi.string(),
+        description: Joi.string(),
+        image: Joi.string(),
+      }),
+      royalties: Joi.object({
+        recipient: Joi.string().optional(),
+        bps: Joi.number(),
+      }),
+      set: Joi.object({
+        compositionId: Joi.string().optional(),
+        tokenCount: Joi.string(),
+        onSaleCount: Joi.string(),
+        uniqueOwnersCount: Joi.string(),
+        sampleImages: Joi.array().items(Joi.string()),
+        market: Joi.object({
+          floorSell: {
+            hash: Joi.string(),
+            value: Joi.string(),
+            maker: Joi.string(),
+            validFrom: Joi.number(),
+          },
+          topBuy: Joi.object({
+            hash: Joi.string(),
+            value: Joi.string(),
+            maker: Joi.string(),
+            validFrom: Joi.number(),
+          }),
+        }),
+      })
+  }))
+}).label('getCollectionsResponse');
+
 export const getCollectionsOptions: RouteOptions = {
   description: "Get collections",
   tags: ["api"],
@@ -20,6 +56,17 @@ export const getCollectionsOptions: RouteOptions = {
       offset: Joi.number().integer().min(0).default(0),
       limit: Joi.number().integer().min(1).max(20).default(20),
     }),
+  },
+  //response: {schema: getCollectionsResponse}, // this format validates, and gives 500 error with no explanation
+  plugins: {
+    'hapi-swagger': {
+        responses: {
+            200: {
+                description: 'Successful',
+                schema: getCollectionsResponse
+            }
+        }
+    }
   },
   handler: async (request: Request) => {
     const query = request.query as any;
