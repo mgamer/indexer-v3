83411453463f943df1f1425072206acc3e62eefb
George Roman <george.roman.99@gmail.com>
2021-12-31 15:57:39 +0200

    Misc fix

--- src/api/types.ts
+++ src/api/types.ts
@@ -5,7 +5,7 @@ import Joi from "joi";
 export const tokenFormat = Joi.object({
   contract: Joi.string(),
   kind: Joi.string(),
-  name: Joi.string().allow(""),
+  name: Joi.string().allow("", null),
   image: Joi.string().allow(""),
   tokenId: Joi.string(),
   collection: Joi.object({
