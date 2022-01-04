f6dbccabaf3b68fa9338a3e081826b3ca93db4e8
George Roman <george.roman.99@gmail.com>
2022-01-04 14:20:00 +0100

    Setup versioning for logging purposes

--- src/common/tracer.ts
+++ src/common/tracer.ts
@@ -4,7 +4,7 @@ import { config } from "@/config/index";
 
 if (process.env.DATADOG_AGENT_URL) {
   const network = config.chainId === 1 ? "mainnet" : "rinkeby";
-  const service = `indexer-v3-${network}`;
+  const service = `indexer-${config.version}-${network}`;
 
   // TODO: Disable Redis tracing since that generates
   // a lot of traces which for now are not relevant
