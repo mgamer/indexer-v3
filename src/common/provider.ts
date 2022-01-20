b0195970fe2ff160b7532d4a17858aec61748ab5
George Roman <george.roman.99@gmail.com>
2022-01-20 12:06:12 +0100

    Refactor

--- src/common/provider.ts
+++ src/common/provider.ts
@@ -1,8 +1,4 @@
-import {
-  CloudflareProvider,
-  AlchemyProvider,
-  StaticJsonRpcProvider,
-} from "@ethersproject/providers";
+import { StaticJsonRpcProvider } from "@ethersproject/providers";
 import Arweave from "arweave";
 
 import { config } from "@/config/index";
@@ -10,7 +6,6 @@ import { config } from "@/config/index";
 // Optimizations:
 // - use http everywhere since websockets are much more expensive
 // - use static providers to avoid redundant `eth_chainId` calls
-// - use free RPC endpoints for non-critical queries (eg. Cloudflare, Etherscan)
 export const baseProvider = new StaticJsonRpcProvider(
   config.baseNetworkHttpUrl
 );
