038702ca5af8c68a127e25b612a0d1e342489410
George Roman <george.roman.99@gmail.com>
2022-01-31 17:34:49 +0100

    Tweak database connection settings

--- src/common/db.ts
+++ src/common/db.ts
@@ -6,7 +6,8 @@ export const pgp = PgPromise();
 export const db = pgp({
   connectionString: config.databaseUrl,
   keepAlive: true,
-  connectionTimeoutMillis: 10 * 1000,
+  max: 20,
+  connectionTimeoutMillis: 60 * 1000,
   query_timeout: 60 * 1000,
   statement_timeout: 60 * 1000,
 });
