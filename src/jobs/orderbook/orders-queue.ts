76020f35cb550b7c18b85d89fa9ba44ed828a3bc
George Roman <george.roman.99@gmail.com>
2022-03-02 21:51:02 +0200

    Debug

--- src/jobs/orderbook/orders-queue.ts
+++ src/jobs/orderbook/orders-queue.ts
@@ -33,10 +33,11 @@ if (config.doBackgroundWork) {
       try {
         switch (kind) {
           case "wyvern-v2.3": {
-            await orders.wyvernV23.save(
+            const result = await orders.wyvernV23.save(
               [info as orders.wyvernV23.OrderInfo],
               relayToArweave
             );
+            logger.info("debug", JSON.stringify(result));
             break;
           }
         }
