ac677c87ecd6fd47017e5b193bf6c4bafae6f5cb
George Roman <george.roman.99@gmail.com>
2022-03-18 10:24:48 +0200

    fix: increase metadata fetch timeout

--- src/jobs/metadata-index/fetch-queue.ts
+++ src/jobs/metadata-index/fetch-queue.ts
@@ -23,7 +23,7 @@ export const queue = new Queue(QUEUE_NAME, {
     },
     removeOnComplete: 10000,
     removeOnFail: 10000,
-    timeout: 2 * 60 * 1000,
+    timeout: 3 * 60 * 1000,
   },
 });
 new QueueScheduler(QUEUE_NAME, { connection: redis.duplicate() });
@@ -141,7 +141,7 @@ if (config.doBackgroundWork) {
 
         if (url) {
           const metadataResult = await axios
-            .get(url, { timeout: 2 * 60 * 1000 })
+            .get(url, { timeout: 3 * 60 * 1000 })
             .then(({ data }) => data);
 
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
