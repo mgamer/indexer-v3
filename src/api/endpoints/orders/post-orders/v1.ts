d424b48de257965d8711a0072a2338e2a335502c
George Roman <george.roman.99@gmail.com>
2022-03-25 17:18:08 +0200

    feat: temporary log keys when posting orders in batch

--- src/api/endpoints/orders/post-orders/v1.ts
+++ src/api/endpoints/orders/post-orders/v1.ts
@@ -23,6 +23,7 @@ export const postOrdersV1Options: RouteOptions = {
         Joi.object({
           kind: Joi.string().lowercase().valid("wyvern-v2.3").required(),
           data: Joi.object().required(),
+          logKey: Joi.string(),
         })
       ),
     }),
@@ -36,12 +37,13 @@ export const postOrdersV1Options: RouteOptions = {
       logger.info(`post-orders-${version}-handler`, `Got ${orders.length} orders`);
 
       const wyvernV23OrderInfos: wyvernV23.OrderInfo[] = [];
-      for (const { kind, data } of orders) {
+      for (const { kind, data, logKey } of orders) {
         switch (kind) {
           case "wyvern-v2.3": {
             wyvernV23OrderInfos.push({
               orderParams: data,
               metadata: {},
+              logKey,
             });
 
             break;
@@ -54,6 +56,7 @@ export const postOrdersV1Options: RouteOptions = {
           kind: "wyvern-v2.3",
           info: orderInfo,
           relayToArweave: true,
+          logKey,
         }))
       );
 
