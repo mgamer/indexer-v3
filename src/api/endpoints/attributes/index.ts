3cc7c61cda2ccdf690062c01c51f775e9190b966
George Roman <george.roman.99@gmail.com>
2022-03-25 11:54:45 +0200

    feat: add attributes static api

--- src/api/endpoints/attributes/index.ts
+++ src/api/endpoints/attributes/index.ts
@@ -2,3 +2,4 @@ export * from "@/api/endpoints/attributes/get-attributes/v1";
 export * from "@/api/endpoints/attributes/get-collection-attributes/v1";
 export * from "@/api/endpoints/attributes/get-attributes-all/v1";
 export * from "@/api/endpoints/attributes/get-attributes-explore/v1";
+export * from "@/api/endpoints/attributes/get-attributes-static/v1";
