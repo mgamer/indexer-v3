e7ec960101bcbdeca0ebe5100f355f2a7fa137a8
Idan Peleg <ipeleg@hotmail.com>
2022-04-01 22:34:01 -0400

    feat: don't mix collections when refreshing data (#254)

--- src/jobs/metadata-index/fetch-queue.ts
+++ src/jobs/metadata-index/fetch-queue.ts
@@ -8,7 +8,7 @@ import { logger } from "@/common/logger";
 import { redis } from "@/common/redis";
 import { fromBuffer, toBuffer } from "@/common/utils";
 import { config } from "@/config/index";
-import { PendingRefreshTokens } from "@/models/pending-refresh-tokens";
+import { PendingRefreshTokens, RefreshTokens } from "@/models/pending-refresh-tokens";
 import * as metadataIndexProcess from "@/jobs/metadata-index/process-queue";
 
 const QUEUE_NAME = "metadata-index-fetch-queue";
@@ -36,25 +36,30 @@ if (config.doBackgroundWork) {
       const { kind, data } = job.data as MetadataIndexInfo;
       const prioritized = !_.isUndefined(job.opts.priority);
       const limit = 1000;
-      let tokens: string[] = [];
+      let refreshTokens: RefreshTokens[] = [];
 
       if (kind === "full-collection") {
         // Get batch of tokens for the collection
         const [contract, tokenId] = data.continuation
           ? data.continuation.split(":")
           : [AddressZero, "0"];
-        tokens = await getTokensForCollection(data.collection, contract, tokenId, limit);
+        refreshTokens = await getTokensForCollection(data.collection, contract, tokenId, limit);
+
+        // If no more tokens found
+        if (!_.isEmpty(refreshTokens)) {
+          return;
+        }
 
         // If there are potentially more tokens to refresh
-        if (!_.isEmpty(tokens) && _.size(tokens) == limit) {
-          const lastToken = _.last(tokens);
+        if (!_.isEmpty(refreshTokens) && _.size(refreshTokens) == limit) {
+          const lastToken = refreshTokens[limit - 1];
           await addToQueue(
             [
               {
                 kind,
                 data: {
                   ...data,
-                  continuation: lastToken,
+                  continuation: `${lastToken.contract}:${lastToken.tokenId}`,
                 },
               },
             ],
@@ -63,15 +68,19 @@ if (config.doBackgroundWork) {
         }
       } else if (kind === "single-token") {
         // Create the single token from the params
-        tokens.push(`${data.contract}:${data.tokenId}`);
+        refreshTokens.push({
+          collection: data.collection,
+          contract: data.contract,
+          tokenId: data.tokenId,
+        });
       }
 
       // Add the tokens to the list
       const pendingRefreshTokens = new PendingRefreshTokens(data.method);
-      await pendingRefreshTokens.add(tokens, prioritized);
+      await pendingRefreshTokens.add(refreshTokens, prioritized);
 
       // Trigger a job to process the queue
-      await metadataIndexProcess.addToQueue(data.method, data.collection);
+      await metadataIndexProcess.addToQueue(data.method);
     },
     { connection: redis.duplicate(), concurrency: 3 }
   );
@@ -89,10 +98,10 @@ async function getTokensForCollection(
 ) {
   const tokens = await idb.manyOrNone(
     `SELECT tokens.contract, tokens.token_id
-              FROM tokens
-              WHERE tokens.collection_id = $/collection/
-              AND (tokens.contract, tokens.token_id) > ($/contract/, $/tokenId/)
-              LIMIT ${limit}`,
+            FROM tokens
+            WHERE tokens.collection_id = $/collection/
+            AND (tokens.contract, tokens.token_id) > ($/contract/, $/tokenId/)
+            LIMIT ${limit}`,
     {
       collection: collection,
       contract: toBuffer(contract),
@@ -100,7 +109,9 @@ async function getTokensForCollection(
     }
   );
 
-  return tokens.map((t) => `${fromBuffer(t.contract)}:${t.token_id}`);
+  return tokens.map((t) => {
+    return { collection, contract: fromBuffer(t.contract), tokenId: t.token_id } as RefreshTokens;
+  });
 }
 
 // We support the following metadata indexing methods.
