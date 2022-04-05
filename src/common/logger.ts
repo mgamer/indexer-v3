9100df8b0e898dbdd646fe9dd6ddc9ddbc5b215a
dikkeduif <donald.dewulf@gmail.com>
2022-04-05 03:34:15 -0400

    chore: add railway snapshot id

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -50,6 +50,7 @@ const log = (level: "error" | "info" | "warn") => {
       component,
       version: process.env.npm_package_version,
       networkInterfaces: results,
+      railwaySnapshotId: process.env.RAILWAY_SNAPSHOT_ID,
     });
 };
 
