dfa9d32c7f186fc0a96881853626a832ceb73834
George Roman <george.roman.99@gmail.com>
2022-04-05 16:19:21 +0300

    chore: debugging

--- src/orderbook/orders/opendao/index.ts
+++ src/orderbook/orders/opendao/index.ts
@@ -126,6 +126,7 @@ export const save = async (
         } else if (error.message === "no-balance") {
           fillabilityStatus = "no-balance";
         } else {
+          logger.info("debug", `error: ${error}`);
           return results.push({
             id,
             status: "not-fillable",
