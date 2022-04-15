cc83629c2f115180c257876c17d48b616c04eb6c
George Roman <george.roman.99@gmail.com>
2022-04-15 10:48:01 +0300

    fix: properly handle 0 as expiration time

--- src/orderbook/orders/opendao/build/utils.ts
+++ src/orderbook/orders/opendao/build/utils.ts
@@ -50,7 +50,7 @@ export const getBuildInfo = async (
     price: options.weiPrice,
     fees: [],
     amount: collectionResult.kind === "erc1155" ? "1" : undefined,
-    expiry: options.expirationTime ? options.expirationTime : undefined,
+    expiry: Number(options.expirationTime) ? options.expirationTime : undefined,
     nonce: options.nonce,
   };
 
