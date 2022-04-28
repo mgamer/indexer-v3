db43b564a3f665c4fb3ff0432a0a6da6c9a352e5
Idan Peleg <ipeleg@hotmail.com>
2022-04-28 15:46:05 -0400

    feat: script to backfill images

--- src/jobs/update-attribute/resync-attribute-floor-sell.ts
+++ src/jobs/update-attribute/resync-attribute-floor-sell.ts
@@ -9,17 +9,17 @@ import { redis, redlock } from "@/common/redis";
 import { config } from "@/config/index";
 
 import { idb } from "@/common/db";
-import * as resyncAttributeCache from "@/jobs/update-attribute/resync-attribute-cache";
 import { fromBuffer } from "@/common/utils";
+import { Tokens } from "@/models/tokens";
 
-const QUEUE_NAME = "resync-attribute-floor-value-queue";
+const QUEUE_NAME = "resync-attribute-sample-image-queue";
 
 export const queue = new Queue(QUEUE_NAME, {
   connection: redis.duplicate(),
   defaultJobOptions: {
-    attempts: 10,
-    removeOnComplete: 10000,
-    removeOnFail: 10000,
+    attempts: 5,
+    removeOnComplete: 1000,
+    removeOnFail: 1000,
   },
 });
 new QueueScheduler(QUEUE_NAME, { connection: redis.duplicate() });
@@ -30,7 +30,7 @@ if (config.doBackgroundWork) {
     QUEUE_NAME,
     async (job: Job) => {
       const { continuation } = job.data;
-      const limit = 500;
+      const limit = 50;
       let continuationFilter = "";
 
       if (continuation != "") {
@@ -57,14 +57,41 @@ if (config.doBackgroundWork) {
             JOIN tokens ON collections.contract = tokens.contract
             JOIN token_attributes ON tokens.contract = token_attributes.contract AND token_attributes.token_id = tokens.token_id
             WHERE collections.id IN ('$/collectionsIds:raw/')
-            AND tokens.floor_sell_value IS NOT NULL
         `;
 
         const tokens = await idb.manyOrNone(tokensQuery, { collectionsIds });
 
-        _.forEach(tokens, (token) => {
-          resyncAttributeCache.addToQueue(fromBuffer(token.contract), token.token_id, 0);
-        });
+        for (const token of tokens) {
+          const tokenAttributes = await Tokens.getTokenAttributes(
+            fromBuffer(token.contract),
+            token.token_id
+          );
+          for (const tokenAttribute of tokenAttributes) {
+            const sampleImageQuery = `
+                SELECT (array_agg(DISTINCT(x.image)))[1:4] AS "sampleImages"
+                FROM (
+                    SELECT image
+                    FROM token_attributes
+                    JOIN tokens ON token_attributes.contract = tokens.contract AND token_attributes.token_id = tokens.token_id
+                    WHERE token_attributes.attribute_id = $/attributeId/
+                    LIMIT 4
+                ) AS x
+            `;
+
+            const images = await idb.oneOrNone(sampleImageQuery, {
+              attributeId: tokenAttribute.attributeId,
+            });
+
+            const query = `UPDATE attributes
+                           SET sample_images = $/images/
+                           WHERE id = $/attributeId/`;
+
+            await idb.none(query, {
+              attributeId: tokenAttribute.attributeId,
+              images: images.sampleImages,
+            });
+          }
+        }
 
         if (_.size(collections) == limit) {
           const lastCollection = _.last(collections);
@@ -82,7 +109,7 @@ if (config.doBackgroundWork) {
   });
 
   redlock
-    .acquire(["sell-value-resync"], 60 * 60 * 24 * 30 * 1000)
+    .acquire(["sample-image-resync"], 60 * 60 * 24 * 30 * 1000)
     .then(async () => {
       await addToQueue();
     })
