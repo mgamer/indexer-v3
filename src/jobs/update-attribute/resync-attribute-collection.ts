eaeb1be6dd0c314e60deb2b8cb55a09c40d453f3
Idan Peleg <ipeleg@hotmail.com>
2022-05-04 18:31:35 -0400

    feat: wip

--- src/jobs/update-attribute/resync-attribute-collection.ts
+++ src/jobs/update-attribute/resync-attribute-collection.ts
@@ -98,7 +98,7 @@ if (config.doBackgroundWork) {
   });
 
   redlock
-    .acquire(["attribute-collection"], 60 * 60 * 24 * 30 * 1000)
+    .acquire(["attribute-key"], 60 * 60 * 24 * 30 * 1000)
     .then(async () => {
       await addToQueue();
     })
