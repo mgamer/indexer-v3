3c3aeb67f0f6651ce94aa85ce69994075389da1f
Idan Peleg <ipeleg@hotmail.com>
2022-05-19 17:58:55 -0400

    feat: stop full collections tokens refresh

--- src/api/endpoints/collections/post-collections-refresh/v1.ts
+++ src/api/endpoints/collections/post-collections-refresh/v1.ts
@@ -9,7 +9,6 @@ import { edb } from "@/common/db";
 import { logger } from "@/common/logger";
 import { OpenseaIndexerApi } from "@/utils/opensea-indexer-api";
 import { Collections } from "@/models/collections";
-import * as metadataIndexFetch from "@/jobs/metadata-index/fetch-queue";
 import * as collectionUpdatesMetadata from "@/jobs/collection-updates/metadata-queue";
 import * as collectionsRefreshCache from "@/jobs/collections-refresh/collections-refresh-cache";
 import * as Boom from "@hapi/boom";
@@ -99,18 +98,18 @@ export const postCollectionsRefreshV1Options: RouteOptions = {
       );
 
       // Refresh the collection tokens metadata
-      await metadataIndexFetch.addToQueue(
-        [
-          {
-            kind: "full-collection",
-            data: {
-              method: "opensea",
-              collection: collection.id,
-            },
-          },
-        ],
-        true
-      );
+      // await metadataIndexFetch.addToQueue(
+      //   [
+      //     {
+      //       kind: "full-collection",
+      //       data: {
+      //         method: "opensea",
+      //         collection: collection.id,
+      //       },
+      //     },
+      //   ],
+      //   true
+      // );
 
       // Refresh the collection metadata
       await collectionUpdatesMetadata.addToQueue(collection.contract);
