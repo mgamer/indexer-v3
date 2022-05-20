b41fd03abe6a1f53cd943d4feebf2c1bda48818c
Peter Watts <pwattsmail@gmail.com>
2022-05-20 10:38:02 +1000

    feat: lower rate limit

--- src/api/endpoints/events/get-tokens-floor-ask/v2.ts
+++ src/api/endpoints/events/get-tokens-floor-ask/v2.ts
@@ -20,7 +20,7 @@ const version = "v2";
 export const getTokensFloorAskV2Options: RouteOptions = {
   cache: {
     privacy: "public",
-    expiresIn: 1000,
+    expiresIn: 5000,
   },
   description: "Get updates any time the best price of a token changes",
   notes:
