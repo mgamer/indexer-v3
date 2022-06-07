ad0860c50cb6d17542e9f367607a20a38262c749
Idan Peleg <ipeleg@hotmail.com>
2022-06-07 08:21:23 -0400

    feat: use replica for transfers api as well

--- src/api/endpoints/transfers/get-transfers/v1.ts
+++ src/api/endpoints/transfers/get-transfers/v1.ts
@@ -3,7 +3,7 @@
 import { Request, RouteOptions } from "@hapi/hapi";
 import Joi from "joi";
 
-import { edb } from "@/common/db";
+import { redb } from "@/common/db";
 import { logger } from "@/common/logger";
 import { formatEth, fromBuffer, toBuffer } from "@/common/utils";
 
@@ -141,7 +141,7 @@ export const getTransfersV1Options: RouteOptions = {
       baseQuery += ` OFFSET $/offset/`;
       baseQuery += ` LIMIT $/limit/`;
 
-      const result = await edb.manyOrNone(baseQuery, query).then((result) =>
+      const result = await redb.manyOrNone(baseQuery, query).then((result) =>
         result.map((r) => ({
           token: {
             contract: fromBuffer(r.address),
