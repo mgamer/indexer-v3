8dc82235fa6b55bc33fed82f7e606602f18da7a3
Idan Peleg <ipeleg@hotmail.com>
2022-06-07 09:52:33 -0400

    feat: more use of read replica

--- src/api/endpoints/events/get-tokens-floor-ask/v1.ts
+++ src/api/endpoints/events/get-tokens-floor-ask/v1.ts
@@ -3,7 +3,7 @@
 import { Request, RouteOptions } from "@hapi/hapi";
 import Joi from "joi";
 
-import { edb } from "@/common/db";
+import { redb } from "@/common/db";
 import { logger } from "@/common/logger";
 import {
   base64Regex,
@@ -160,7 +160,7 @@ export const getTokensFloorAskV1Options: RouteOptions = {
       // Pagination
       baseQuery += ` LIMIT $/limit/`;
 
-      const rawResult = await edb.manyOrNone(baseQuery, query);
+      const rawResult = await redb.manyOrNone(baseQuery, query);
 
       let continuation = null;
       if (rawResult.length === query.limit) {
