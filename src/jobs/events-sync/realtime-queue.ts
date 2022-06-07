ef7dac724d2f353b5ff353e8ebbc0a56602abc6d
George Roman <george.roman.99@gmail.com>
2022-06-07 15:52:00 +0300

    fix: update log for debugging purposes

--- src/jobs/events-sync/realtime-queue.ts
+++ src/jobs/events-sync/realtime-queue.ts
@@ -50,7 +50,10 @@ if (config.doBackgroundWork) {
         }
 
         const fromBlock = Math.max(localBlock, headBlock - maxBlocks + 1);
-        logger.info(QUEUE_NAME, `Events realtime syncing block range [${fromBlock}, ${headBlock}]`);
+        logger.info(
+          QUEUE_NAME,
+          `Events-v2 realtime syncing block range [${fromBlock}, ${headBlock}]`
+        );
 
         await syncEvents(fromBlock, headBlock);
 
