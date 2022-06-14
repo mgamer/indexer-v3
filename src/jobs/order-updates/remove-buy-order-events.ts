4ad709f7882086c3f49e70444935a1c2d27573a6
Ofir Sudai <ofirsud@gmail.com>
2022-06-14 09:29:07 -0400

    feat: disabled backfill

--- src/jobs/order-updates/remove-buy-order-events.ts
+++ src/jobs/order-updates/remove-buy-order-events.ts
@@ -90,7 +90,7 @@ if (config.doBackgroundWork) {
   redlock
     .acquire([`${QUEUE_NAME}-lock`], 60 * 60 * 24 * 30 * 1000)
     .then(async () => {
-      await addToQueue();
+      // await addToQueue();
     })
     .catch(() => {
       // Skip on any errors
