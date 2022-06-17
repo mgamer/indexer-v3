a4be359572798f3cbf1cda95156cdef110681f92
Idan Peleg <ipeleg@hotmail.com>
2022-06-17 08:41:24 -0400

    feat: update token bids in batches

---
+++ src/jobs/bid-updates/index.ts
@@ -0,0 +1,2 @@
+import "@/jobs/bid-updates/break-token-set-bid-queue";
+import "@/jobs/bid-updates/top-bid-update-queue";
--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -17,6 +17,7 @@ import "@/jobs/daily-volumes";
 import "@/jobs/update-attribute";
 import "@/jobs/collections-refresh";
 import "@/jobs/nft-balance-updates";
+import "@/jobs/bid-updates";
 
 // Export all job queues for monitoring through the BullMQ UI
 
@@ -61,6 +62,8 @@ import * as updateNftBalanceTopBidQueue from "@/jobs/nft-balance-updates/update-
 import * as backfillNftBalanceTopBidQueue from "@/jobs/nft-balance-updates/backfill-top-bid-queue";
 import * as backfillFillEventsCreatedAt from "@/jobs/backfill/backfill-fill-events-created-at";
 import * as removeBuyOrderEvents from "@/jobs/order-updates/remove-buy-order-events";
+import * as breakTokenSetBidQueue from "@/jobs/bid-updates/break-token-set-bid-queue";
+import * as topBidUpdateQueue from "@/jobs/bid-updates/top-bid-update-queue";
 
 export const allJobQueues = [
   arweaveSyncBackfill.queue,
@@ -104,4 +107,6 @@ export const allJobQueues = [
   backfillNftBalanceTopBidQueue.queue,
   backfillFillEventsCreatedAt.queue,
   removeBuyOrderEvents.queue,
+  breakTokenSetBidQueue.queue,
+  topBidUpdateQueue.queue,
 ];
