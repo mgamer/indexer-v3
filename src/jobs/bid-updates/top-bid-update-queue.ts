a97caa41e0cdd9692ef42b7089988d33547ce297
Idan Peleg <ipeleg@hotmail.com>
2022-06-17 11:47:25 -0400

    feat: update bids only if it's higher

--- src/jobs/bid-updates/top-bid-update-queue.ts
+++ src/jobs/bid-updates/top-bid-update-queue.ts
@@ -75,6 +75,7 @@ if (config.doBackgroundWork) {
           WHERE "t"."contract" = "z"."contract"
           AND "t"."token_id" = "z"."token_id"
           AND "t"."top_buy_id" IS DISTINCT FROM "z"."order_id"
+          AND "z"."value" > "t"."top_buy_value"
         )
         
         SELECT contract, token_id
@@ -93,7 +94,7 @@ if (config.doBackgroundWork) {
         await addToQueue(tokenSetId, fromBuffer(result.contract), result.token_id);
       }
     },
-    { connection: redis.duplicate(), concurrency: 10 }
+    { connection: redis.duplicate(), concurrency: 15 }
   );
 
   worker.on("error", (error) => {
