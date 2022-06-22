d50392e97446978530fcce7d430bf96cdef5860d
Idan Peleg <ipeleg@hotmail.com>
2022-06-22 11:21:40 -0400

    feat: new api to get swagger json

---
+++ src/api/endpoints/admin/get-open-api.ts
@@ -0,0 +1,24 @@
+import { RouteOptions } from "@hapi/hapi";
+import { logger } from "@/common/logger";
+import { inject } from "@/api/index";
+
+export const getOpenApiOptions: RouteOptions = {
+  description: "Get swagger json in OpenApi V3",
+  tags: ["api", "x-admin"],
+  timeout: {
+    server: 10 * 1000,
+  },
+  handler: async () => {
+    try {
+      const response = await inject({
+        method: "GET",
+        url: "/swagger.json",
+      });
+
+      return { message: JSON.parse(response.payload) };
+    } catch (error) {
+      logger.error("get-open-api-handler", `Handler failure: ${error}`);
+      throw error;
+    }
+  },
+};
