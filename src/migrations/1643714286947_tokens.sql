b1a4ab5a18441d581481d4ae7af9a49de814b3ba
Idan Peleg <ipeleg@hotmail.com>
2022-06-28 11:45:33 -0400

    feat: update migration file

--- src/migrations/1643714286947_tokens.sql
+++ src/migrations/1643714286947_tokens.sql
@@ -44,10 +44,6 @@ CREATE INDEX "tokens_collection_id_contract_token_id_index"
 CREATE INDEX "tokens_collection_id_source_id_floor_sell_value_index"
   ON "tokens" ("collection_id", "floor_sell_source_id", "floor_sell_value");
 
-CREATE INDEX "tokens_collection_id_floor_sell_value_index"
-  ON "tokens" ("collection_id", "floor_sell_value")
-  WHERE ("floor_sell_is_reservoir");
-
 CREATE INDEX "tokens_contract_top_buy_value_index"
   ON "tokens" ("contract", "top_buy_value" DESC NULLS LAST);
 
