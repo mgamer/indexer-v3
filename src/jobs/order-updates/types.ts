461ca132cece922e6a4c5e40175bdb120f64ec91
George Roman <george.roman.99@gmail.com>
2022-07-05 11:39:45 +0300

    refactor: order updates by maker logic

--- src/jobs/order-updates/types.ts
+++ src/jobs/order-updates/types.ts
@@ -1,4 +1,4 @@
-// Possible kinds of triggers that could result in new floor ask prices.
+// Possible kinds of triggers that could result in new floor ask prices
 export type TriggerKind =
   | "new-order"
   | "expiry"
