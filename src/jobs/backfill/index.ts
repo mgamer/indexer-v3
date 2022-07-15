daf35065481085262df4e886061405c26c896e9d
George Roman <george.roman.99@gmail.com>
2022-07-15 09:37:33 +0300

    fix: backfill missing transaction fields

--- src/jobs/backfill/index.ts
+++ src/jobs/backfill/index.ts
@@ -2,3 +2,4 @@ import "@/jobs/backfill/backfill-block-timestamps";
 import "@/jobs/backfill/backfill-fill-events-created-at";
 import "@/jobs/backfill/backfill-fill-events-fill-source";
 import "@/jobs/backfill/backfill-fill-events-order-source";
+import "@/jobs/backfill/backfill-transaction-block-fields";
