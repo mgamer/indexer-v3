7a632e07469b80e6060021c4d3f818c064ffd3a3
Ofir Sudai <ofirsud@gmail.com>
2022-07-18 16:15:20 -0400

    feat: added comments

--- src/jobs/orderbook/post-order-external.ts
+++ src/jobs/orderbook/post-order-external.ts
@@ -8,10 +8,14 @@ import * as Sdk from "@reservoir0x/sdk";
 import { joinSignature } from "@ethersproject/bytes";
 
 const QUEUE_NAME = "orderbook-post-order-external-queue";
+
+// Open Sea default rate limit - 2 requests per second for post apis
 const OPENSEA_RATE_LIMIT_REQUEST_COUNT = 2;
-const OPENSEA_RATE_LIMIT_INTERVAL = 1;
-const LOOKSRARE_RATE_LIMIT_REQUEST_COUNT = 2;
-const LOOKSRARE_RATE_LIMIT_INTERVAL = 1;
+const OPENSEA_RATE_LIMIT_INTERVAL = 1000;
+
+// Looks Rare default rate limit - 120 requests per minute
+const LOOKSRARE_RATE_LIMIT_REQUEST_COUNT = 120;
+const LOOKSRARE_RATE_LIMIT_INTERVAL = 1000 * 60;
 
 export const queue = new Queue(QUEUE_NAME, {
   connection: redis.duplicate(),
@@ -44,15 +48,14 @@ if (config.doBackgroundWork) {
         throw new Error("Unsupported orderbook");
       }
 
-      let orderbookApiKey = null;
-
-      if (orderbookApiKeyEncrypted) {
-        orderbookApiKey = decryptApiKey(orderbookApiKeyEncrypted);
-      }
-
+      // Decrypt api key if passed, otherwise get the default api key.
+      const orderbookApiKey = orderbookApiKeyEncrypted
+        ? decryptApiKey(orderbookApiKeyEncrypted)
+        : getOrderbookDefaultApiKey(orderbook);
       const apiKeyRateLimitReached = await reachedApiKeyRateLimit(orderbook, orderbookApiKey);
 
       if (apiKeyRateLimitReached) {
+        // If limit reached, reschedule job based on the limit expiration.
         const rateLimitKey = getApiKeyRateLimitRedisKey(orderbook, orderbookApiKey);
         const delay = await redis.pttl(rateLimitKey);
 
@@ -68,7 +71,12 @@ if (config.doBackgroundWork) {
           await postOrder(orderbook, orderData, orderbookApiKey);
         } catch (error) {
           if (error instanceof RequestWasThrottledError) {
-            const delay = error.delay * 1000;
+            // If we got throttled by the api, reschedule job based on the provided delay.
+            const rateLimitKey = getApiKeyRateLimitRedisKey(orderbook, orderbookApiKey);
+            const delay = error.delay;
+
+            // Extend the rate limit expiration.
+            await redis.pexpire(rateLimitKey, delay);
 
             job.data.retryJob = true;
             job.data.retryJobDelay = delay;
@@ -78,6 +86,7 @@ if (config.doBackgroundWork) {
               `Post Order Throttled. orderbook: ${orderbook}, orderbookApiKey: ${orderbookApiKey}, delay: ${delay}, retry: ${retry}`
             );
           } else {
+            // If we got an error from the api, reschedule job based on fixed delay.
             job.data.retryJob = true;
             job.data.retryJobDelay = 5000;
 
@@ -101,10 +110,16 @@ if (config.doBackgroundWork) {
       const delay = job.data.retryJobDelay;
 
       if (retry <= 5) {
+        let orderbookApiKey = null;
+
+        if (job.data.orderbookApiKeyEncrypted) {
+          orderbookApiKey = decryptApiKey(job.data.orderbookApiKeyEncrypted);
+        }
+
         await addToQueue(
           job.data.orderData,
           job.data.orderbook,
-          job.data.orderbookApiKey,
+          orderbookApiKey,
           retry,
           delay,
           true
@@ -112,7 +127,7 @@ if (config.doBackgroundWork) {
       } else {
         logger.error(
           QUEUE_NAME,
-          `Max Retries Reached. orderbook: ${job.data.orderbook}, orderbookApiKey: ${job.data.orderbookApiKey}, retry: ${retry}`
+          `Max Retries Reached. orderbook: ${job.data.orderbook}, orderbookApiKey: ${job.data.orderbookApiKey}`
         );
       }
     }
@@ -130,36 +145,44 @@ export type PostOrderExternalParams = {
   retry: number;
 };
 
-const getApiKeyRateLimitRedisKey = (orderbook: string, apiKey: string | null) => {
-  let rateLimitKey = "orderbook-post-order-external" + ":" + orderbook;
-
-  if (apiKey) {
-    rateLimitKey = rateLimitKey + ":" + apiKey;
-  }
-
-  return rateLimitKey;
-};
-
-const reachedApiKeyRateLimit = async (orderbook: string, apiKeyHash: string | null) => {
-  let rateLimitRequestCount = 2;
-  let rateLimitInterval = 1;
-
+const getOrderbookDefaultApiKey = (orderbook: string) => {
   switch (orderbook) {
     case "opensea":
-      rateLimitRequestCount = OPENSEA_RATE_LIMIT_REQUEST_COUNT;
-      rateLimitInterval = OPENSEA_RATE_LIMIT_INTERVAL;
-      break;
+      return config.openSeaApiKey;
+    case "looks-rare":
+      return config.looksRareApiKey;
+  }
+
+  throw new Error(`Unsupported orderbook ${orderbook}`);
+};
+
+const getApiKeyRateLimitRedisKey = (orderbook: string, orderbookApiKey: string) => {
+  return "orderbook-post-order-external" + ":" + orderbook + ":" + orderbookApiKey;
+};
+
+const reachedApiKeyRateLimit = async (orderbook: string, orderbookApiKey: string) => {
+  let rateLimitRequestCount;
+  let rateLimitInterval;
+
+  switch (orderbook) {
     case "looks-rare":
       rateLimitRequestCount = LOOKSRARE_RATE_LIMIT_REQUEST_COUNT;
       rateLimitInterval = LOOKSRARE_RATE_LIMIT_INTERVAL;
       break;
+    case "opensea":
+    default:
+      rateLimitRequestCount = OPENSEA_RATE_LIMIT_REQUEST_COUNT;
+      rateLimitInterval = OPENSEA_RATE_LIMIT_INTERVAL;
+      break;
   }
 
-  const rateLimitKey = getApiKeyRateLimitRedisKey(orderbook, apiKeyHash);
+  const rateLimitKey = getApiKeyRateLimitRedisKey(orderbook, orderbookApiKey);
+
+  // Always increment count.
   const current = await redis.incr(rateLimitKey);
 
   if (current == 1) {
-    await redis.expire(rateLimitKey, rateLimitInterval);
+    await redis.pexpire(rateLimitKey, rateLimitInterval);
   }
 
   return current > rateLimitRequestCount;
@@ -168,7 +191,7 @@ const reachedApiKeyRateLimit = async (orderbook: string, apiKeyHash: string | nu
 const postOrder = async (
   orderbook: string,
   orderData: Record<string, unknown>,
-  orderbookApiKey: string | null
+  orderbookApiKey: string
 ) => {
   switch (orderbook) {
     case "opensea":
@@ -180,7 +203,7 @@ const postOrder = async (
   throw new Error(`Unsupported orderbook ${orderbook}`);
 };
 
-const postOpenSea = async (orderData: Record<string, unknown>, apiKey: string | null) => {
+const postOpenSea = async (orderData: Record<string, unknown>, apiKey: string) => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const sdkOrder = new Sdk.Seaport.Order(config.chainId, orderData as any);
 
@@ -220,7 +243,7 @@ const postOpenSea = async (orderData: Record<string, unknown>, apiKey: string | 
           error.response.data.detail?.startsWith("Request was throttled. Expected available in")
         ) {
           const detailParts = error.response.data.detail.split(" ");
-          const delay = detailParts[detailParts.length - 2];
+          const delay = detailParts[detailParts.length - 2] * 1000;
 
           throw new RequestWasThrottledError("Request was throttled by OpenSea", delay);
         }
@@ -230,7 +253,7 @@ const postOpenSea = async (orderData: Record<string, unknown>, apiKey: string | 
     });
 };
 
-const postLooksRare = async (orderData: Record<string, unknown>, apiKey: string | null) => {
+const postLooksRare = async (orderData: Record<string, unknown>, apiKey: string) => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const sdkOrder = new Sdk.LooksRare.Order(config.chainId, orderData as any);
   const lrOrder = {
@@ -298,6 +321,7 @@ export const addToQueue = async (
   delay = 0,
   prioritized = false
 ) => {
+  // Encrypt api key so it won't be visible in bullmq admin ui.
   const orderbookApiKeyEncrypted = orderbookApiKey ? encryptApiKey(orderbookApiKey) : null;
 
   await queue.add(
