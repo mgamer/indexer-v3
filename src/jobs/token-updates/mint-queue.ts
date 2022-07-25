44c09a0fcd71e464aeea31b7419f13101a5a4cf8
Idan Peleg <ipeleg@hotmail.com>
2022-07-25 19:52:43 -0400

    fix: update mint-queue concurrency

--- src/jobs/token-updates/mint-queue.ts
+++ src/jobs/token-updates/mint-queue.ts
@@ -216,7 +216,7 @@ if (config.doBackgroundWork) {
         throw error;
       }
     },
-    { connection: redis.duplicate(), concurrency: 30 }
+    { connection: redis.duplicate(), concurrency: 5 }
   );
   worker.on("error", (error) => {
     logger.error(QUEUE_NAME, `Worker errored: ${error}`);
