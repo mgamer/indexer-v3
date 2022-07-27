f341afeaf6ecd05c0a6af1204c738b289f590606
Idan Peleg <ipeleg@hotmail.com>
2022-07-27 09:05:00 -0400

    feat: update events utils

--- src/sync/events/utils.ts
+++ src/sync/events/utils.ts
@@ -99,15 +99,15 @@ export const getOrderSourceByOrderKind = async (orderKind: string): Promise<numb
 
     switch (orderKind) {
       case "x2y2":
-        return sources.getByName("X2Y2").id;
+        return sources.getByDomain("x2y2.io").id;
       case "foundation":
-        return sources.getByName("Foundation").id;
+        return sources.getByDomain("foundation.app").id;
       case "looks-rare":
-        return sources.getByName("LooksRare").id;
+        return sources.getByDomain("looksrare.org").id;
       case "seaport":
       case "wyvern-v2":
       case "wyvern-v2.3":
-        return sources.getByName("OpenSea").id;
+        return sources.getByDomain("opensea.io").id;
       default:
         // For all other order kinds we cannot default the source
         return null;
