cf8d53ecd286a5e832944e5c123c15e837739c2d
Idan Peleg <ipeleg@hotmail.com>
2022-07-28 12:54:05 -0400

    feat: fix pubsub

--- src/pubsub/channels.ts
+++ src/pubsub/channels.ts
@@ -1,3 +1,3 @@
-export enum events {
+export enum channels {
   sourcesUpdated = "sources-updated",
 }
--- src/pubsub/sources-updated-event.ts
+++ src/pubsub/sources-updated-event.ts
@@ -1,19 +1,14 @@
-import { redisSubscriber } from "@/common/redis";
-import { events } from "@/pubsub/events";
+import { channels } from "@/pubsub/channels";
 import { logger } from "@/common/logger";
-import { Sources } from "@/models/sources";
 import { config } from "@/config/index";
+import { Sources } from "@/models/sources";
 
-redisSubscriber.subscribe(events.sourcesUpdated, (err) => {
-  if (err) {
-    logger.error(events.sourcesUpdated, `Failed to subscribe ${err.message}`);
+export class SourcesUpdatedEvent {
+  public static async handleEvent(message: string) {
+    await Sources.forceDataReload();
+    logger.info(
+      channels.sourcesUpdated,
+      `Reloaded sources message=${message} on ${config.railwayStaticUrl}`
+    );
   }
-});
-
-redisSubscriber.on(events.sourcesUpdated, async (channel, message) => {
-  await Sources.forceDataReload();
-  logger.info(
-    events.sourcesUpdated,
-    `Reloaded sources from channel=${channel}, message=${message} on ${config.railwayStaticUrl}`
-  );
-});
+}
