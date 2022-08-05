0cf3a191bc3e374e49d27e5c366e96bf3e8b3440
Idan Peleg <ipeleg@hotmail.com>
2022-08-05 12:22:31 -0400

    feat: increase bids cleanup frequency

--- src/models/user-received-bids/index.ts
+++ src/models/user-received-bids/index.ts
@@ -5,12 +5,15 @@ import { idb } from "@/common/db";
 
 export class UserReceivedBids {
   public static async cleanBids(limit: number) {
+    const offset = _.random(0, 6) * limit;
+
     const query = `
       DELETE FROM user_received_bids
       WHERE id IN (
         SELECT id
         FROM user_received_bids
         WHERE clean_at < NOW()
+        OFFSET ${offset}
         LIMIT ${limit}
       )
       
