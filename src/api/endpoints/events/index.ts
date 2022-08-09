55bef302b88ffbc56dc17a4a6806f6fd6b745cbb
nofir <42353301+nofir@users.noreply.github.com>
2022-08-09 14:10:28 -0400

    Feat/bid events (#1430)
    * feat: added bid events logic
    
    * feat: fixed price, added api
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/api/endpoints/events/index.ts
+++ src/api/endpoints/events/index.ts
@@ -1,3 +1,4 @@
 export * from "@/api/endpoints/events/get-collections-floor-ask/v1";
 export * from "@/api/endpoints/events/get-order-events/v1";
 export * from "@/api/endpoints/events/get-tokens-floor-ask/v2";
+export * from "@/api/endpoints/events/get-bid-events/v1";
