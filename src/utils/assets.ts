a6ed007aebb481108332a3698ca10f9cb2f05e15
Idan Peleg <ipeleg@hotmail.com>
2022-09-09 08:54:26 -0400

    feat: enable images cdn

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -1,32 +1,31 @@
-// import _ from "lodash";
-// import { config } from "@/config/index";
-// import { encrypt } from "@/common/utils";
+import _ from "lodash";
+import { config } from "@/config/index";
+import { encrypt } from "@/common/utils";
 
 export class Assets {
   public static getLocalAssetsLink(assets: string | string[]) {
-    return assets;
-    // if (_.isEmpty(assets) || assets == "") {
-    //   return undefined;
-    // }
-    //
-    // const baseUrl = `https://api${
-    //   config.chainId == 1 ? "" : "-rinkeby"
-    // }.reservoir.tools/assets/v1?`;
-    //
-    // if (_.isArray(assets)) {
-    //   const assetsResult = [];
-    //   for (const asset of assets) {
-    //     const queryParams = new URLSearchParams();
-    //     queryParams.append("asset", encrypt(asset));
-    //     assetsResult.push(`${baseUrl}${queryParams.toString()}`);
-    //   }
-    //
-    //   return assetsResult;
-    // } else {
-    //   const queryParams = new URLSearchParams();
-    //   queryParams.append("asset", encrypt(assets));
-    //
-    //   return `${baseUrl}${queryParams.toString()}`;
-    // }
+    if (_.isEmpty(assets) || assets == "") {
+      return undefined;
+    }
+
+    const baseUrl = `https://api${
+      config.chainId == 1 ? "" : "-rinkeby"
+    }.reservoir.tools/assets/v1?`;
+
+    if (_.isArray(assets)) {
+      const assetsResult = [];
+      for (const asset of assets) {
+        const queryParams = new URLSearchParams();
+        queryParams.append("asset", encrypt(asset));
+        assetsResult.push(`${baseUrl}${queryParams.toString()}`);
+      }
+
+      return assetsResult;
+    } else {
+      const queryParams = new URLSearchParams();
+      queryParams.append("asset", encrypt(assets));
+
+      return `${baseUrl}${queryParams.toString()}`;
+    }
   }
 }
