18708905b1029be10fa076be71073efe8ea8acea
George Roman <george.roman.99@gmail.com>
2022-09-15 13:38:31 +0300

    refactor: separate looksrare handling logic

--- src/sync/events/handlers/index.ts
+++ src/sync/events/handlers/index.ts
@@ -2,13 +2,14 @@ import { EnhancedEvent, OnChainData, processOnChainData } from "@/events-sync/ha
 
 import * as cryptopunks from "@/events-sync/handlers/cryptopunks";
 import * as foundation from "@/events-sync/handlers/foundation";
+import * as looksrare from "@/events-sync/handlers/looks-rare";
 import * as nftx from "@/events-sync/handlers/nftx";
 import * as nouns from "@/events-sync/handlers/nouns";
 import * as sudoswap from "@/events-sync/handlers/sudoswap";
 import * as wyvern from "@/events-sync/handlers/wyvern";
 
 export type EventsInfo = {
-  kind: "cryptopunks" | "foundation" | "nftx" | "nouns" | "sudoswap" | "wyvern";
+  kind: "cryptopunks" | "foundation" | "looks-rare" | "nftx" | "nouns" | "sudoswap" | "wyvern";
   events: EnhancedEvent[];
   backfill?: boolean;
 };
@@ -23,6 +24,11 @@ export const processEvents = async (info: EventsInfo) => {
 
     case "foundation": {
       data = await foundation.handleEvents(info.events);
+      break;
+    }
+
+    case "looks-rare": {
+      data = await looksrare.handleEvents(info.events);
       break;
     }
 
