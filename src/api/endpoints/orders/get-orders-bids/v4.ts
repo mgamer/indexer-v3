f4d07a67e7e0facdb6ecb22b2cc4a71deadf8fd6
Idan Peleg <ipeleg@hotmail.com>
2022-09-28 15:08:23 -0400

    feat: allow pass maker along other values in bids api

--- src/api/endpoints/orders/get-orders-bids/v4.ts
+++ src/api/endpoints/orders/get-orders-bids/v4.ts
@@ -100,7 +100,7 @@ export const getOrdersBidsV4Options: RouteOptions = {
         .max(1000)
         .default(50)
         .description("Amount of items returned in response."),
-    }).oxor("token", "tokenSetId", "maker", "contracts", "ids", "source", "native"),
+    }).oxor("token", "tokenSetId", "contracts", "ids", "source", "native"),
   },
   response: {
     schema: Joi.object({
