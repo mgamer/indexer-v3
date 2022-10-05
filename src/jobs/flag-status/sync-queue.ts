5afa1e74342d2f630654f12429ddf3902a7b6304
nofir <42353301+nofir@users.noreply.github.com>
2022-10-05 17:10:22 -0400

    Ofir/res 604 attempt to improve open sea bid coverage (#2051)
    * feat: added improvments
    
    * feat: added func to skip check
    
    * feat: added token set id lock
    
    * feat: removed logs, updated delay
    
    * feat: add async calls
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/jobs/flag-status/sync-queue.ts
+++ src/jobs/flag-status/sync-queue.ts
@@ -11,9 +11,10 @@ import MetadataApi from "@/utils/metadata-api";
 import { PendingFlagStatusSyncTokens } from "@/models/pending-flag-status-sync-tokens";
 import * as flagStatusProcessQueue from "@/jobs/flag-status/process-queue";
 import { randomUUID } from "crypto";
+import _ from "lodash";
 
 const QUEUE_NAME = "flag-status-sync-queue";
-const LIMIT = 20;
+const LIMIT = 40;
 
 export const queue = new Queue(QUEUE_NAME, {
   connection: redis.duplicate(),
@@ -52,54 +53,63 @@ if (config.doBackgroundWork) {
         return;
       }
 
-      try {
-        const tokensMetadata = await MetadataApi.getTokensMetadata(
-          pendingSyncFlagStatusTokens,
-          true
-        );
+      const pendingSyncFlagStatusTokensChunks = _.chunk(pendingSyncFlagStatusTokens, 20);
 
-        for (const pendingSyncFlagStatusToken of pendingSyncFlagStatusTokens) {
-          const tokenMetadata = tokensMetadata.find(
-            (tokenMetadata) => tokenMetadata.tokenId === pendingSyncFlagStatusToken.tokenId
-          );
-
-          if (!tokenMetadata) {
-            logger.warn(
-              QUEUE_NAME,
-              `Missing Token Metadata. contract:${contract}, tokenId: ${pendingSyncFlagStatusToken.tokenId}, tokenIsFlagged:${pendingSyncFlagStatusToken.isFlagged}`
+      await Promise.all(
+        pendingSyncFlagStatusTokensChunks.map(async (pendingSyncFlagStatusTokensChunk) => {
+          try {
+            const tokensMetadata = await MetadataApi.getTokensMetadata(
+              pendingSyncFlagStatusTokensChunk,
+              true
             );
 
-            continue;
+            for (const pendingSyncFlagStatusToken of pendingSyncFlagStatusTokensChunk) {
+              const tokenMetadata = tokensMetadata.find(
+                (tokenMetadata) => tokenMetadata.tokenId === pendingSyncFlagStatusToken.tokenId
+              );
+
+              if (!tokenMetadata) {
+                logger.warn(
+                  QUEUE_NAME,
+                  `Missing Token Metadata. contract:${contract}, tokenId: ${pendingSyncFlagStatusToken.tokenId}, tokenIsFlagged:${pendingSyncFlagStatusToken.isFlagged}`
+                );
+
+                continue;
+              }
+
+              const isFlagged = Number(tokenMetadata.flagged);
+
+              if (pendingSyncFlagStatusToken.isFlagged != isFlagged) {
+                logger.info(
+                  QUEUE_NAME,
+                  `Flag Status Diff. contract:${contract}, tokenId: ${pendingSyncFlagStatusToken.tokenId}, tokenIsFlagged:${pendingSyncFlagStatusToken.isFlagged}, isFlagged:${isFlagged}`
+                );
+              }
+
+              await Tokens.update(contract, pendingSyncFlagStatusToken.tokenId, {
+                isFlagged,
+                lastFlagUpdate: new Date().toISOString(),
+              });
+            }
+          } catch (error) {
+            if ((error as any).response?.status === 429) {
+              logger.info(
+                QUEUE_NAME,
+                `Too Many Requests. error: ${JSON.stringify((error as any).response.data)}`
+              );
+
+              delay = 60 * 1000;
+
+              await pendingFlagStatusSyncTokensQueue.add(pendingSyncFlagStatusTokensChunk);
+            } else {
+              logger.error(
+                QUEUE_NAME,
+                `getTokenMetadata error. contract:${contract}, error:${error}`
+              );
+            }
           }
-
-          const isFlagged = Number(tokenMetadata.flagged);
-
-          if (pendingSyncFlagStatusToken.isFlagged != isFlagged) {
-            logger.info(
-              QUEUE_NAME,
-              `Flag Status Diff. contract:${contract}, tokenId: ${pendingSyncFlagStatusToken.tokenId}, tokenIsFlagged:${pendingSyncFlagStatusToken.isFlagged}, isFlagged:${isFlagged}`
-            );
-          }
-
-          await Tokens.update(contract, pendingSyncFlagStatusToken.tokenId, {
-            isFlagged,
-            lastFlagUpdate: new Date().toISOString(),
-          });
-        }
-      } catch (error) {
-        if ((error as any).response?.status === 429) {
-          logger.info(
-            QUEUE_NAME,
-            `Too Many Requests. error: ${JSON.stringify((error as any).response.data)}`
-          );
-
-          delay = 60 * 1000;
-
-          await pendingFlagStatusSyncTokensQueue.add(pendingSyncFlagStatusTokens);
-        } else {
-          logger.error(QUEUE_NAME, `getTokensMetadata error. contract:${contract}, error:${error}`);
-        }
-      }
+        })
+      );
 
       await addToQueue(collectionId, contract, delay);
     },
