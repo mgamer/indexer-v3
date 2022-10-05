a90e5d4bb89d7aaab1b134675dc00abee90dbf7c
Idan Peleg <ipeleg@hotmail.com>
2022-10-05 14:53:48 -0400

    feat: consistent collectionBidSupported

--- src/api/endpoints/collections/get-collection/v3.ts
+++ src/api/endpoints/collections/get-collection/v3.ts
@@ -339,7 +339,7 @@ export const getCollectionV3Options: RouteOptions = {
                   ? Number(r.floor_sell_value) / Number(r.day30_floor_sell_value)
                   : null,
               },
-              collectionBidSupported: Number(r.token_count) <= config.maxCollectionBidTokenSetSize,
+              collectionBidSupported: Number(r.token_count) <= config.maxTokenSetSize,
               ownerCount: Number(r.ownerCount),
               attributes: _.map(_.sortBy(r.attributes, ["rank", "key"]), (attribute) => ({
                 key: attribute.key,
