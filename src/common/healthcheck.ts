29f5397edf3a66c3ff2d310cd3fe8af5a6e73a1a
Idan Peleg <ipeleg@hotmail.com>
2022-10-18 17:31:10 -0400

    feat: increase postgres connections pool and split health check connection

--- src/common/healthcheck.ts
+++ src/common/healthcheck.ts
@@ -1,11 +1,11 @@
-import { redb } from "@/common/db";
+import { hdb } from "@/common/db";
 import { logger } from "@/common/logger";
 import { redis } from "@/common/redis";
 
 export class HealthCheck {
   static async check(): Promise<boolean> {
     try {
-      await redb.query("SELECT 1");
+      await hdb.query("SELECT 1");
     } catch (error) {
       logger.error("healthcheck", `Postgres Healthcheck failed: ${error}`);
       return false;
