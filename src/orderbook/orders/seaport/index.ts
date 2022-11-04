ab0a22f43adc30bdc428283f9b354ba7a2158eb3
Idan Peleg <ipeleg@hotmail.com>
2022-11-04 19:03:42 -0400

    feat: set seaportBidPercentageThreshold to 90

--- src/orderbook/orders/seaport/index.ts
+++ src/orderbook/orders/seaport/index.ts
@@ -397,7 +397,7 @@ export const save = async (
       if (info.side === "buy" && order.params.kind === "single-token" && validateBidValue) {
         const typedInfo = info as typeof info & { tokenId: string };
         const tokenId = typedInfo.tokenId;
-        const seaportBidPercentageThreshold = 75;
+        const seaportBidPercentageThreshold = 90;
 
         try {
           const collectionFloorAskValue = await getCollectionFloorAskValue(
