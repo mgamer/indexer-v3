404cf940603516dc36c45def624d1c3de12171e2
George Roman <george.roman.99@gmail.com>
2022-11-09 14:11:26 +0100

    fix: filling partial seaport bids on the v5 router

--- src/api/endpoints/execute/get-execute-sell/v3.ts
+++ src/api/endpoints/execute/get-execute-sell/v3.ts
@@ -165,6 +165,7 @@ export const getExecuteSellV3Options: RouteOptions = {
 
       const bidDetails = await generateBidDetailsV5(
         {
+          id: bestOrderResult.id,
           kind: bestOrderResult.kind,
           rawData: bestOrderResult.raw_data,
         },
