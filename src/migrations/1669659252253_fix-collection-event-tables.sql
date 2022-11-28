be5a1f7a9dd15b7e4adc491f212bc67c195b919f
George Roman <george.roman.99@gmail.com>
2022-11-28 19:17:52 +0100

    fix: column name

--- src/migrations/1669659252253_fix-collection-event-tables.sql
+++ src/migrations/1669659252253_fix-collection-event-tables.sql
@@ -6,6 +6,6 @@ ALTER TABLE "collection_normalized_floor_sell_events"
 
 ALTER TABLE "collection_top_bid_events"
   ALTER "contract" DROP NOT NULL,
-  ALTER "token_id" DROP NOT NULL;
+  ALTER "token_set_id" DROP NOT NULL;
 
 -- Down Migration
