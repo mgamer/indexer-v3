4b5cf59cdd7fee6d4af7e44f374ef5bb4394c5eb
Idan Peleg <ipeleg@hotmail.com>
2022-12-02 11:33:46 -0500

    feat: update migration

--- src/migrations/1669990797123_tokens.sql
+++ src/migrations/1669990797123_tokens.sql
@@ -3,7 +3,6 @@
 ALTER TABLE "tokens" ADD COLUMN "last_flag_change" TIMESTAMPTZ;
 
 --CREATE INDEX "tokens_last_flag_change_is_flagged_index"
---  ON "tokens" ("last_flag_change" DESC NULLS LAST, "is_flagged")
---  INCLUDE ("contract", "token_id");
+--  ON "tokens" ("last_flag_change" DESC NULLS LAST, "is_flagged");
 
 -- Down Migration
