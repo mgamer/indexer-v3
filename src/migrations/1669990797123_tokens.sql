7062738709d5796655d2865ce265299098651ec5
Idan Peleg <ipeleg@hotmail.com>
2022-12-02 09:29:56 -0500

    feat: added last_flag_change

---
+++ src/migrations/1669990797123_tokens.sql
@@ -0,0 +1,9 @@
+-- Up Migration
+
+ALTER TABLE "tokens" ADD COLUMN "last_flag_change" TIMESTAMPTZ;
+
+--CREATE INDEX "tokens_last_flag_change_is_flagged_index"
+--  ON "tokens" ("last_flag_change", "is_flagged")
+--  INCLUDE ("contract", "token_id");
+
+-- Down Migration
