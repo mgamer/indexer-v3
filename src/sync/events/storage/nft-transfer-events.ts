a233d7e843cd74249ed6ced68e30d50d36b50d6a
Idan Peleg <ipeleg@hotmail.com>
2022-12-07 11:41:37 -0500

    feat: chunk realtime nft_transfer_events

--- src/sync/events/storage/nft-transfer-events.ts
+++ src/sync/events/storage/nft-transfer-events.ts
@@ -220,7 +220,9 @@ export const addEvents = async (events: Event[], backfill: boolean) => {
       // on the events to have been written to the database at the time
       // they get to run and we have no way to easily enforce this when
       // using the write buffer.
-      await idb.none(pgp.helpers.concat(queries));
+      for (const query of _.chunk(queries, 500)) {
+        await idb.none(pgp.helpers.concat(query));
+      }
     }
   }
 };
