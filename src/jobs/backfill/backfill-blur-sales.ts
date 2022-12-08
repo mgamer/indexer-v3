4c44aff5144bc7e0d4e16c74566c290e34a374e7
George Roman <george.roman.99@gmail.com>
2022-12-08 16:48:14 +0100

    fix: update lock

--- src/jobs/backfill/backfill-blur-sales.ts
+++ src/jobs/backfill/backfill-blur-sales.ts
@@ -178,7 +178,7 @@ if (config.doBackgroundWork) {
 
   if (config.chainId === 1) {
     redlock
-      .acquire([`${QUEUE_NAME}-lock-2`], 60 * 60 * 24 * 30 * 1000)
+      .acquire([`${QUEUE_NAME}-lock-3`], 60 * 60 * 24 * 30 * 1000)
       .then(async () => {
         await addToQueue(16140000);
       })
