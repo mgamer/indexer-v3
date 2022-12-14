7ef874fe8c3b8049cf8d75a178f1e92222579b38
George Roman <george.roman.99@gmail.com>
2022-12-14 10:06:54 +0100

    fix: increase completed jobs buffer size

--- src/jobs/order-updates/by-id-queue.ts
+++ src/jobs/order-updates/by-id-queue.ts
@@ -28,7 +28,7 @@ export const queue = new Queue(QUEUE_NAME, {
       type: "exponential",
       delay: 10000,
     },
-    removeOnComplete: 10000,
+    removeOnComplete: 100000,
     removeOnFail: 10000,
     timeout: 60000,
   },
