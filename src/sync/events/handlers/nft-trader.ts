97c0cda3944f0d37bf4e065a5676a50ff0c9d1cc
Boris Bonin <boris.bonin@limechain.tech>
2022-12-16 16:45:22 +0200

    fix: simplify nft-trader event logic

--- src/sync/events/handlers/nft-trader.ts
+++ src/sync/events/handlers/nft-trader.ts
@@ -20,9 +20,8 @@ export const handleEvents = async (events: EnhancedEvent[]): Promise<OnChainData
         const { args } = eventData.abi.parseLog(log);
         const status = args["status"];
         const taker = args["creator"].toLowerCase();
-        const orderId = args["swapId"];
 
-        //statuses:
+        // statuses:
         // 0 - opened
         // 1 - closed
         // 2 - canceled
@@ -38,114 +37,58 @@ export const handleEvents = async (events: EnhancedEvent[]): Promise<OnChainData
 
         const parsedTrace = parseCallTrace(txTrace.calls);
 
-        let tokenCounterA = 0;
-        let tokenCounterB = 0;
-        let addressesCounter = 0;
+        let transferedTokensCounter = 0;
         let tokenId = "";
         let tokenContract = "";
         let currency = "";
         let currencyPrice = "";
         let maker = "";
 
-        // {
-        //   '0x24deb03382400c993819df42f5adcf897f4f66b3': {
-        //     tokenBalanceState: {
-        //       'native:0x0000000000000000000000000000000000000000': '253700000000000000',
-        //       'erc721:0x64a1c0937728d8d2fa8cd81ef61a9c860b7362db:506': '1',
-        //       'erc721:0x64a1c0937728d8d2fa8cd81ef61a9c860b7362db:2140': '-1'
-        //     }
-        //   },
-        //   '0x657e383edb9a7407e468acbcc9fe4c9730c7c275': {
-        //     tokenBalanceState: {
-        //       'native:0x0000000000000000000000000000000000000000': '-265000000000000000'
-        //     }
-        //   },
-        //   '0xfe20d3b9419d37bc1a9c65fdd0a66109fffd5f43': {
-        //     tokenBalanceState: {
-        //       'erc721:0x64a1c0937728d8d2fa8cd81ef61a9c860b7362db:506': '-1',
-        //       'erc721:0x64a1c0937728d8d2fa8cd81ef61a9c860b7362db:2140': '1'
-        //     }
-        //   },
-        //   '0x83db44123e76503203fdf83d2be58be60c15b894': {
-        //     tokenBalanceState: {
-        //       'native:0x0000000000000000000000000000000000000000': '11300000000000000'
-        //     }
-        //   }
-        // }
-
-        // {
-        //   "0x10a89eacfc556e4f943050eafc350d3c1122836a": {
-        //     tokenBalanceState: {
-        //       "native:0x0000000000000000000000000000000000000000": "-3805000000000000000",
-        //       "erc721:0x231d3559aa848bf10366fb9868590f01d34bf240:1475": "1",
-        //     },
-        //   },
-        //   "0x657e383edb9a7407e468acbcc9fe4c9730c7c275": {
-        //     tokenBalanceState: {
-        //       "native:0x0000000000000000000000000000000000000000": "-5000000000000000",
-        //     },
-        //   },
-        //   "0xce9f867f70d1db3a37db2cda0d0eec099020f695": {
-        //     tokenBalanceState: {
-        //       "erc721:0x231d3559aa848bf10366fb9868590f01d34bf240:1475": "-1",
-        //       "native:0x0000000000000000000000000000000000000000": "3781000000000000000",
-        //     },
-        //   },
-        //   "0x83db44123e76503203fdf83d2be58be60c15b894": {
-        //     tokenBalanceState: {
-        //       "native:0x0000000000000000000000000000000000000000": "29000000000000000",
-        //     },
-        //   },
-        // }
-
         let transferToken = "";
         let amount = "0";
         let currencyType = "";
+
         for (const token of Object.keys(parsedTrace[taker].tokenBalanceState)) {
           if (token.startsWith("erc721") || token.startsWith("erc1155")) {
             transferToken = token;
-
+            transferedTokensCounter++;
             amount = parsedTrace[taker].tokenBalanceState[token];
+            [, tokenContract, tokenId] = token.split(":");
           } else if (token.startsWith("erc20") || token.startsWith("native")) {
-            currency = token.split(":")[1];
             currencyType = token;
+            currency = token.split(":")[1];
           }
         }
 
-        for (const address of Object.keys(parsedTrace)) {
-          if (address === baseEventParams.address) {
-            continue;
-          }
-
+        for (const address of Object.keys(parsedTrace).filter(
+          (key) => baseEventParams.address !== key
+        )) {
           for (const token of Object.keys(parsedTrace[address].tokenBalanceState)) {
-            if (token.startsWith("erc721") || token.startsWith("erc1155")) {
-              addressesCounter === 0 ? tokenCounterA++ : tokenCounterB++;
-              addressesCounter++;
-
-              if (address !== taker && token === transferToken) {
-                maker = address;
-              }
-
-              [, tokenContract, tokenId] = token.split(":");
+            if (address !== taker && token === transferToken) {
+              maker = address;
             }
           }
         }
 
-        //we don't support token for token exchange
-        //we don't support bundles
-        if ((tokenCounterA && tokenCounterB) || tokenCounterA > 1 || tokenCounterB > 1) {
+        // we don't support token for token exchange
+        // we don't support bundles
+        if (transferedTokensCounter !== 1) {
           break;
         }
 
-        if (bn(amount).gt(0)) {
-          currencyPrice = bn(parsedTrace[taker].tokenBalanceState[currencyType])
-            .add(bn(parsedTrace[baseEventParams.address].tokenBalanceState[currencyType]))
-            .toString();
-        } else {
-          currencyPrice = bn(parsedTrace[maker].tokenBalanceState[currencyType])
-            .add(bn(parsedTrace[baseEventParams.address].tokenBalanceState[currencyType]))
-            .toString();
-        }
+        // depending on the order side we have to calculate:
+        // price when order is "sale" = taker price paid + conctract fee
+        // price when order is "buy" = maker price received + concract fee
+        const orderSide = bn(amount).gt(0) ? "sell" : "buy";
+
+        currencyPrice = bn(
+          parsedTrace[orderSide === "sell" ? taker : maker].tokenBalanceState[currencyType]
+        )
+          .add(bn(parsedTrace[baseEventParams.address].tokenBalanceState[currencyType]))
+          .abs()
+          .toString();
+
+        amount = bn(amount).abs().toString();
 
         const priceData = await getUSDAndNativePrices(
           currency,
@@ -163,11 +106,10 @@ export const handleEvents = async (events: EnhancedEvent[]): Promise<OnChainData
           orderKind
         );
 
-        // break;
         fillEvents.push({
           orderKind,
           currency,
-          orderSide: bn(amount).gt(0) ? "sell" : "buy",
+          orderSide,
           maker,
           taker,
           price: priceData.nativePrice,
@@ -183,8 +125,8 @@ export const handleEvents = async (events: EnhancedEvent[]): Promise<OnChainData
         });
 
         fillInfos.push({
-          context: `nft-trader-${tokenContract}-${tokenId}-${orderId}-${baseEventParams.txHash}`,
-          orderSide: "buy",
+          context: `nft-trader-${tokenContract}-${tokenId}-${baseEventParams.txHash}`,
+          orderSide,
           contract: tokenContract,
           tokenId,
           amount,
