49f75fdced812382612476db03781e3ca5cfbf6e
Idan Peleg <ipeleg@hotmail.com>
2022-12-28 18:43:32 -0500

    feat: increase concurrency for polygon token refresh cache

--- src/jobs/token-updates/token-refresh-cache.ts
+++ src/jobs/token-updates/token-refresh-cache.ts
@@ -58,7 +58,7 @@ if (config.doBackgroundWork) {
         }
       }
     },
-    { connection: redis.duplicate(), concurrency: config.chainId === 137 ? 10 : 1 }
+    { connection: redis.duplicate(), concurrency: config.chainId === 137 ? 20 : 1 }
   );
   worker.on("error", (error) => {
     logger.error(QUEUE_NAME, `Worker errored: ${error}`);
