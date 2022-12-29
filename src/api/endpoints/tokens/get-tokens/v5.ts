7658748041fb911f204a8b42834acc8424351b3b
JadenDurnford <jadendurnford@gmail.com>
2022-12-29 14:30:32 -0500

    feat: return attribute kind from tokensv5 (#2842)

--- src/api/endpoints/tokens/get-tokens/v5.ts
+++ src/api/endpoints/tokens/get-tokens/v5.ts
@@ -161,6 +161,7 @@ export const getTokensV5Options: RouteOptions = {
               .items(
                 Joi.object({
                   key: Joi.string(),
+                  kind: Joi.string(),
                   value: Joi.string(),
                   tokenCount: Joi.number(),
                   onSaleCount: Joi.number(),
@@ -270,6 +271,7 @@ export const getTokensV5Options: RouteOptions = {
             array_agg(
               json_build_object(
                 'key', ta.key,
+                'kind', attributes.kind,
                 'value', ta.value,
                 'tokenCount', attributes.token_count,
                 'onSaleCount', attributes.on_sale_count,
@@ -853,6 +855,7 @@ export const getTokensV5Options: RouteOptions = {
               ? r.attributes
                 ? _.map(r.attributes, (attribute) => ({
                     key: attribute.key,
+                    kind: attribute.kind,
                     value: attribute.value,
                     tokenCount: attribute.tokenCount,
                     onSaleCount: attribute.onSaleCount,
