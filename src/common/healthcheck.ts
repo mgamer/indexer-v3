8cab4817d11135b21eacf326c0d821aad2f52312
nofir <42353301+nofir@users.noreply.github.com>
2023-01-05 15:46:00 -0500

    chore: disabled pg check (#2893)

--- src/common/healthcheck.ts
+++ src/common/healthcheck.ts
@@ -1,15 +1,14 @@
-import { hdb } from "@/common/db";
 import { logger } from "@/common/logger";
 import { redis } from "@/common/redis";
 
 export class HealthCheck {
   static async check(): Promise<boolean> {
-    try {
-      await hdb.query("SELECT 1");
-    } catch (error) {
-      logger.error("healthcheck", `Postgres Healthcheck failed: ${error}`);
-      return false;
-    }
+    // try {
+    //   await hdb.query("SELECT 1");
+    // } catch (error) {
+    //   logger.error("healthcheck", `Postgres Healthcheck failed: ${error}`);
+    //   return false;
+    // }
 
     try {
       await redis.ping();
