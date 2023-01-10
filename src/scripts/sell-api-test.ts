98174fcd9bcd769890ad919f0c481cd89075f2ed
Jaden Durnford <jadendurnford@gmail.com>
2023-01-10 16:40:04 -0500

    fix: reformatting to match script format

--- src/scripts/sell-api-test.ts
+++ src/scripts/sell-api-test.ts
@@ -6,10 +6,12 @@ import { config as dotEnvConfig } from "dotenv";
 
 dotEnvConfig();
 
-(async () => {
-  const collectionId = "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d";
-  const ownerList: [] = [];
-  let moreOwners = true;
+const collectionId = "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d";
+const ownerList: [] = [];
+let moreOwners = true;
+let i = 0;
+
+const main = async () => {
   while (moreOwners) {
     const {
       data: { owners },
@@ -31,112 +33,117 @@ dotEnvConfig();
 
   console.log("Owner fetch complete");
 
-  let i = 0;
+  return new Promise(ownerLoop);
+};
 
-  const ownerLoop = async (resolve: any, reject: any) => {
-    try {
-      if (i > ownerList.length) {
-        return resolve;
-      }
-
-      let moreBids = true;
-      let continuation: string | undefined;
-      let numberOfBids = 0;
-      const bidList: [] = [];
-
-      while (moreBids) {
-        const url = `https://api.reservoir.tools/orders/users/${
-          ownerList[i]["address"]
-        }/top-bids/v3?sortBy=topBidValue&sortDirection=desc&limit=100&collection=${collectionId}${
-          continuation ? "&continuation=" + continuation : ""
-        }
-    `;
-        const { data } = await axios.get(url, {
-          headers: {
-            "x-api-key": `${process.env.ADMIN_API_KEY}`,
-          },
-        });
-        const topBids = data.topBids;
-        const tempArray: [] = topBids.map((obj: any) => ({
-          ...obj,
-          owner: ownerList[i]["address"],
-        }));
-        bidList.push(...tempArray);
-        if (!data.continuation) {
-          numberOfBids = data.totalTokensWithBids;
-          moreBids = false;
-          break;
-        }
-        continuation = data.continuation;
-      }
-
-      console.log(
-        `Bid fetch complete, found ${numberOfBids} tokens with bids for ${ownerList[i]["address"]}`
-      );
-      console.log("Starting sell api test on the user's tokens...");
-
-      let j = 0;
-      const callInterval = setInterval(() => {
-        if (j < bidList.length) {
-          topBidSimulation(j, bidList, resolve, reject);
-          j++;
-        } else if (j >= bidList.length) {
-          clearInterval(callInterval);
-        }
-      }, 500);
-    } catch (err) {
-      console.log(
-        `Error fetching top bid orders for user ${ownerList[i]["address"]}, moving to next...`
-      );
-      i++;
-      setTimeout(ownerLoop, 1000, resolve, reject);
+async function ownerLoop(resolve: any, reject: any) {
+  try {
+    if (i > ownerList.length) {
+      return resolve;
     }
-  };
 
-  async function topBidSimulation(index: number, bidList: any, resolve: any, reject: any) {
-    try {
-      await axios.post(
-        "https://api.reservoir.tools/execute/sell/v6",
-        {
-          onlyPath: false,
-          normalizeRoyalties: false,
-          token: `${collectionId}:${bidList[index]["token"]["tokenId"]}`,
-          taker: bidList[index]["owner"],
+    let moreBids = true;
+    let continuation: string | undefined;
+    let numberOfBids = 0;
+    const bidList: [] = [];
+
+    while (moreBids) {
+      const url = `https://api.reservoir.tools/orders/users/${
+        ownerList[i]["address"]
+      }/top-bids/v3?sortBy=topBidValue&sortDirection=desc&limit=100&collection=${collectionId}${
+        continuation ? "&continuation=" + continuation : ""
+      }
+  `;
+      const { data } = await axios.get(url, {
+        headers: {
+          "x-api-key": `${process.env.ADMIN_API_KEY}`,
         },
-        {
-          headers: {
-            "x-api-key": `${process.env.ADMIN_API_KEY}`,
-          },
-        }
-      );
-      const {
-        data: { message },
-      } = await axios.post(
-        "https://api.reservoir.tools/tokens/simulate-top-bid/v1",
-        {
-          token: `${collectionId}:${bidList[index]["token"]["tokenId"]}`,
-        },
-        {
-          headers: {
-            "x-api-key": `${process.env.ADMIN_API_KEY}`,
-          },
-        }
-      );
-      if (message !== "Top bid order is fillable") {
-        console.log(`Error for token #${bidList[index]["token"]["tokenId"]}: ${message}`);
+      });
+      const topBids = data.topBids;
+      const tempArray: [] = topBids.map((obj: any) => ({
+        ...obj,
+        owner: ownerList[i]["address"],
+      }));
+      bidList.push(...tempArray);
+      if (!data.continuation) {
+        numberOfBids = data.totalTokensWithBids;
+        moreBids = false;
+        break;
       }
-    } catch (err: any) {
-      console.log(
-        `Error for token #${bidList[index]["token"]["tokenId"]}: ${err.response.data.message}`
-      );
+      continuation = data.continuation;
     }
 
-    if (index + 1 === bidList.length) {
-      console.log("\nFinished checking current user's tokens, proceeding to the next one...");
-      i++;
-      setTimeout(ownerLoop, 1000, resolve, reject);
+    console.log(
+      `Bid fetch complete, found ${numberOfBids} tokens with bids for ${ownerList[i]["address"]}`
+    );
+    console.log("Starting sell api test on the user's tokens...");
+
+    let j = 0;
+    const callInterval = setInterval(() => {
+      if (j < bidList.length) {
+        topBidSimulation(j, bidList, resolve, reject);
+        j++;
+      } else if (j >= bidList.length) {
+        clearInterval(callInterval);
+      }
+    }, 500);
+  } catch (err) {
+    console.log(
+      `Error fetching top bid orders for user ${ownerList[i]["address"]}, moving to next...`
+    );
+    i++;
+    setTimeout(ownerLoop, 1000, resolve, reject);
+  }
+}
+
+async function topBidSimulation(index: number, bidList: any, resolve: any, reject: any) {
+  try {
+    await axios.post(
+      "https://api.reservoir.tools/execute/sell/v6",
+      {
+        onlyPath: false,
+        normalizeRoyalties: false,
+        token: `${collectionId}:${bidList[index]["token"]["tokenId"]}`,
+        taker: bidList[index]["owner"],
+      },
+      {
+        headers: {
+          "x-api-key": `${process.env.ADMIN_API_KEY}`,
+        },
+      }
+    );
+    const {
+      data: { message },
+    } = await axios.post(
+      "https://api.reservoir.tools/tokens/simulate-top-bid/v1",
+      {
+        token: `${collectionId}:${bidList[index]["token"]["tokenId"]}`,
+      },
+      {
+        headers: {
+          "x-api-key": `${process.env.ADMIN_API_KEY}`,
+        },
+      }
+    );
+    if (message !== "Top bid order is fillable") {
+      console.log(`Error for token #${bidList[index]["token"]["tokenId"]}: ${message}`);
     }
+  } catch (err: any) {
+    console.log(
+      `Error for token #${bidList[index]["token"]["tokenId"]}: ${err.response.data.message}`
+    );
   }
 
-  return new Promise(ownerLoop);
-})();
+  if (index + 1 === bidList.length) {
+    console.log("\nFinished checking current user's tokens, proceeding to the next one...");
+    i++;
+    setTimeout(ownerLoop, 1000, resolve, reject);
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
