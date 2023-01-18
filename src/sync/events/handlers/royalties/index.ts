07b397b6434ad58ec55481f4bc3ddda898be831e
George Roman <george.roman.99@gmail.com>
2023-01-18 15:09:03 +0100

    fix: debug slow sale royalty detection

--- src/sync/events/handlers/royalties/index.ts
+++ src/sync/events/handlers/royalties/index.ts
@@ -21,7 +21,13 @@ export interface RoyaltyAdapter {
   extractRoyalties(fillEvent: es.fills.Event): Promise<RoyaltyResult | null>;
 }
 
+function parseHrtimeToSeconds(hrtime: [number, number]) {
+  return (hrtime[0] + hrtime[1] / 1e9).toFixed(3);
+}
+
 export async function extractOnChainData(enhancedEvents: EnhancedEvent[]) {
+  const startTime = process.hrtime();
+
   const allOnChainData: OnChainData[] = [];
 
   const eventBatches = extractEventsBatches(enhancedEvents, true);
@@ -30,35 +36,45 @@ export async function extractOnChainData(enhancedEvents: EnhancedEvent[]) {
     allOnChainData.push(onChainData);
   }
 
+  logger.info(
+    "debug",
+    `Time extractOnChainData: ${parseHrtimeToSeconds(process.hrtime(startTime))}`
+  );
+
   return allOnChainData;
 }
 
 export async function getFillEventsFromTx(txHash: string) {
+  const startTime = process.hrtime();
+
   const events = await getEnhancedEventsFromTx(txHash);
   const allOnChainData = await extractOnChainData(events);
 
   let fillEvents: es.fills.Event[] = [];
   for (let i = 0; i < allOnChainData.length; i++) {
     const data = allOnChainData[i];
-    const allEvents = concat(data.fillEvents, data.fillEventsPartial, data.fillEventsOnChain);
+    const allEvents = concat(
+      data.fillEvents,
+      data.fillEventsPartial,
+      data.fillEventsOnChain
+    ).filter((e) => e.orderKind !== "mint");
     fillEvents = [...fillEvents, ...allEvents];
   }
+
+  logger.info(
+    "debug",
+    `Time getFillEventsFromTx(${txHash}): ${parseHrtimeToSeconds(process.hrtime(startTime))}`
+  );
 
   return fillEvents;
 }
 
-function checkFeeIsValid(result: RoyaltyResult) {
-  return result.marketplaceFeeBps + result.royaltyFeeBps < 10000;
-}
+const checkFeeIsValid = (result: RoyaltyResult) =>
+  result.marketplaceFeeBps + result.royaltyFeeBps < 10000;
 
 export const assignRoyaltiesToFillEvents = async (fillEvents: es.fills.Event[]) => {
-  for (let index = 0; index < fillEvents.length; index++) {
-    // Skip mints
-    if (fillEvents[index].orderKind === "mint") {
-      continue;
-    }
-
-    const fillEvent = fillEvents[index];
+  for (let i = 0; i < fillEvents.length; i++) {
+    const fillEvent = fillEvents[i];
     const royaltyAdapter = registry.get(fillEvent.orderKind) ?? registry.get("fallback");
     try {
       if (royaltyAdapter) {
@@ -70,11 +86,12 @@ export const assignRoyaltiesToFillEvents = async (fillEvents: es.fills.Event[]) 
               `invalid royalties: marketplaceFeeBps=${result.marketplaceFeeBps}, royaltyFeeBps=${result.royaltyFeeBps}`
             );
           }
-          fillEvents[index].royaltyFeeBps = result.royaltyFeeBps;
-          fillEvents[index].marketplaceFeeBps = result.marketplaceFeeBps;
-          fillEvents[index].royaltyFeeBreakdown = result.royaltyFeeBreakdown;
-          fillEvents[index].marketplaceFeeBreakdown = result.marketplaceFeeBreakdown;
-          fillEvents[index].paidFullRoyalty = result.paidFullRoyalty;
+
+          fillEvents[i].royaltyFeeBps = result.royaltyFeeBps;
+          fillEvents[i].marketplaceFeeBps = result.marketplaceFeeBps;
+          fillEvents[i].royaltyFeeBreakdown = result.royaltyFeeBreakdown;
+          fillEvents[i].marketplaceFeeBreakdown = result.marketplaceFeeBreakdown;
+          fillEvents[i].paidFullRoyalty = result.paidFullRoyalty;
         }
       }
     } catch (error) {
