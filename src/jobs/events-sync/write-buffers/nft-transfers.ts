1d07b33fc8d6be1923d8572fee3fc86f16498e9f
Idan Peleg <ipeleg@hotmail.com>
2023-01-19 17:45:19 -0500

    feat: pause events-sync-nft-transfers-write on polygon

--- src/jobs/events-sync/write-buffers/nft-transfers.ts
+++ src/jobs/events-sync/write-buffers/nft-transfers.ts
@@ -25,7 +25,7 @@ export const queue = new Queue(QUEUE_NAME, {
 new QueueScheduler(QUEUE_NAME, { connection: redis.duplicate() });
 
 // BACKGROUND WORKER ONLY
-if (config.doBackgroundWork) {
+if (config.doBackgroundWork && config.chainId !== 137) {
   const worker = new Worker(
     QUEUE_NAME,
     async (job: Job) => {
