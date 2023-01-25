ec7bafcb91d2026c652064ffc250b9a08bf315ab
George Roman <george.roman.99@gmail.com>
2023-01-25 12:41:39 +0100

    fix: properly handle pool order token set cache updates

--- src/jobs/order-updates/by-id-queue.ts
+++ src/jobs/order-updates/by-id-queue.ts
@@ -123,7 +123,10 @@ if (config.doBackgroundWork) {
                   collection_id = token_sets.collection_id
                 FROM x
                 WHERE token_sets.id = x.token_set_id
-                  AND token_sets.top_buy_id IS DISTINCT FROM x.order_id
+                  AND (
+                    token_sets.top_buy_id IS DISTINCT FROM x.order_id
+                    OR token_sets.top_buy_value IS DISTINCT FROM x.value
+                  )
                 RETURNING
                   collection_id AS "collectionId",
                   attribute_id AS "attributeId",
