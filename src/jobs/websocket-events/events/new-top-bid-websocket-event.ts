428f5cd0ec1b91e106971f3f046c778b82da55ee
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-01-27 14:08:53 -0500

    Ofir/res 1728 user top bids websocket (#3175)
    * feat: added basic impl
    
    * chore: disable imports
    
    * fix: fixed import
    
    * feat: code structure changes
    
    * chore: added owners cache
    
    * fix: fixed logic
    
    * fix: fixed to trigger only on new order
    
    * chore: enable only for colleciton offers
    
    * feat: added more fields
    
    * feat: added user auth, added rate limit
    
    * chore: added api key validation
    
    * chore: added logs
    
    * chore: updated logs
    
    * fix: fix merge
    
    * chore: updated logs
    
    * chore: updated logs
    
    * chore: updated payload
    
    * chore: enabled for trait offers
    
    * feat: enable to bid for single token token sets
    
    * feat: added price normalized
    
    * chore: disable single token logic
    
    * feat: added tracing
    
    * fix: multiple fixes
    
    * chore: added log
    
    * feat: updated promise logic

--- src/jobs/websocket-events/events/new-top-bid-websocket-event.ts
+++ src/jobs/websocket-events/events/new-top-bid-websocket-event.ts
@@ -3,7 +3,6 @@ import * as Pusher from "pusher";
 import { fromBuffer, now } from "@/common/utils";
 import { Orders } from "@/utils/orders";
 import _ from "lodash";
-import { BatchEvent } from "pusher";
 import { config } from "@/config/index";
 import { redis } from "@/common/redis";
 import { logger } from "@/common/logger";
@@ -116,17 +115,24 @@ export class NewTopBidWebsocketEvent {
 
     const payloadsBatches = _.chunk(payloads, Number(config.websocketServerEventMaxBatchSize));
 
-    for (const payloadsBatch of payloadsBatches) {
-      const events: BatchEvent[] = payloadsBatch.map((payload) => {
-        return {
-          channel: "top-bids",
-          name: "new-top-bid",
-          data: JSON.stringify(payload),
-        };
-      });
+    await Promise.all(
+      payloadsBatches.map((payloadsBatch) =>
+        server.triggerBatch(
+          payloadsBatch.map((payload) => {
+            return {
+              channel: "top-bids",
+              name: "new-top-bid",
+              data: JSON.stringify(payload),
+            };
+          })
+        )
+      )
+    );
 
-      await server.triggerBatch(events);
-    }
+    logger.info(
+      "new-top-bid-websocket-event",
+      `End. orderId=${data.orderId}, tokenSetId=${order.token_set_id}, owners=${owners.length}, payloadsBatches=${payloadsBatches.length}`
+    );
   }
 
   static async getOwners(tokenSetId: string): Promise<string[]> {
--- src/jobs/websocket-events/trigger-queue.ts
+++ src/jobs/websocket-events/trigger-queue.ts
@@ -35,7 +35,7 @@ if (config.doBackgroundWork) {
         case EventKind.NewTopBid:
           await tracer.trace(
             "triggerEvent",
-            { resource: "NewTopBidWebsocketEvent", tags: data },
+            { resource: "NewTopBidWebsocketEvent", tags: { event: data } },
             () => NewTopBidWebsocketEvent.triggerEvent(data)
           );
           break;
