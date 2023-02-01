1ef6bb3127529df634918a159c7ca2aa3f8f6dee
Idan Peleg <ipeleg@hotmail.com>
2023-02-01 09:35:41 -0500

    feat: update default redis max mem

--- src/config/index.ts
+++ src/config/index.ts
@@ -86,7 +86,7 @@ export const config = {
 
   slackApiKeyWebhookUrl: String(process.env.SLACK_API_KEY_WEBHOOK_URL),
   // Used to prevent redis from being overloaded in heavy process like backfilling
-  redisMaxMemoryGB: Number(process.env.REDIS_MAX_MEMORY_GB || 22),
+  redisMaxMemoryGB: Number(process.env.REDIS_MAX_MEMORY_GB || 25),
 
   websocketServerHost: String(process.env.WEBSOCKET_SERVER_HOST),
   websocketServerAppId: String(process.env.WEBSOCKET_SERVER_APP_ID),
