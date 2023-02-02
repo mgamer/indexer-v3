3efa15d0939438ee4445cedbc1c8ffae8c97ebca
George Roman <george.roman.99@gmail.com>
2023-02-02 13:04:01 +0100

    feat: add support for delayed orders

--- src/orderbook/orders/seaport/index.ts
+++ src/orderbook/orders/seaport/index.ts
@@ -67,6 +67,7 @@ type SaveResult = {
   id: string;
   status: string;
   unfillable?: boolean;
+  delay?: number;
 };
 
 export const save = async (
@@ -147,6 +148,15 @@ export const save = async (
         return results.push({
           id,
           status: "invalid-start-time",
+        });
+      }
+
+      // Delay the validation of the order if it's start time is very soon in the future
+      if (startTime > currentTime) {
+        return results.push({
+          id,
+          status: "delayed",
+          delay: startTime - currentTime + 5,
         });
       }
 
@@ -776,6 +786,15 @@ export const save = async (
         });
       }
 
+      // Delay the validation of the order if it's start time is very soon in the future
+      if (startTime > currentTime) {
+        return results.push({
+          id,
+          status: "delayed",
+          delay: startTime - currentTime + 5,
+        });
+      }
+
       // Check: order is not expired
       const endTime = orderParams.endTime;
       if (currentTime >= endTime) {
@@ -1175,273 +1194,6 @@ export const save = async (
       throw error;
     }
   };
-
-  // const handleBundleOrder = async ({ orderParams, isReservoir, metadata }: OrderInfo) => {
-  //   try {
-  //     const order = new Sdk.Seaport.BundleOrder(config.chainId, orderParams);
-  //     const info = order.getInfo();
-  //     const id = order.hash();
-
-  //     // Check: order has a valid format
-  //     if (!info) {
-  //       return results.push({
-  //         id,
-  //         status: "invalid-format",
-  //       });
-  //     }
-
-  //     // Check: order doesn't already exist
-  //     const orderExists = await idb.oneOrNone(`SELECT 1 FROM orders WHERE orders.id = $/id/`, {
-  //       id,
-  //     });
-  //     if (orderExists) {
-  //       return results.push({
-  //         id,
-  //         status: "already-exists",
-  //       });
-  //     }
-
-  //     const currentTime = now();
-
-  //     // Check: order has a valid start time
-  //     const startTime = order.params.startTime;
-  //     if (startTime - 5 * 60 >= currentTime) {
-  //       // TODO: Add support for not-yet-valid orders
-  //       return results.push({
-  //         id,
-  //         status: "invalid-start-time",
-  //       });
-  //     }
-
-  //     // Check: order is not expired
-  //     const endTime = order.params.endTime;
-  //     if (currentTime >= endTime) {
-  //       return results.push({
-  //         id,
-  //         status: "expired",
-  //       });
-  //     }
-
-  //     // Check: order has a known zone
-  //     if (
-  //       ![
-  //         // No zone
-  //         AddressZero,
-  //         // Are these really used?
-  //         "0xf397619df7bfd4d1657ea9bdd9df7ff888731a11",
-  //         "0x9b814233894cd227f561b78cc65891aa55c62ad2",
-  //         // Pausable zone
-  //         Sdk.Seaport.Addresses.PausableZone[config.chainId],
-  //       ].includes(order.params.zone)
-  //     ) {
-  //       return results.push({
-  //         id,
-  //         status: "unsupported-zone",
-  //       });
-  //     }
-
-  //     // Check: order is valid
-  //     try {
-  //       order.checkValidity();
-  //     } catch {
-  //       return results.push({
-  //         id,
-  //         status: "invalid",
-  //       });
-  //     }
-
-  //     // Check: order has a valid signature
-  //     try {
-  //       order.checkSignature();
-  //     } catch (error) {
-  //       return results.push({
-  //         id,
-  //         status: "invalid-signature",
-  //       });
-  //     }
-
-  //     // Check: order fillability
-  //     let fillabilityStatus = "fillable";
-  //     let approvalStatus = "approved";
-  //     try {
-  //       await offChainCheckBundle(order, { onChainApprovalRecheck: true });
-  //       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //     } catch (error: any) {
-  //       // Keep any orders that can potentially get valid in the future
-  //       if (error.message === "no-balance-no-approval") {
-  //         fillabilityStatus = "no-balance";
-  //         approvalStatus = "no-approval";
-  //       } else if (error.message === "no-approval") {
-  //         approvalStatus = "no-approval";
-  //       } else if (error.message === "no-balance") {
-  //         fillabilityStatus = "no-balance";
-  //       } else {
-  //         return results.push({
-  //           id,
-  //           status: "not-fillable",
-  //         });
-  //       }
-  //     }
-
-  //     // TODO: Add support for non-token token sets
-  //     const tokenSets = await tokenSet.singleToken.save(
-  //       info.offerItems.map((item) => ({
-  //         id: `token:${item.contract}:${item.tokenId!}`,
-  //         schemaHash: generateSchemaHash(),
-  //         contract: item.contract,
-  //         tokenId: item.tokenId!,
-  //       }))
-  //     );
-
-  //     // TODO: Add support for consideration bundles
-  //     const offerBundle = await bundles.create(tokenSets.map(({ id }) => ({ kind: "nft", id })));
-
-  //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //     const currency = (info as any).paymentToken;
-  //     if (order.params.kind === "bundle-ask") {
-  //       // Check: order has a non-zero price
-  //       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //       if (bn((info as any).price).lte(0)) {
-  //         return results.push({
-  //           id,
-  //           status: "zero-price",
-  //         });
-  //       }
-  //     }
-
-  //     // Handle: price and value
-  //     let price = bn(order.getMatchingPrice());
-  //     const currencyPrice = price;
-  //     let value = price;
-
-  //     // Handle: fees
-  //     const feeAmount = order.getFeeAmount();
-
-  //     const feeBps = price.eq(0) ? bn(0) : feeAmount.mul(10000).div(price);
-  //     if (feeBps.gt(10000)) {
-  //       return results.push({
-  //         id,
-  //         status: "fees-too-high",
-  //       });
-  //     }
-
-  //     // Handle: fee breakdown
-  //     const openSeaFeeRecipients = [
-  //       "0x5b3256965e7c3cf26e11fcaf296dfc8807c01073",
-  //       "0x8de9c5a032463c561423387a9648c5c7bcc5bc90",
-  //     ];
-
-  //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //     const feeBreakdown = ((info as any) || []).fees.map(
-  //       ({ recipient, amount }: { recipient: string; amount: string }) => ({
-  //         kind: openSeaFeeRecipients.includes(recipient.toLowerCase()) ? "marketplace" : "royalty",
-  //         recipient,
-  //         bps: price.eq(0) ? 0 : bn(amount).mul(10000).div(price).toNumber(),
-  //       })
-  //     );
-
-  //     // Handle: source
-  //     const sources = await Sources.getInstance();
-  //     let source;
-
-  //     if (metadata.source) {
-  //       source = await sources.getOrInsert(metadata.source);
-  //     } else {
-  //       // If one of the fees is marketplace the source of the order is opensea
-  //       for (const fee of feeBreakdown) {
-  //         if (fee.kind == "marketplace") {
-  //           source = await sources.getOrInsert("opensea.io");
-  //           break;
-  //         }
-  //       }
-  //     }
-
-  //     // Handle: price conversion
-  //     {
-  //       const prices = await getUSDAndNativePrices(
-  //         currency,
-  //         price.toString(),
-  //         currentTime
-  //       );
-  //       if (!prices.nativePrice) {
-  //         // Getting the native price is a must
-  //         return results.push({
-  //           id,
-  //           status: "failed-to-convert-price",
-  //         });
-  //       }
-  //       price = bn(prices.nativePrice);
-  //     }
-  //     {
-  //       const prices = await getUSDAndNativePrices(
-  //         currency,
-  //         value.toString(),
-  //         currentTime
-  //       );
-  //       if (!prices.nativePrice) {
-  //         // Getting the native price is a must
-  //         return results.push({
-  //           id,
-  //           status: "failed-to-convert-price",
-  //         });
-  //       }
-  //       value = bn(prices.nativePrice);
-  //     }
-
-  //     const validFrom = `date_trunc('seconds', to_timestamp(${startTime}))`;
-  //     const validTo = endTime
-  //       ? `date_trunc('seconds', to_timestamp(${order.params.endTime}))`
-  //       : "'infinity'";
-  //     orderValues.push({
-  //       id,
-  //       kind: "seaport",
-  //       side: "bundle",
-  //       fillability_status: fillabilityStatus,
-  //       approval_status: approvalStatus,
-  //       token_set_id: null,
-  //       token_set_schema_hash: null,
-  //       offer_bundle_id: offerBundle,
-  //       consideration_bundle_id: undefined,
-  //       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //       bundle_kind: order.params.kind as any,
-  //       contract: null,
-  //       maker: toBuffer(order.params.offerer),
-  //       taker: toBuffer(info.taker),
-  //       price: price.toString(),
-  //       value: value.toString(),
-  //       currency: currency ? toBuffer(currency) : undefined,
-  //       currency_price: currencyPrice.toString(),
-  //       valid_between: `tstzrange(${validFrom}, ${validTo}, '[]')`,
-  //       nonce: order.params.counter,
-  //       source_id_int: source?.id,
-  //       is_reservoir: isReservoir ? isReservoir : null,
-  //       conduit: toBuffer(
-  //         new Sdk.Seaport.Exchange(config.chainId).deriveConduit(order.params.conduitKey)
-  //       ),
-  //       fee_breakdown: feeBreakdown,
-  //       fee_bps: feeBps.toNumber(),
-  //       raw_data: order.params,
-  //       dynamic: null,
-  //       expiration: validTo,
-  //     });
-
-  //     results.push({
-  //       id,
-  //       status: "success",
-  //       unfillable:
-  //         fillabilityStatus !== "fillable" || approvalStatus !== "approved" ? true : undefined,
-  //     });
-
-  //     if (relayToArweave) {
-  //       arweaveData.push({ order, source: source?.domain });
-  //     }
-  //   } catch (error) {
-  //     logger.error(
-  //       "orders-seaport-save-bundle",
-  //       `Failed to handle bundle order with params ${JSON.stringify(orderParams)}: ${error}`
-  //     );
-  //   }
-  // };
 
   // Process all orders concurrently
   const limit = pLimit(20);
