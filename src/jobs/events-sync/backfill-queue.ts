52ce8a953dae8b7643f97bda911f07ca1f986c29
Idan Peleg <ipeleg@hotmail.com>
2023-02-02 13:42:30 -0500

    feat: update events-sync-backfill concurrency

--- src/jobs/events-sync/backfill-queue.ts
+++ src/jobs/events-sync/backfill-queue.ts
@@ -63,7 +63,7 @@ if (config.doBackgroundWork && config.doEventsSyncBackfill) {
         throw error;
       }
     },
-    { connection: redis.duplicate(), concurrency: 8 }
+    { connection: redis.duplicate(), concurrency: 5 }
   );
   worker.on("error", (error) => {
     logger.error(QUEUE_NAME, `Worker errored: ${error}`);
