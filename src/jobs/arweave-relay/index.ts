e9534519aa1a6a7466217eefe3f4b4fd3575b478
George Roman <george.roman.99@gmail.com>
2023-02-02 14:56:31 +0100

    fix: build

--- src/jobs/arweave-relay/index.ts
+++ src/jobs/arweave-relay/index.ts
@@ -13,7 +13,7 @@ const PENDING_DATA_KEY = "pending-arweave-data";
 
 export const addPendingOrdersSeaport = async (
   data: {
-    order: Sdk.Seaport.Order | Sdk.Seaport.BundleOrder;
+    order: Sdk.Seaport.Order;
     schemaHash?: string;
     source?: string;
   }[]
