3f7de3fea78bec1e3f2505388ac81ed8ca0f7024
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-02-03 09:50:04 -0500

    chore: added tracing (#3252)

--- src/jobs/events-sync/realtime-queue.ts
+++ src/jobs/events-sync/realtime-queue.ts
@@ -8,6 +8,7 @@ import { config } from "@/config/index";
 import { getNetworkSettings } from "@/config/network";
 import { syncEvents } from "@/events-sync/index";
 import * as eventsSyncBackfill from "@/jobs/events-sync/backfill-queue";
+import tracer from "@/common/tracer";
 
 const QUEUE_NAME = "events-sync-realtime";
 
@@ -28,52 +29,57 @@ if (config.doBackgroundWork) {
   const worker = new Worker(
     QUEUE_NAME,
     async () => {
-      try {
-        // We allow syncing of up to `maxBlocks` blocks behind the head
-        // of the blockchain. If we lag behind more than that, then all
-        // previous blocks that we cannot cover here will be relayed to
-        // the backfill queue.
-        const maxBlocks = getNetworkSettings().realtimeSyncMaxBlockLag;
+      await tracer.trace("processEvent", { resource: "eventsSyncRealtime" }, async () => {
+        try {
+          // We allow syncing of up to `maxBlocks` blocks behind the head
+          // of the blockchain. If we lag behind more than that, then all
+          // previous blocks that we cannot cover here will be relayed to
+          // the backfill queue.
+          const maxBlocks = getNetworkSettings().realtimeSyncMaxBlockLag;
 
-        const headBlock = await baseProvider.getBlockNumber();
+          const headBlock = await baseProvider.getBlockNumber();
 
-        // Fetch the last synced blocked
-        let localBlock = Number(await redis.get(`${QUEUE_NAME}-last-block`));
-        if (localBlock >= headBlock) {
-          // Nothing to sync
-          return;
-        }
+          // Fetch the last synced blocked
+          let localBlock = Number(await redis.get(`${QUEUE_NAME}-last-block`));
+          if (localBlock >= headBlock) {
+            // Nothing to sync
+            return;
+          }
 
-        if (localBlock === 0) {
-          localBlock = headBlock;
-        } else {
-          localBlock++;
-        }
+          if (localBlock === 0) {
+            localBlock = headBlock;
+          } else {
+            localBlock++;
+          }
 
-        const fromBlock = Math.max(localBlock, headBlock - maxBlocks + 1);
-        logger.info(QUEUE_NAME, `Events realtime syncing block range [${fromBlock}, ${headBlock}]`);
-
-        await syncEvents(fromBlock, headBlock);
-
-        // Send any remaining blocks to the backfill queue
-        if (localBlock < fromBlock) {
+          const fromBlock = Math.max(localBlock, headBlock - maxBlocks + 1);
           logger.info(
             QUEUE_NAME,
-            `Out of sync: local block ${localBlock} and upstream block ${fromBlock}`
+            `Events realtime syncing block range [${fromBlock}, ${headBlock}]`
           );
-          await eventsSyncBackfill.addToQueue(localBlock, fromBlock - 1);
-        }
 
-        // To avoid missing any events, save the last synced block with a delay
-        // in order to ensure that the latest blocks will get queried more than
-        // once, which is exactly what we are looking for (since events for the
-        // latest blocks might be missing due to upstream chain reorgs):
-        // https://ethereum.stackexchange.com/questions/109660/eth-getlogs-and-some-missing-logs
-        await redis.set(`${QUEUE_NAME}-last-block`, headBlock - 5);
-      } catch (error) {
-        logger.error(QUEUE_NAME, `Events realtime syncing failed: ${error}`);
-        throw error;
-      }
+          await syncEvents(fromBlock, headBlock);
+
+          // Send any remaining blocks to the backfill queue
+          if (localBlock < fromBlock) {
+            logger.info(
+              QUEUE_NAME,
+              `Out of sync: local block ${localBlock} and upstream block ${fromBlock}`
+            );
+            await eventsSyncBackfill.addToQueue(localBlock, fromBlock - 1);
+          }
+
+          // To avoid missing any events, save the last synced block with a delay
+          // in order to ensure that the latest blocks will get queried more than
+          // once, which is exactly what we are looking for (since events for the
+          // latest blocks might be missing due to upstream chain reorgs):
+          // https://ethereum.stackexchange.com/questions/109660/eth-getlogs-and-some-missing-logs
+          await redis.set(`${QUEUE_NAME}-last-block`, headBlock - 5);
+        } catch (error) {
+          logger.error(QUEUE_NAME, `Events realtime syncing failed: ${error}`);
+          throw error;
+        }
+      });
     },
     { connection: redis.duplicate(), concurrency: 5 }
   );
