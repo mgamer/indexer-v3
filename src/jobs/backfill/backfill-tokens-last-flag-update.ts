9babff35aee6babe3dec5ddad520eb939954323a
Mihalis <Mihalis>
2023-02-03 09:57:09 +0200

    fix: update flag update query conditions

--- src/jobs/backfill/backfill-tokens-last-flag-update.ts
+++ src/jobs/backfill/backfill-tokens-last-flag-update.ts
@@ -30,7 +30,7 @@ if (config.doBackgroundWork) {
         const query = `
           UPDATE tokens
           SET last_flag_change = last_flag_update
-          WHERE (contract, token_id) IN (SELECT t.contract, t.token_id FROM tokens t WHERE t.last_flag_change IS NULL AND t.last_flag_update IS NOT NULL LIMIT ${limit});
+          WHERE (contract, token_id) IN (SELECT t.contract, t.token_id FROM tokens t WHERE t.last_flag_change IS NULL AND t.last_flag_update IS NOT NULL and t.is_flagged = 1 LIMIT ${limit});
         `;
         const { rowCount } = await idb.result(query);
         if (rowCount >= limit) {
