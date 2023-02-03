4bfa686ea1faa30eccf5016f9c944117746f3199
Idan Peleg <ipeleg@hotmail.com>
2023-02-03 08:37:47 -0500

    feat: clean failed jobs in events-sync-realtime

--- src/jobs/events-sync/realtime-queue.ts
+++ src/jobs/events-sync/realtime-queue.ts
@@ -83,6 +83,6 @@ if (config.doBackgroundWork) {
 }
 
 export const addToQueue = async () => {
-  await queue.clean(0, 100, "failed");
+  await queue.clean(0, 100, "failed"); // Due to bug with bullmq make sure failed messages are cleaned
   await queue.add(randomUUID(), {}, { jobId: QUEUE_NAME });
 };
