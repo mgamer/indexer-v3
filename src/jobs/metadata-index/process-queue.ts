e167906899532bf41a9381abe635fed7bf3e0989
Idan Peleg <ipeleg@hotmail.com>
2023-02-07 16:08:36 -0500

    feat: requeue if polygon

--- src/jobs/metadata-index/process-queue.ts
+++ src/jobs/metadata-index/process-queue.ts
@@ -63,10 +63,6 @@ if (config.doBackgroundWork) {
       // If no more tokens
       if (_.isEmpty(refreshTokens)) {
         await releaseLock(getLockName(method));
-        if (config.chainId === 137) {
-          await addToQueue(method);
-        }
-
         return;
       }
 
