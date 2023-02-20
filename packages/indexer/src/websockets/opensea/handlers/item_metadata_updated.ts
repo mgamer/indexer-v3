c0d5c6101c9b8650e1b4358e894e18c30ac2495e
Mihalis <Mihalis>
2023-02-21 00:05:17 +0200

    fix: refactor metadata stream handling

--- packages/indexer/src/websockets/opensea/handlers/item_metadata_updated.ts
+++ packages/indexer/src/websockets/opensea/handlers/item_metadata_updated.ts
@@ -1,6 +1,8 @@
 import { ItemMetadataUpdatePayload } from "@opensea/stream-js";
 import * as metadataIndexWrite from "@/jobs/metadata-index/write-queue";
 import { logger } from "@/common/logger";
+import { Tokens } from "@/models/tokens";
+import * as metadataIndexFetch from "@/jobs/metadata-index/fetch-queue";
 
 export const handleEvent = async (payload: ItemMetadataUpdatePayload | any): Promise<void> => {
   const [, contract, tokenId] = payload.item.nft_id.split("/");
@@ -10,16 +12,35 @@ export const handleEvent = async (payload: ItemMetadataUpdatePayload | any): Pro
     `Processing metadata ${JSON.stringify(payload)}`
   );
 
-  await metadataIndexWrite.addToQueue([
-    {
-      collection: contract,
-      contract,
-      tokenId,
-      name: payload.item.metadata.name ?? undefined,
-      description: payload.item.metadata.description ?? undefined,
-      imageUrl: payload.item.metadata.image_url ?? undefined,
-      mediaUrl: payload.item.metadata.animation_url ?? undefined,
-      attributes: [],
-    },
-  ]);
+  const token = await Tokens.getByContractAndTokenId(contract, tokenId);
+
+  if (token?.image) {
+    await metadataIndexFetch.addToQueue(
+      [
+        {
+          kind: "single-token",
+          data: {
+            method: metadataIndexFetch.getIndexingMethod("opensea"),
+            contract,
+            tokenId,
+            collection: token?.collectionId ?? contract,
+          },
+        },
+      ],
+      true
+    );
+  } else {
+    await metadataIndexWrite.addToQueue([
+      {
+        collection: contract,
+        contract,
+        tokenId,
+        name: payload.item.metadata.name ?? undefined,
+        description: payload.item.metadata.description ?? undefined,
+        imageUrl: payload.item.metadata.image_url ?? undefined,
+        mediaUrl: payload.item.metadata.animation_url ?? undefined,
+        attributes: [],
+      },
+    ]);
+  }
 };
