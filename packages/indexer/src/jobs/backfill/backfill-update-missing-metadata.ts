70767b7f88a117e2990d730c0ebcb54ce70ec342
Mihalis Plelis <mihalispl@gmail.com>
2023-02-21 11:40:42 +0200

    Fix update missing metadata query (#3393)
    * fix: update missing metadata logic
    
    * fix: update missing metadata logic
    
    * fix: update missing metadata logic
    
    * fix: update missing metadata logic
    
    ---------
    
    Co-authored-by: Mihalis <Mihalis>

--- packages/indexer/src/jobs/backfill/backfill-update-missing-metadata.ts
+++ packages/indexer/src/jobs/backfill/backfill-update-missing-metadata.ts
@@ -141,6 +141,11 @@ async function processCollectionTokens(
       lastTokenId = _.last(tokens).token_id;
     }
   }
+
+  if (unindexedTokens.length === 0) {
+    return;
+  }
+
   // push to tokens refresh queue
   const pendingRefreshTokens = new PendingRefreshTokens(indexingMethod);
   await pendingRefreshTokens.add(unindexedTokens);
@@ -153,6 +158,9 @@ async function processCollection(collection: {
   slug: string;
   tokenCount: number;
 }) {
+  if (collection.tokenCount === 0) {
+    return;
+  }
   const indexingMethod = getIndexingMethod(collection.community);
   const limit = Number(await redis.get(`${QUEUE_NAME}-tokens-limit`)) || 1000;
   if (!collection.slug) {
@@ -176,6 +184,9 @@ async function processCollection(collection: {
     `Processing collection with ID: ${collection.id}. Total tokens count: ${collection.tokenCount}, unindexed count: ${unindexedResult.unindexed_token_count}`
   );
 
+  if (unindexedResult.unindexed_token_count === 0) {
+    return;
+  }
   if (
     unindexedResult.unindexed_token_count / collection.tokenCount >
     missingMetadataPercentageThreshold
