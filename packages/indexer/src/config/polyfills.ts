44506dc64189e61950d5063a21e4088c146df3b1
George Roman <george.roman.99@gmail.com>
2023-02-23 11:04:41 +0100

    fix: polyfill fetch

--- packages/indexer/src/config/polyfills.ts
+++ packages/indexer/src/config/polyfills.ts
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+// Reference: https://github.com/mswjs/msw/issues/686#issuecomment-816241875
+import "node-fetch";
+
 // Reference: https://github.com/apollographql/apollo-link-rest/issues/41#issuecomment-354923559
 import * as f from "node-fetch";
 (global as any).Headers = f.Headers;
