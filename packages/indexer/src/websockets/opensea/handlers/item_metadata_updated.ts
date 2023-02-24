480cd61e904a223cfa07257186dd021efe8968bb
Mihalis <Mihalis>
2023-02-24 13:11:17 +0200

    feat: parse stream metadata through metadata api

--- packages/indexer/src/websockets/opensea/handlers/item_metadata_updated.ts
+++ packages/indexer/src/websockets/opensea/handlers/item_metadata_updated.ts
@@ -4,6 +4,7 @@ import { logger } from "@/common/logger";
 import { Tokens } from "@/models/tokens";
 import * as metadataIndexFetch from "@/jobs/metadata-index/fetch-queue";
 import { Collections } from "@/models/collections";
+import MetadataApi from "@/utils/metadata-api";
 
 export const handleEvent = async (payload: ItemMetadataUpdatePayload | any): Promise<void> => {
   const [, contract, tokenId] = payload.item.nft_id.split("/");
@@ -32,30 +33,26 @@ export const handleEvent = async (payload: ItemMetadataUpdatePayload | any): Pro
       true
     );
   } else if (token != null) {
-    await metadataIndexWrite.addToQueue([
-      {
-        collection: contract,
-        contract,
-        tokenId,
-        name: payload.item.metadata.name ?? undefined,
-        description: payload.item.metadata.description ?? undefined,
-        imageUrl: payload.item.metadata.image_url ?? undefined,
-        mediaUrl: payload.item.metadata.animation_url ?? undefined,
-        attributes: payload.item.metadata.traits.map(
-          (trait: {
-            display_type?: any;
-            max_value?: any;
-            order?: any;
-            trait_count: 0;
-            trait_type: string;
-            value: string | number;
-          }) => ({
-            key: trait.trait_type,
-            value: trait.value,
-            kind: typeof trait.value,
-          })
-        ),
+    const request = {
+      asset_contract: {
+        address: "0x059edd72cd353df5106d2b9cc5ab83a52287ac3a",
       },
-    ]);
+      token_id: tokenId,
+      name: payload.item.metadata.name ?? undefined,
+      description: payload.item.metadata.description ?? undefined,
+      image_url: payload.item.metadata.image_url ?? undefined,
+      animation_url: payload.item.metadata.animation_url ?? undefined,
+      traits: payload.item.metadata.traits,
+    };
+    const metadata = await MetadataApi.parseTokenMetadata(request);
+
+    logger.info(
+      "opensea-websocket-ITEM_METADATA_UPDATED",
+      `Response from metadata API: ${JSON.stringify(metadata)}`
+    );
+
+    if (metadata) {
+      await metadataIndexWrite.addToQueue([metadata]);
+    }
   }
 };
