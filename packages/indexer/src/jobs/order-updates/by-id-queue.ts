9b1a3e4c2711f4f36544deff73bc8bbeffee8e9a
Idan Peleg <ipeleg@hotmail.com>
2023-02-28 13:33:37 -0500

    feat: change removeOnComplete

--- packages/indexer/src/jobs/order-updates/by-id-queue.ts
+++ packages/indexer/src/jobs/order-updates/by-id-queue.ts
@@ -29,8 +29,8 @@ export const queue = new Queue(QUEUE_NAME, {
       type: "exponential",
       delay: 10000,
     },
-    removeOnComplete: 100000,
-    removeOnFail: 10000,
+    removeOnComplete: 1000,
+    removeOnFail: 1000,
     timeout: 60000,
   },
 });
