0baea6bdb73690f46c5a2aebba6b6d88683ef025
George Roman <george.roman.99@gmail.com>
2023-03-02 13:27:31 +0100

    chore: add cancellation zone address on mainnet

--- packages/sdk/src/seaport-v1.4/addresses.ts
+++ packages/sdk/src/seaport-v1.4/addresses.ts
@@ -47,5 +47,6 @@ export const PausableZone: ChainIdToAddress = {};
 export const ApprovalOrderZone: ChainIdToAddress = {};
 
 export const CancellationZone: ChainIdToAddress = {
+  [Network.Ethereum]: "0xe1066481cc3b038badd0c68dfa5c8f163c3ff192",
   [Network.EthereumGoerli]: "0x49b91d1d7b9896d28d370b75b92c2c78c1ac984a",
 };
