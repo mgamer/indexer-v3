6ab8e7335aa796d7b954d977d2a09aa05a5c1259
d3or <deorrunner@gmail.com>
2023-03-02 10:34:58 -0500

    fix: remove v6, make changes to v5

--- packages/indexer/src/api/endpoints/activities/index.ts
+++ packages/indexer/src/api/endpoints/activities/index.ts
@@ -3,7 +3,6 @@ export * from "@/api/endpoints/activities/get-collection-activity/v2";
 export * from "@/api/endpoints/activities/get-collection-activity/v3";
 export * from "@/api/endpoints/activities/get-collection-activity/v4";
 export * from "@/api/endpoints/activities/get-collection-activity/v5";
-export * from "@/api/endpoints/activities/get-collection-activity/v6";
 export * from "@/api/endpoints/activities/get-token-activity/v1";
 export * from "@/api/endpoints/activities/get-token-activity/v2";
 export * from "@/api/endpoints/activities/get-token-activity/v3";
