d7d0705b61b89584991b9965ff5b0172cfdd4314
Filipe Mota <filipe@unevenlabs.com>
2023-03-07 16:11:50 +0100

    pipeline
    Signed-off-by: Filipe Mota <filipe@unevenlabs.com>

--- release.config.js
+++ release.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   "dryRun": false,
-  "branches": ["main"],
+  "branches": ["pipeline"],
   "plugins": [
     "@semantic-release/commit-analyzer",
-    // "@semantic-release/release-notes-generator",
-    // [
-    //   "@semantic-release/changelog",
-    //   {
-    //     "changelogFile": "docs/CHANGELOG.md"
-    //   }
-    // ],
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "docs/CHANGELOG.md"
+      }
+    ],
     // "@semantic-release/npm",
     // "@semantic-release/github",
     // [
