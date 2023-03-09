d3a586da75c8dd124d99f9e140dbbbc1c767de26
Idan Peleg <ipeleg@hotmail.com>
2023-03-09 18:02:33 -0500

    feat: don't force seaport 1.4

--- packages/indexer/src/api/endpoints/execute/get-execute-list/v4.ts
+++ packages/indexer/src/api/endpoints/execute/get-execute-list/v4.ts
@@ -206,10 +206,6 @@ export const getExecuteListV4Options: RouteOptions = {
         const params = payload.params[i];
         const [contract, tokenId] = params.token.split(":");
 
-        if (params.orderKind === "seaport") {
-          params.orderKind = "seaport-v1.4";
-        }
-
         // For now, ERC20 listings are only supported on Seaport
         if (
           params.orderKind !== "seaport" &&
