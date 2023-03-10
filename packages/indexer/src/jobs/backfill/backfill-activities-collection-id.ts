644c772ba4ad43ff6f20e9b88b53fa31d95bc62c
tv <tv3636@gmail.com>
2023-03-10 12:30:56 -0800

    fix: remove unused cursor

--- packages/indexer/src/jobs/backfill/backfill-activities-collection-id.ts
+++ packages/indexer/src/jobs/backfill/backfill-activities-collection-id.ts
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { Job, Queue, QueueScheduler, Worker } from "bullmq";
+import { Queue, QueueScheduler, Worker } from "bullmq";
 import { randomUUID } from "crypto";
-import _ from "lodash";
 
 import { idb } from "@/common/db";
 import { logger } from "@/common/logger";
@@ -25,19 +24,8 @@ new QueueScheduler(QUEUE_NAME, { connection: redis.duplicate() });
 if (config.doBackgroundWork) {
   const worker = new Worker(
     QUEUE_NAME,
-    async (job: Job) => {
-      let cursor = job.data.cursor as CursorInfo;
-
+    async () => {
       const limit = 1000;
-
-      if (!cursor) {
-        const cursorJson = await redis.get(`${QUEUE_NAME}-next-cursor`);
-
-        if (cursorJson) {
-          cursor = JSON.parse(cursorJson);
-        }
-      }
-
       const results = await idb.manyOrNone(
         `
               UPDATE activities SET
@@ -47,37 +35,21 @@ if (config.doBackgroundWork) {
                 FROM activities a JOIN orders o ON o.id = a.order_id 
                 WHERE a.collection_id = 'collection-non-flagged'
                 AND a.type = 'bid'
-                LIMIT $/limit/
+                LIMIT 1000
               ) x
               WHERE activities.id = x.activityId
               RETURNING activities.id
           `,
         {
-          activityId: cursor?.activityId,
           limit,
         }
       );
 
-      let nextCursor;
-
       if (results.length == limit) {
-        const lastResult = _.last(results);
-
-        nextCursor = {
-          activityId: lastResult.id,
-        };
-
-        await redis.set(`${QUEUE_NAME}-next-cursor`, JSON.stringify(nextCursor));
-
-        await addToQueue(nextCursor);
+        await addToQueue();
       }
 
-      logger.info(
-        QUEUE_NAME,
-        `Processed ${results.length} activities.  limit=${limit}, cursor=${JSON.stringify(
-          cursor
-        )}, nextCursor=${JSON.stringify(nextCursor)}`
-      );
+      logger.info(QUEUE_NAME, `Processed ${results.length} activities. limit=${limit}`);
     },
     { connection: redis.duplicate(), concurrency: 1 }
   );
@@ -87,10 +59,6 @@ if (config.doBackgroundWork) {
   });
 }
 
-export type CursorInfo = {
-  activityId: number;
-};
-
-export const addToQueue = async (cursor?: CursorInfo) => {
-  await queue.add(randomUUID(), { cursor }, { delay: 1000 });
+export const addToQueue = async () => {
+  await queue.add(randomUUID(), {}, { delay: 1000 });
 };
