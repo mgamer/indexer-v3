b40efe9c1cca066d5149208891813c9a46122700
George Roman <george.roman.99@gmail.com>
2023-03-13 13:08:21 +0100

    fix: listing simulation

--- packages/indexer/src/api/endpoints/management/post-simulate-order/v1.ts
+++ packages/indexer/src/api/endpoints/management/post-simulate-order/v1.ts
@@ -157,6 +157,11 @@ export const postSimulateOrderV1Options: RouteOptions = {
           return { message: "Nothing to simulate" };
         }
 
+        const saleData = parsedPayload.steps[2].items[0]?.data;
+        if (!saleData) {
+          return { message: "Nothing to simulate" };
+        }
+
         const pathItem = parsedPayload.path[0];
 
         const { result: success, callTrace } = await ensureBuyTxSucceeds(
@@ -167,8 +172,7 @@ export const postSimulateOrderV1Options: RouteOptions = {
             tokenId: pathItem.tokenId as string,
             amount: pathItem.quantity as string,
           },
-          // Step 0 is the approval transaction
-          parsedPayload.steps[1].items[0].data
+          saleData
         );
         if (success) {
           // active -> inactive
