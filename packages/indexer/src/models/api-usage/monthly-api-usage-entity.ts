d2a4d0fe39ad6efd38e97a1c4ef90a33df9c3529
Idan Peleg <ipeleg@hotmail.com>
2023-03-21 14:30:42 -0400

    feat: update models

--- packages/indexer/src/models/api-usage/monthly-api-usage-entity.ts
+++ packages/indexer/src/models/api-usage/monthly-api-usage-entity.ts
@@ -2,6 +2,8 @@ export type MonthlyApiUsageEntityParams = {
   month: string;
   route: string;
   api_calls_count: number;
+  status_code: number;
+  points: number;
   api_key: string;
 };
 
@@ -9,12 +11,16 @@ export class MonthlyApiUsageEntity {
   month: string;
   route: string;
   apiCallsCount: number;
+  statusCode: number;
+  points: number;
   apiKey: string;
 
   constructor(params: MonthlyApiUsageEntityParams) {
     this.month = params.month;
     this.route = params.route;
     this.apiCallsCount = params.api_calls_count;
+    this.statusCode = params.status_code;
+    this.points = params.points;
     this.apiKey = params.api_key;
   }
 }
