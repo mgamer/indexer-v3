d160526aa6c4407d38349472174e91a65bee8701
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-03-23 16:47:08 -0400

    Fix/disable trait offers (#3762)
    * fix: diabled trait offers
    
    * fix: enabled trait offers

--- packages/indexer/src/websockets/opensea/index.ts
+++ packages/indexer/src/websockets/opensea/index.ts
@@ -54,7 +54,7 @@ if (config.doWebsocketWork && config.openSeaApiKey) {
       EventType.ITEM_LISTED,
       EventType.ITEM_RECEIVED_BID,
       EventType.COLLECTION_OFFER,
-      // EventType.TRAIT_OFFER,
+      EventType.TRAIT_OFFER,
     ],
     async (event) => {
       try {
