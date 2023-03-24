0f5bca0242c762772ae1d606e328dfbe4bd87464
Idan Peleg <ipeleg@hotmail.com>
2023-03-24 09:09:06 -0400

    feat: change metrics api to be an admin and add clearing tables cron

--- packages/indexer/src/api/endpoints/api-keys/index.ts
+++ packages/indexer/src/api/endpoints/api-keys/index.ts
@@ -1,3 +1,2 @@
 export * from "@/api/endpoints/api-keys/post-api-key";
 export * from "@/api/endpoints/api-keys/get-api-key-rate-limits";
-export * from "@/api/endpoints/api-keys/get-api-key-metrics";
