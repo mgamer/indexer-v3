03edf076f186cfc134453abf8fa4b75fcc26523c
Idan Peleg <ipeleg@hotmail.com>
2023-03-29 12:10:28 -0400

    fix: api tag

--- packages/indexer/src/api/endpoints/collections/get-collection-supported-marketplaces/v1.ts
+++ packages/indexer/src/api/endpoints/collections/get-collection-supported-marketplaces/v1.ts
@@ -30,7 +30,7 @@ const version = "v1";
 export const getCollectionSupportedMarketplacesV1Options: RouteOptions = {
   description: "Collections",
   notes: "Supported marketplaces by collection",
-  tags: ["api"],
+  tags: ["api", "Collections"],
   plugins: {
     "hapi-swagger": {
       order: 5,
