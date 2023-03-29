8da84204a7f4a91ed87e01a069e66a290b3b9657
Joey <joey.xf@gmail.com>
2023-03-29 10:52:00 +0800

    feat: add a ConduitController class
    1. seaport protocol should share the same conduit controller
    2. conduit key always has value, so no need to getConduit from chain
    just deriveConduit is ok

---
+++ packages/sdk/src/seaport-base/conduit-controller.ts
@@ -0,0 +1,30 @@
+import { Contract } from "@ethersproject/contracts";
+import * as Addresses from "./addresses";
+import ConduitControllerAbi from "./abis/ConduitController.json";
+import { keccak256 as solidityKeccak256 } from "@ethersproject/solidity";
+import { HashZero } from "@ethersproject/constants";
+
+export class ConduitController {
+  public chainId: number;
+  public contract: Contract;
+
+  constructor(chainId: number) {
+    this.chainId = chainId;
+    this.contract = new Contract(Addresses.ConduitController[this.chainId], ConduitControllerAbi);
+  }
+
+  public deriveConduit(conduitKey: string, defaultConduit: string) {
+    return conduitKey === HashZero
+      ? defaultConduit
+      : "0x" +
+          solidityKeccak256(
+            ["bytes1", "address", "bytes32", "bytes32"],
+            [
+              "0xff",
+              Addresses.ConduitController[this.chainId],
+              conduitKey,
+              Addresses.ConduitControllerCodeHash[this.chainId],
+            ]
+          ).slice(-40);
+  }
+}
