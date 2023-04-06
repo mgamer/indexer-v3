bf7540da4f0c560368b9578d24ae65912615e6cb
Joey <joey.xf@gmail.com>
2023-04-06 14:57:50 +0800

    fix: remove unnecessary comment

--- packages/sdk/src/alienswap/index.ts
+++ packages/sdk/src/alienswap/index.ts
@@ -2,5 +2,4 @@ import * as Addresses from "./addresses";
 import { Exchange } from "./exchange";
 import { Order } from "./order";
 
-// TODO: @Joey, use real addresses and abi
 export { Addresses, Exchange, Order };
