fb8fa9dd71b130a812dbb3eafa740b05a420d900
George Roman <george.roman.99@gmail.com>
2023-04-06 16:34:25 +0200

    fix: blur bid ingestion

--- packages/indexer/src/websockets/blur/index.ts
+++ packages/indexer/src/websockets/blur/index.ts
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 
 import { logger } from "@/common/logger";
 import { config } from "@/config/index";
-// import * as orderbook from "@/jobs/orderbook/orders-queue";
+import * as orderbook from "@/jobs/orderbook/orders-queue";
 
 const COMPONENT = "blur-websocket";
 
@@ -31,18 +31,18 @@ if (config.doWebsocketWork && config.blurWsUrl && config.blurWsApiKey) {
 
       if (parsedMessage.contractAddress === "0x19b86299c21505cdf59ce63740b240a9c822b5e4") {
         logger.info(COMPONENT, JSON.stringify(parsedMessage));
-        // await orderbook.addToQueue([
-        //   {
-        //     kind: "blur-bid",
-        //     info: {
-        //       orderParams: {
-        //         collection: parsedMessage.contractAddress.toLowerCase(),
-        //         pricePoints: parsedMessage.updates,
-        //       },
-        //       metadata: {},
-        //     },
-        //   },
-        // ]);
+        await orderbook.addToQueue([
+          {
+            kind: "blur-bid",
+            info: {
+              orderParams: {
+                collection: parsedMessage.contractAddress.toLowerCase(),
+                pricePoints: parsedMessage.updates,
+              },
+              metadata: {},
+            },
+          },
+        ]);
       }
     } catch (error) {
       logger.error(COMPONENT, `Error handling bid: ${error} (message = ${message})`);
