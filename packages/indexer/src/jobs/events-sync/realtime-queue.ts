0d24563b61b58b93bd3c3272565eae856bacb80c
Idan Peleg <ipeleg@hotmail.com>
2023-04-07 00:49:39 -0400

    feat: get lock for mainnet

--- packages/indexer/src/jobs/events-sync/realtime-queue.ts
+++ packages/indexer/src/jobs/events-sync/realtime-queue.ts
@@ -1,5 +1,6 @@
 import { Queue, QueueScheduler, Worker } from "bullmq";
 import { randomUUID } from "crypto";
+
 import { logger } from "@/common/logger";
 import { baseProvider } from "@/common/provider";
 import { redis } from "@/common/redis";
