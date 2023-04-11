e4bf27792e49d3f423a08cca79e3545333c47203
George Roman <george.roman.99@gmail.com>
2023-04-11 08:40:46 +0200

    refactor: tweaks

--- packages/indexer/src/migrations/1680570377136_looksrare-v2-subset-nonces.sql
+++ packages/indexer/src/migrations/1680570377136_looksrare-v2-subset-nonces.sql
@@ -17,8 +17,7 @@ ALTER TABLE "looksrare_v2_subset_nonce_cancel_events"
   ADD CONSTRAINT "looksrare_v2_subset_nonce_cancel_events_pk"
   PRIMARY KEY ("tx_hash", "log_index", "batch_index");
 
-
-CREATE INDEX "looksrare_v2_subset_nonce_cancel_events_order_kind_maker_nonce_index"
+CREATE INDEX "looksrare_v2_subset_nonce_cancel_events_maker_nonce_index"
   ON "looksrare_v2_subset_nonce_cancel_events" ("maker", "nonce");
 
 -- Down Migration
