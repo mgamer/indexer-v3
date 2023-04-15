0a24948a42f9fb0a4d0d597f05bdd77e2a61aaa7
George Roman <george.roman.99@gmail.com>
2023-04-15 10:57:17 +0300

    fix: add regular refresh for blur bids

---
+++ packages/indexer/src/jobs/order-updates/misc/blur-bids-refresh.ts
@@ -0,0 +1,78 @@
+import * as Sdk from "@reservoir0x/sdk";
+import axios from "axios";
+import { Job, Queue, QueueScheduler, Worker } from "bullmq";
+
+import { logger } from "@/common/logger";
+import { redis } from "@/common/redis";
+import { config } from "@/config/index";
+import * as orderbook from "@/jobs/orderbook/orders-queue";
+
+const QUEUE_NAME = "blur-bids-refresh";
+
+export const queue = new Queue(QUEUE_NAME, {
+  connection: redis.duplicate(),
+  defaultJobOptions: {
+    attempts: 1,
+    backoff: {
+      type: "exponential",
+      delay: 10000,
+    },
+    removeOnComplete: 1000,
+    removeOnFail: 10000,
+    timeout: 60000,
+  },
+});
+new QueueScheduler(QUEUE_NAME, { connection: redis.duplicate() });
+
+// BACKGROUND WORKER ONLY
+if (config.doBackgroundWork) {
+  const worker = new Worker(
+    QUEUE_NAME,
+    async (job: Job) => {
+      const { collection } = job.data as { collection: string };
+
+      try {
+        const pricePoints = await axios
+          .get(`${config.orderFetcherBaseUrl}/api/blur-collection-bids?collection=${collection}`)
+          .then((result) => result.data.bids as Sdk.Blur.Types.BlurBidPricePoint[]);
+
+        await orderbook.addToQueue([
+          {
+            kind: "blur-bid",
+            info: {
+              orderParams: {
+                collection,
+                pricePoints,
+              },
+              metadata: {},
+              fullUpdate: true,
+            },
+          },
+        ]);
+      } catch (error) {
+        logger.error(
+          QUEUE_NAME,
+          `Failed to refresh Blur bids for collection ${collection}: ${error}`
+        );
+        throw error;
+      }
+    },
+    { connection: redis.duplicate(), concurrency: 20 }
+  );
+  worker.on("error", (error) => {
+    logger.error(QUEUE_NAME, `Worker errored: ${error}`);
+  });
+}
+
+export const addToQueue = async (collection: string) =>
+  queue.add(
+    collection,
+    { collection },
+    {
+      jobId: collection,
+      repeat: {
+        // Run every 5 minutes
+        every: 5 * 60 * 1000,
+      },
+    }
+  );
