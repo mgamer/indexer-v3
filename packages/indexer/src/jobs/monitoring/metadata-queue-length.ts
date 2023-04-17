15655c602ea4889f49e702ae1cd588f684416e32
tv <tv3636@gmail.com>
2023-04-17 09:39:23 -0700

    fix: update pendingRefreshTokens and fix import

--- packages/indexer/src/jobs/monitoring/metadata-queue-length.ts
+++ packages/indexer/src/jobs/monitoring/metadata-queue-length.ts
@@ -1,12 +1,15 @@
 import cron from "node-cron";
-import { redis } from "@/common/redis";
 import { config } from "@/config/index";
 import { logger } from "@/common/logger";
+import { PendingRefreshTokens } from "@/models/pending-refresh-tokens";
 
 if (config.doBackgroundWork) {
   // Every 5 minutes log metadata queue length
   cron.schedule("*/5 * * * *", async () => {
-    const queueLength = await redis.llen("pending-refresh-tokens:opensea");
-    logger.info("metadata-queue-length", JSON.stringify({ queueLength }));
+    for (const method of ["opensea"]) {
+      const pendingRefreshTokens = new PendingRefreshTokens(method);
+      const queueLength = await pendingRefreshTokens.length();
+      logger.info("metadata-queue-length", JSON.stringify({ method, queueLength }));
+    }
   });
 }
