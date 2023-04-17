ced206b12c5a70dfe937856ee26047a6a00b7495
pedromcunha <pedro.spinsomewebs@gmail.com>
2023-04-17 17:59:43 -0400

    fix: sales continuation

--- packages/indexer/src/api/endpoints/transfers/get-sales/v4.ts
+++ packages/indexer/src/api/endpoints/transfers/get-sales/v4.ts
@@ -190,7 +190,7 @@ export const getSalesV4Options: RouteOptions = {
       `;
       } else if (query.orderBy && query.orderBy === "updated_at") {
         paginationFilter = `
-        AND (updated_ts, fill_events_2.log_index, fill_events_2.batch_index) ${inequalitySymbol} ($/timestamp/, $/logIndex/, $/batchIndex/)
+        AND (extract(epoch from fill_events_2.updated_at), fill_events_2.log_index, fill_events_2.batch_index) ${inequalitySymbol} ($/timestamp/, $/logIndex/, $/batchIndex/)
         `;
       } else {
         paginationFilter = `
