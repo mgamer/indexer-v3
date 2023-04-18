d6245d7f3a33f0c8ad739c054ab2869524876cbb
Jaden <jadendurnford@gmail.com>
2023-04-18 16:54:44 -0400

    feat: adding sort by updated_at

--- packages/indexer/src/api/endpoints/orders/get-orders-asks/v4.ts
+++ packages/indexer/src/api/endpoints/orders/get-orders-asks/v4.ts
@@ -113,7 +113,7 @@ export const getOrdersAsksV4Options: RouteOptions = {
         .default(false)
         .description("If true, prices will include missing royalties to be added on-top."),
       sortBy: Joi.string()
-        .valid("createdAt", "price")
+        .valid("createdAt", "price", "updatedAt")
         .default("createdAt")
         .description(
           "Order the items are returned in the response, Sorting by price allowed only when filtering by token"
@@ -201,7 +201,7 @@ export const getOrdersAsksV4Options: RouteOptions = {
               ELSE 'active'
             END
           ) AS status,
-          orders.updated_at,
+          extract(epoch from orders.updated_at) AS updated_at,
           (${criteriaBuildQuery}) AS criteria
           ${query.includeRawData || query.includeDynamicPricing ? ", orders.raw_data" : ""}
         FROM orders
@@ -220,11 +220,17 @@ export const getOrdersAsksV4Options: RouteOptions = {
       // Filters
       const conditions: string[] =
         query.startTimestamp || query.endTimestamp
-          ? [
-              `orders.created_at >= to_timestamp($/startTimestamp/)`,
-              `orders.created_at <= to_timestamp($/endTimestamp/)`,
-              `orders.side = 'sell'`,
-            ]
+          ? query.sortBy === "updatedAt"
+            ? [
+                `orders.updated_at >= to_timestamp($/startTimestamp/)`,
+                `orders.updated_at <= to_timestamp($/endTimestamp/)`,
+                `orders.side = 'sell'`,
+              ]
+            : [
+                `orders.created_at >= to_timestamp($/startTimestamp/)`,
+                `orders.created_at <= to_timestamp($/endTimestamp/)`,
+                `orders.side = 'sell'`,
+              ]
           : [`orders.side = 'sell'`];
 
       let communityFilter = "";
@@ -366,22 +372,28 @@ export const getOrdersAsksV4Options: RouteOptions = {
       }
 
       if (query.continuation) {
-        const [priceOrCreatedAt, id] = splitContinuation(
+        const [priceOrCreatedAtOrUpdatedAt, id] = splitContinuation(
           query.continuation,
           /^\d+(.\d+)?_0x[a-f0-9]{64}$/
         );
-        (query as any).priceOrCreatedAt = priceOrCreatedAt;
+        (query as any).priceOrCreatedAtOrUpdatedAt = priceOrCreatedAtOrUpdatedAt;
         (query as any).id = id;
 
         if (query.sortBy === "price") {
           if (query.normalizeRoyalties) {
-            conditions.push(`(orders.normalized_value, orders.id) > ($/priceOrCreatedAt/, $/id/)`);
+            conditions.push(
+              `(orders.normalized_value, orders.id) > ($/priceOrCreatedAtOrUpdatedAt/, $/id/)`
+            );
           } else {
-            conditions.push(`(orders.price, orders.id) > ($/priceOrCreatedAt/, $/id/)`);
+            conditions.push(`(orders.price, orders.id) > ($/priceOrCreatedAtOrUpdatedAt/, $/id/)`);
           }
+        } else if (query.sortBy === "updatedAt") {
+          conditions.push(
+            `(orders.updated_at, orders.id) < (to_timestamp($/priceOrCreatedAtOrUpdatedAt/), $/id/)`
+          );
         } else {
           conditions.push(
-            `(orders.created_at, orders.id) < (to_timestamp($/priceOrCreatedAt/), $/id/)`
+            `(orders.created_at, orders.id) < (to_timestamp($/priceOrCreatedAtOrUpdatedAt/), $/id/)`
           );
         }
       }
@@ -400,6 +412,8 @@ export const getOrdersAsksV4Options: RouteOptions = {
         } else {
           baseQuery += ` ORDER BY orders.price, orders.id`;
         }
+      } else if (query.sortBy === "updatedAt") {
+        baseQuery += ` ORDER BY orders.updated_at DESC, orders.id DESC`;
       } else {
         baseQuery += ` ORDER BY orders.created_at DESC, orders.id DESC`;
       }
@@ -422,6 +436,10 @@ export const getOrdersAsksV4Options: RouteOptions = {
               rawResult[rawResult.length - 1].price + "_" + rawResult[rawResult.length - 1].id
             );
           }
+        } else if (query.sortBy === "updatedAt") {
+          continuation = buildContinuation(
+            rawResult[rawResult.length - 1].updated_at + "_" + rawResult[rawResult.length - 1].id
+          );
         } else {
           continuation = buildContinuation(
             rawResult[rawResult.length - 1].created_at + "_" + rawResult[rawResult.length - 1].id
