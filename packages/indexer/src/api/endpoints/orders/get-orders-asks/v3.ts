3620aeb644228a642cb815a49c678344a99c6b7a
George Roman <george.roman.99@gmail.com>
2023-04-20 15:25:11 +0200

    fix: tweaks

--- packages/indexer/src/api/endpoints/orders/get-orders-asks/v3.ts
+++ packages/indexer/src/api/endpoints/orders/get-orders-asks/v3.ts
@@ -307,7 +307,7 @@ export const getOrdersAsksV3Options: RouteOptions = {
               ELSE 'active'
             END
           ) AS status,
-          extract(epoch from orders.updated_at) AS updated_at
+          orders.updated_at
           ${query.includeRawData ? ", orders.raw_data" : ""}
           ${query.includeMetadata ? `, ${metadataBuildQuery}` : ""}
         FROM orders
