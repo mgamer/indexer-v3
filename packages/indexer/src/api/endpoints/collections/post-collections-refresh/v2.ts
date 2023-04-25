25aab82d87eabce713e2a71cf81b9e4e50d5b057
Jamie Chicago <87397251+jamiechicago312@users.noreply.github.com>
2023-04-25 17:01:35 -0500

    fix: token count admin update
    fixed the amount from 10k to 30k for admin key required for refresh

--- packages/indexer/src/api/endpoints/collections/post-collections-refresh/v2.ts
+++ packages/indexer/src/api/endpoints/collections/post-collections-refresh/v2.ts
@@ -24,7 +24,7 @@ const version = "v2";
 
 export const postCollectionsRefreshV2Options: RouteOptions = {
   description: "Refresh Collection",
-  note: "Use this API to refresh a collection metadata. Only use this endpoint when you notice multiple tokens with incorrect metadata. Otherwise, refresh single token metadata. Collections with over 10,000 tokens require admin key override, so please contact technical support for assistance.\n\n Collection metadata is automatically updated at 23:30 UTC daily for:\n\n- Top 500 Collection by 24hr Volume\n\n- Collections Minted 1 Day Ago\n\n- Collections Minted 7 Days Ago\n\n Caution: This API should be used in moderation, like only when missing data is discovered. Calling it in bulk or programmatically will result in your API key getting rate limited.",
+  note: "Use this API to refresh a collection metadata. Only use this endpoint when you notice multiple tokens with incorrect metadata. Otherwise, refresh single token metadata. Collections with over 30,000 tokens require admin key override, so please contact technical support for assistance.\n\n Collection metadata is automatically updated at 23:30 UTC daily for:\n\n- Top 500 Collection by 24hr Volume\n\n- Collections Minted 1 Day Ago\n\n- Collections Minted 7 Days Ago\n\n Caution: This API should be used in moderation, like only when missing data is discovered. Calling it in bulk or programmatically will result in your API key getting rate limited.",
   tags: ["api", "Collections"],
   plugins: {
     "hapi-swagger": {
