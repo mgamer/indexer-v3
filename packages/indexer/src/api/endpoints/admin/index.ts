4c15fe4ced82af6e29751de138f6d801c43cd471
Jaden <jadendurnford@gmail.com>
2023-04-25 14:01:28 -0400

    feat: changing admin metrics api endpoint to be post

--- packages/indexer/src/api/endpoints/admin/index.ts
+++ packages/indexer/src/api/endpoints/admin/index.ts
@@ -25,4 +25,4 @@ export * from "@/api/endpoints/admin/post-sync-events";
 export * from "@/api/endpoints/admin/post-update-api-key";
 export * from "@/api/endpoints/admin/post-update-rate-limit-rule";
 export * from "@/api/endpoints/admin/post-update-source";
-export * from "@/api/endpoints/admin/get-api-key-metrics";
+export * from "@/api/endpoints/admin/post-api-key-metrics";
