6b9da867881c710b8ee36540fb8356a2ca62b3a2
Jamie Chicago <87397251+jamiechicago312@users.noreply.github.com>
2023-04-27 15:22:26 -0500

    Feat: add description
    Max limits and offset added, adjusted displayCurrency description

--- packages/indexer/src/api/endpoints/collections/get-user-collections/v3.ts
+++ packages/indexer/src/api/endpoints/collections/get-user-collections/v3.ts
@@ -56,17 +56,17 @@ export const getUserCollectionsV3Options: RouteOptions = {
         .min(0)
         .max(10000)
         .default(0)
-        .description("Use offset to request the next batch of items."),
+        .description("Use offset to request the next batch of items. Max is 10,000."),
       limit: Joi.number()
         .integer()
         .min(1)
         .max(100)
         .default(20)
-        .description("Amount of items returned in response."),
+        .description("Amount of items returned in response. max limit is 100."),
       displayCurrency: Joi.string()
         .lowercase()
         .pattern(regex.address)
-        .description("Return result in given currency"),
+        .description("Input any ERC20 address to return result in given currency."),
     }),
   },
   response: {
