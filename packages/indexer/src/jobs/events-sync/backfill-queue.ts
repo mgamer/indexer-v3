1f241e8568f9058a7b4b1c71a646edef00ebc32f
Idan Peleg <ipeleg@hotmail.com>
2023-04-28 17:47:52 -0400

    Update backfill-queue.ts

--- packages/indexer/src/jobs/events-sync/backfill-queue.ts
+++ packages/indexer/src/jobs/events-sync/backfill-queue.ts
@@ -61,7 +61,7 @@ if (config.doBackgroundWork && config.doEventsSyncBackfill) {
         throw error;
       }
     },
-    { connection: redis.duplicate(), concurrency: 5 }
+    { connection: redis.duplicate(), concurrency: 10 }
   );
   worker.on("error", (error) => {
     logger.error(QUEUE_NAME, `Worker errored: ${error}`);
