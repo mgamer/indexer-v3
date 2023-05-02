8ca92130b89e7cb09314c9c443b002a2777d587c
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-05-02 09:30:51 -0400

    Ofir/platf 999 refresh royalties fees whenever we (#4347)
    * feat: added refresh logic
    
    * feat: added log
    
    * feat: fixed query
    
    * feat: fixed query
    
    * feat: added log
    
    * feat: added handling based on order kind
    
    * fix: fix job params
    
    * feat: revert change

--- packages/indexer/src/jobs/orderbook/post-order-external/index.ts
+++ packages/indexer/src/jobs/orderbook/post-order-external/index.ts
@@ -26,7 +26,7 @@ import * as crossPostingOrdersModel from "@/models/cross-posting-orders";
 import { CrossPostingOrderStatus } from "@/models/cross-posting-orders";
 import { TSTAttribute, TSTCollection, TSTCollectionNonFlagged } from "@/orderbook/token-sets/utils";
 import * as collectionUpdatesMetadata from "@/jobs/collection-updates/metadata-queue";
-import { toBuffer } from "@/common/utils";
+import { fromBuffer, toBuffer } from "@/common/utils";
 
 const QUEUE_NAME = "orderbook-post-order-external-queue";
 const MAX_RETRIES = 5;
@@ -173,6 +173,8 @@ if (config.doBackgroundWork) {
                 SELECT
                   tokens.contract,
                   tokens.token_id,
+                  collections.royalties,
+                  collections.new_royalties,
                   collections.community
                 FROM collections
                 JOIN tokens ON tokens.collection_id = collections.id
@@ -189,6 +191,8 @@ if (config.doBackgroundWork) {
                 SELECT
                   tokens.contract,
                   tokens.token_id,
+                  collections.royalties,
+                  collections.new_royalties,
                   collections.community
                 FROM tokens
                 JOIN collections ON collections.id = tokens.collection_id
@@ -202,7 +206,7 @@ if (config.doBackgroundWork) {
               if (rawResult) {
                 logger.info(
                   QUEUE_NAME,
-                  `Post Order Failed - Invalid Fees - Refreshing. orderbook=${orderbook}, crossPostingOrderId=${crossPostingOrderId}, orderKind=${
+                  `Post Order Failed - Invalid Fees - Refreshing. orderbook=${orderbook}, crossPostingOrderId=${crossPostingOrderId}, orderbookApiKey=${orderbookApiKey}, orderKind=${
                     order.params.kind
                   }, orderId=${orderId}, orderData=${JSON.stringify(
                     orderData
@@ -210,7 +214,7 @@ if (config.doBackgroundWork) {
                 );
 
                 await collectionUpdatesMetadata.addToQueue(
-                  rawResult.contract,
+                  fromBuffer(rawResult.contract),
                   rawResult.token_id,
                   rawResult.community
                 );
