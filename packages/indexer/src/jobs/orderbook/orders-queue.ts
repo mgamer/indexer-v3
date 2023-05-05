f41c0db3a8d0e45420a68d9bdfbc7139cd9437d6
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-05-05 15:19:30 -0400

    Revert "Revert "Feat/dedicated top bid job" (#4430)" (#4433)
    This reverts commit 5243a7a1ace09147c2b19791ae60093dfa4eae43.

--- packages/indexer/src/jobs/orderbook/orders-queue.ts
+++ packages/indexer/src/jobs/orderbook/orders-queue.ts
@@ -1,5 +1,4 @@
 import { Job, Queue, QueueScheduler, Worker } from "bullmq";
-import { randomUUID } from "crypto";
 import cron from "node-cron";
 
 import { idb } from "@/common/db";
@@ -7,6 +6,7 @@ import { logger } from "@/common/logger";
 import { redis, redlock } from "@/common/redis";
 import { config } from "@/config/index";
 import * as orders from "@/orderbook/orders";
+import { addToQueue as addToQueueV2 } from "@/jobs/orderbook/orders-queue-v2";
 
 const QUEUE_NAME = "orderbook-orders-queue";
 
@@ -314,16 +314,4 @@ export const addToQueue = async (
   prioritized = false,
   delay = 0,
   jobId?: string
-) => {
-  await queue.addBulk(
-    orderInfos.map((orderInfo) => ({
-      name: randomUUID(),
-      data: orderInfo,
-      opts: {
-        priority: prioritized ? 1 : undefined,
-        delay: delay ? delay * 1000 : undefined,
-        jobId,
-      },
-    }))
-  );
-};
+) => addToQueueV2(orderInfos, prioritized, delay, jobId);
