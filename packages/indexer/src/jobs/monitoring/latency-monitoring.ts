16b63ad8ebd1aef303ca3596355f715c16b6432c
tv <tv3636@gmail.com>
2023-05-09 13:00:53 -0700

    fix: use reader and limit query

--- packages/indexer/src/jobs/monitoring/latency-monitoring.ts
+++ packages/indexer/src/jobs/monitoring/latency-monitoring.ts
@@ -1,16 +1,17 @@
 import cron from "node-cron";
 import { config } from "@/config/index";
 import { logger } from "@/common/logger";
-import { idb } from "@/common/db";
+import { redb } from "@/common/db";
 
 if (config.doBackgroundWork) {
   cron.schedule("*/5 * * * *", async () => {
     // Log sales latency for all fill events in last 5 minutes
-    const results = await idb.manyOrNone(
+    const results = await redb.manyOrNone(
       `
             SELECT EXTRACT(epoch FROM created_at) - "timestamp" AS latency
             FROM fill_events_2
             WHERE created_at > NOW() - INTERVAL '5 minutes'
+            LIMIT 1000
       `
     );
 
