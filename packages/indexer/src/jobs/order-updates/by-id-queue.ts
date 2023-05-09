8cd70000a2729aef564e9f6f88d0731545e8d4bc
Idan Peleg <ipeleg@hotmail.com>
2023-05-09 14:07:16 -0400

    Update by-id-queue.ts

--- packages/indexer/src/jobs/order-updates/by-id-queue.ts
+++ packages/indexer/src/jobs/order-updates/by-id-queue.ts
@@ -425,7 +425,7 @@ if (config.doBackgroundWork) {
         throw error;
       }
     },
-    { connection: redis.duplicate(), concurrency: 80 }
+    { connection: redis.duplicate(), concurrency: 100 }
   );
   worker.on("error", (error) => {
     logger.error(QUEUE_NAME, `Worker errored: ${error}`);
