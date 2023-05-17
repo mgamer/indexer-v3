92daef8341a571a3266a5f82e7fcab2ff9da7789
d3or <deorrunner@gmail.com>
2023-05-17 16:25:01 -0400

    feat: remove nft balance events for now

--- packages/indexer/src/jobs/cdc/topics/index.ts
+++ packages/indexer/src/jobs/cdc/topics/index.ts
@@ -1,15 +1,12 @@
 import { KafkaEventHandler } from "./KafkaEventHandler";
 import { IndexerFillEventsHandler } from "./indexer-fill-events";
 import { IndexerApprovalEventsHandler } from "./indexer-nft-approvals";
-import { IndexerBalanceEventsHandler } from "./indexer-nft-balances";
 import { IndexerTransferEventsHandler } from "./indexer-nft-transfer-events";
 import { IndexerOrdersHandler } from "./indexer-orders";
 
 export const TopicHandlers: KafkaEventHandler[] = [
-  // new IndexerOrderEventsHandler(),
   new IndexerOrdersHandler(),
   new IndexerTransferEventsHandler(),
-  new IndexerBalanceEventsHandler(),
   new IndexerApprovalEventsHandler(),
   new IndexerFillEventsHandler(),
 ];
