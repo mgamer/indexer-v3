d2c65af1d5be9516c4768cd6987ffc8ef1f14414
Idan Peleg <ipeleg@hotmail.com>
2023-05-17 14:18:22 -0400

    feat: change bids aggregation

--- packages/indexer/src/websockets/opensea/index.ts
+++ packages/indexer/src/websockets/opensea/index.ts
@@ -30,7 +30,8 @@ import * as backfillBids from "@/jobs/backfill/backfill-bids";
 
 if (config.doWebsocketWork && config.openSeaApiKey) {
   const network = config.chainId === 5 ? Network.TESTNET : Network.MAINNET;
-  const maxEventsSize = config.chainId === 1 ? 200 : 1;
+  const maxCollectionBidsSize = config.chainId === 1 ? 50 : 1;
+  const maxBidsSize = config.chainId === 1 ? 200 : 1;
   const bidsEvents: GenericOrderInfo[] = [];
   const collectionBidsEvents: GenericOrderInfo[] = [];
 
@@ -101,12 +102,12 @@ if (config.doWebsocketWork && config.openSeaApiKey) {
                 bidsEvents.push(orderInfo);
               }
 
-              if (collectionBidsEvents.length >= maxEventsSize) {
+              if (collectionBidsEvents.length >= maxCollectionBidsSize) {
                 const orderInfoBatch = collectionBidsEvents.splice(0, collectionBidsEvents.length);
                 await orderbookOrders.addToQueue(orderInfoBatch);
               }
 
-              if (bidsEvents.length >= maxEventsSize) {
+              if (bidsEvents.length >= maxBidsSize) {
                 const orderInfoBatch = bidsEvents.splice(0, bidsEvents.length);
 
                 const id = await MqJobsDataManager.addJobData(
