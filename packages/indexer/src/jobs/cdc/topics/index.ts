04c1787f93c8ef5e97e41246276956b3c9ee031a
d3or <deorrunner@gmail.com>
2023-05-17 15:33:53 -0400

    feat: switch to be based on orders table instead of order events

--- packages/indexer/src/jobs/cdc/topics/index.ts
+++ packages/indexer/src/jobs/cdc/topics/index.ts
@@ -1,18 +1,15 @@
 import { KafkaEventHandler } from "./KafkaEventHandler";
-import { IndexerBidEventsHandler } from "./indexer-bid-events";
 import { IndexerFillEventsHandler } from "./indexer-fill-events";
 import { IndexerApprovalEventsHandler } from "./indexer-nft-approvals";
 import { IndexerBalanceEventsHandler } from "./indexer-nft-balances";
 import { IndexerTransferEventsHandler } from "./indexer-nft-transfer-events";
-import { IndexerOrderEventsHandler } from "./indexer-order-events";
-// import { IndexerOrdersHandler } from "./indexer-orders";
+import { IndexerOrdersHandler } from "./indexer-orders";
 
 export const TopicHandlers: KafkaEventHandler[] = [
-  new IndexerOrderEventsHandler(),
-  // new IndexerOrdersHandler(),
+  // new IndexerOrderEventsHandler(),
+  new IndexerOrdersHandler(),
   new IndexerTransferEventsHandler(),
   new IndexerBalanceEventsHandler(),
   new IndexerApprovalEventsHandler(),
   new IndexerFillEventsHandler(),
-  new IndexerBidEventsHandler(),
 ];
