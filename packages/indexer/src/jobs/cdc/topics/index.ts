9f380ae3fb4c5e1e983e2e840a9776c998d9ca1b
d3or <97639237+d3or@users.noreply.github.com>
2023-05-23 09:49:39 -0400

    feat: unsub from orders to not suffocate other websockets (#4670)

--- packages/indexer/src/jobs/cdc/topics/index.ts
+++ packages/indexer/src/jobs/cdc/topics/index.ts
@@ -2,10 +2,10 @@ import { KafkaEventHandler } from "./KafkaEventHandler";
 import { IndexerFillEventsHandler } from "./indexer-fill-events";
 import { IndexerApprovalEventsHandler } from "./indexer-nft-approvals";
 import { IndexerTransferEventsHandler } from "./indexer-nft-transfer-events";
-import { IndexerOrdersHandler } from "./indexer-orders";
+// import { IndexerOrdersHandler } from "./indexer-orders";
 
 export const TopicHandlers: KafkaEventHandler[] = [
-  new IndexerOrdersHandler(),
+  // new IndexerOrdersHandler(),
   new IndexerTransferEventsHandler(),
   new IndexerApprovalEventsHandler(),
   new IndexerFillEventsHandler(),
