42f8d4ecbbfb4eedb4fbd70ced19d2d97e8fc32b
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-05-25 09:24:51 -0400

    Feat/fix refresh collection metadata lock (#4716)
    * feat: fix lock logic
    
    * feat: fix type
    
    * feat: updated impl
    
    * feat: added logs

--- packages/indexer/src/jobs/collection-updates/metadata-queue.ts
+++ packages/indexer/src/jobs/collection-updates/metadata-queue.ts
@@ -3,7 +3,7 @@ import { randomUUID } from "crypto";
 import _ from "lodash";
 
 import { logger } from "@/common/logger";
-import { redis, acquireLock } from "@/common/redis";
+import { redis, acquireLock, releaseLock } from "@/common/redis";
 import { config } from "@/config/index";
 import { Collections } from "@/models/collections";
 
@@ -24,27 +24,40 @@ if (config.doBackgroundWork) {
   const worker = new Worker(
     QUEUE_NAME,
     async (job: Job) => {
-      const { contract, tokenId, community } = job.data;
+      const { contract, tokenId, community, forceRefresh } = job.data;
 
-      if (await acquireLock(QUEUE_NAME, 1)) {
+      if (forceRefresh || (await acquireLock(`${QUEUE_NAME}:${contract}`, 5 * 60))) {
         logger.info(
           QUEUE_NAME,
-          `Refresh collection metadata - got lock. contract=${contract}, tokenId=${tokenId}, community=${community}`
+          `Refresh collection metadata - got contract lock. contract=${contract}, tokenId=${tokenId}, community=${community}, forceRefresh=${forceRefresh}`
         );
 
-        // Lock this contract for the next 5 minutes
-        await acquireLock(`${QUEUE_NAME}:${contract}`, 5 * 60);
-
-        try {
-          await Collections.updateCollectionCache(contract, tokenId, community);
-        } catch (error) {
-          logger.error(
+        if (await acquireLock(QUEUE_NAME, 1)) {
+          logger.info(
             QUEUE_NAME,
-            `Failed to update collection metadata. contract=${contract}, tokenId=${tokenId}, community=${community}, error=${error}`
+            `Refresh collection metadata - got update lock. contract=${contract}, tokenId=${tokenId}, community=${community}, forceRefresh=${forceRefresh}`
           );
+
+          try {
+            await Collections.updateCollectionCache(contract, tokenId, community);
+          } catch (error) {
+            logger.error(
+              QUEUE_NAME,
+              `Failed to update collection metadata. contract=${contract}, tokenId=${tokenId}, community=${community}, forceRefresh=${forceRefresh}, error=${error}`
+            );
+          }
+        } else {
+          logger.info(
+            QUEUE_NAME,
+            `Refresh collection metadata - reschedule. contract=${contract}, tokenId=${tokenId}, community=${community}, forceRefresh=${forceRefresh}`
+          );
+
+          job.data.addToQueue = true;
+
+          if (!forceRefresh) {
+            await releaseLock(`${QUEUE_NAME}:${contract}`);
+          }
         }
-      } else {
-        job.data.addToQueue = true;
       }
     },
     { connection: redis.duplicate(), concurrency: 1 }
@@ -66,12 +79,20 @@ export type CollectionMetadataInfo = {
   contract: string;
   tokenId: string;
   community: string;
+  forceRefresh?: boolean;
 };
 
 export const addToQueueBulk = async (
   collectionMetadataInfos: CollectionMetadataInfo[],
   delay = 0
 ) => {
+  logger.debug(
+    QUEUE_NAME,
+    `debugaAdToQueueBulk. collectionMetadataInfos=${JSON.stringify(
+      collectionMetadataInfos
+    )}, callStack=${new Error().stack}`
+  );
+
   await queue.addBulk(
     collectionMetadataInfos.map((collectionMetadataInfo) => ({
       name: `${collectionMetadataInfo.contract}-${collectionMetadataInfo.tokenId}-${collectionMetadataInfo.community}`,
@@ -88,21 +109,22 @@ export const addToQueue = async (
   delay = 0,
   forceRefresh = false
 ) => {
+  logger.debug(
+    QUEUE_NAME,
+    `debugAddToQueue. contract=${JSON.stringify(
+      contract
+    )}, tokenId=${tokenId}, community=${community}, callStack=${new Error().stack}`
+  );
+
   if (_.isArray(contract)) {
     await queue.addBulk(
       _.map(contract, (c) => ({
         name: randomUUID(),
-        data: { contract: c.contract, tokenId, community: c.community },
+        data: { contract: c.contract, tokenId, community: c.community, forceRefresh },
         opts: { delay },
       }))
     );
   } else {
-    if (forceRefresh || _.isNull(await redis.get(`${QUEUE_NAME}:${contract}`))) {
-      logger.info(
-        QUEUE_NAME,
-        `Refresh collection metadata - add to queue. contract=${contract}, tokenId=${tokenId}, community=${community}`
-      );
-      await queue.add(randomUUID(), { contract, tokenId, community }, { delay });
-    }
+    await queue.add(randomUUID(), { contract, tokenId, community, forceRefresh }, { delay });
   }
 };
