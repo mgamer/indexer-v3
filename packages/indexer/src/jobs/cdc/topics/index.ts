9bda90c617fc0f088a520de5dc8d615980842418
d3or <97639237+d3or@users.noreply.github.com>
2023-05-26 00:07:13 -0400

    d3or/disable delete ops (#4750)
    * feat: disable normalization for delete ops
    
    * feat: disable subbing to old orders when doOldOrderWebsocketWork=false

--- packages/indexer/src/jobs/cdc/topics/index.ts
+++ packages/indexer/src/jobs/cdc/topics/index.ts
@@ -1,3 +1,4 @@
+import { config } from "@/config/index";
 import { KafkaEventHandler } from "./KafkaEventHandler";
 import { IndexerFillEventsHandler } from "./indexer-fill-events";
 import { IndexerApprovalEventsHandler } from "./indexer-nft-approvals";
@@ -5,8 +6,11 @@ import { IndexerTransferEventsHandler } from "./indexer-nft-transfer-events";
 import { IndexerOrdersHandler } from "./indexer-orders";
 
 export const TopicHandlers: KafkaEventHandler[] = [
-  new IndexerOrdersHandler(),
   new IndexerTransferEventsHandler(),
   new IndexerApprovalEventsHandler(),
   new IndexerFillEventsHandler(),
 ];
+
+if (!config.doOldOrderWebsocketWork) {
+  TopicHandlers.push(new IndexerOrdersHandler());
+}
