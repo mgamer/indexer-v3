cd42e14d505334a3e049a092dd760093b40141b3
d3or <deorrunner@gmail.com>
2023-06-01 10:22:13 -0400

    feat: move imports

--- packages/indexer/src/jobs/cdc/topics/index.ts
+++ packages/indexer/src/jobs/cdc/topics/index.ts
@@ -1,14 +1,14 @@
-import { IndexerTokensHandler } from "@/jobs/cdc/topics/indexer-tokens";
 import { KafkaEventHandler } from "./KafkaEventHandler";
 import { IndexerFillEventsHandler } from "@/jobs/cdc/topics/indexer-fill-events";
 import { IndexerApprovalEventsHandler } from "@/jobs/cdc/topics/indexer-nft-approvals";
 import { IndexerTransferEventsHandler } from "@/jobs/cdc/topics/indexer-nft-transfer-events";
 import { IndexerOrdersHandler } from "@/jobs/cdc/topics/indexer-orders";
+import { IndexerTokensHandler } from "@/jobs/cdc/topics/indexer-tokens";
 
 export const TopicHandlers: KafkaEventHandler[] = [
-  new IndexerTokensHandler(),
   new IndexerOrdersHandler(),
   new IndexerTransferEventsHandler(),
   new IndexerApprovalEventsHandler(),
   new IndexerFillEventsHandler(),
+  new IndexerTokensHandler(),
 ];
