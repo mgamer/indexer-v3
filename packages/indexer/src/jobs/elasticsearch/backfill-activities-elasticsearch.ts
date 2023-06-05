25767fceb6eb386ea37620709b39e48bad60f1a4
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-06-04 20:34:48 -0400

    Ofir/platf 980 add job to indexer to index items into (#4883)
    * feat: added activities on es
    
    * feat: updated enable check
    
    * feat: added log, updated limit
    
    * feat: added bid / bid cancel
    
    * feat: added bid / bid cancel
    
    * feat: bid backfill fixes
    
    * feat: added log
    
    * feat: fixed timestamp issue, added cloud indexing
    
    * feat: added log
    
    * feat: updated lock
    
    * feat: fixed user query
    
    * feat: fixed search
    
    * feat: fixed search
    
    * feat: updated limit
    
    * feat: added missing collection id logic
    
    * feat: added error handling
    
    * feat: add support for batch logic
    
    * feat: debug
    
    * feat: fixed bid activity
    
    * feat: updated impl
    
    * feat: updated job lock
    
    * feat: added paid mint support
    
    * feat: fixed query syntax
    
    * feat: added better mint detection
    
    * feat: added log
    
    * feat: added log
    
    * feat: removed log
    
    * feat: config fix
    
    * feat: added log
    
    * feat: updated backfill job logic
    
    * feat: fixed query
    
    * feat: fixed job
    
    * feat: updated lock
    
    * feat: updated job logic
    
    * feat: index sorting wip
    
    * feat: index sorting wip 2
    
    * feat: reset transfer backfill
    
    * feat: updated index settings
    
    * feat: updated logs
    
    * feat: fixed total hits logic
    
    * feat: updated schedule job logic
    
    * feat: removed cloud, fixed query
    
    * feat: query fixes, added admin api
    
    * feat: fixed impl
    
    * feat: added support for contract set id
    
    * feat: api updates, removed logs
    
    * feat: added log
    
    * feat: added lock
    
    * feat: removed logs, updated lock logic
    
    * feat: added log, updated backfill logic
    
    * feat: query fix
    
    * feat: added cancel backfill
    
    * feat: fix queue names
    
    * feat: fix query
    
    * feat: removed debug log
    
    * feat: added activity swap logic
    
    * feat: fix bid query
    
    * feat: added includeMetadata support
    
    * feat: undo package json changes
    
    * feat: fix createdAt
    
    * feat: format fix
    
    * feat: added es query support
    
    * feat: response fix
    
    * feat: fix criteria
    
    * feat: more fixes
    
    * feat: fix limit
    
    * feat: remove comment
    
    * feat: remove comment
    
    * feat: enable v2
    
    * feat: enabled v3
    
    * feat: added v4
    
    * feat: added v5
    
    * feat: added error handling
    
    * feat: added bulk errors log
    
    * feat: enabled v6
    
    * feat: added retries
    
    * feat: updated log
    
    * feat: added logs
    
    * feat: added job
    
    * feat: fixed logic
    
    * feat: added more josbs

--- packages/indexer/src/jobs/elasticsearch/backfill-activities-elasticsearch.ts
+++ packages/indexer/src/jobs/elasticsearch/backfill-activities-elasticsearch.ts
@@ -10,6 +10,11 @@ import { config } from "@/config/index";
 import { ridb } from "@/common/db";
 
 import { addToQueue as addToQueueTransfers } from "@/jobs/elasticsearch/backfill-transfer-activities-elasticsearch";
+import { addToQueue as addToQueueSales } from "@/jobs/elasticsearch/backfill-sale-activities-elasticsearch";
+import { addToQueue as addToQueueAsks } from "@/jobs/elasticsearch/backfill-ask-activities-elasticsearch";
+import { addToQueue as addToQueueAsksCancel } from "@/jobs/elasticsearch/backfill-ask-cancel-activities-elasticsearch";
+import { addToQueue as addToQueueBids } from "@/jobs/elasticsearch/backfill-bid-activities-elasticsearch";
+import { addToQueue as addToQueueBidsCancel } from "@/jobs/elasticsearch/backfill-bid-cancel-activities-elasticsearch";
 
 const QUEUE_NAME = "backfill-activities-elasticsearch";
 
@@ -29,25 +34,200 @@ if (config.doBackgroundWork && config.doElasticsearchWork) {
   const worker = new Worker(
     QUEUE_NAME,
     async () => {
-      const query =
-        "SELECT min(timestamp) AS min_timestamp, MAX(timestamp) AS max_timestamp from nft_transfer_events;";
+      const backfillTransferActivities = async () => {
+        const query =
+          "SELECT min(timestamp) AS min_timestamp, MAX(timestamp) AS max_timestamp from nft_transfer_events;";
 
-      const timestamps = await ridb.oneOrNone(query);
+        const timestamps = await ridb.oneOrNone(query);
 
-      const start = new Date(timestamps.min_timestamp * 1000);
-      const end = new Date(timestamps.max_timestamp * 1000);
+        const start = new Date(timestamps.min_timestamp * 1000);
+        const end = new Date(timestamps.max_timestamp * 1000);
 
-      let loop = new Date(start);
+        let loop = new Date(start);
 
-      while (loop <= end) {
-        const fromTimestamp = Math.floor(loop.getTime() / 1000);
-        const newDate = loop.setDate(loop.getDate() + 1);
-        const toTimestamp = Math.floor(newDate / 1000);
+        while (loop <= end) {
+          const fromTimestamp = Math.floor(loop.getTime() / 1000);
+          const newDate = loop.setDate(loop.getDate() + 1);
+          const toTimestamp = Math.floor(newDate / 1000);
 
-        await addToQueueTransfers(undefined, fromTimestamp, toTimestamp);
+          await addToQueueTransfers(undefined, fromTimestamp, toTimestamp);
 
-        loop = new Date(newDate);
-      }
+          loop = new Date(newDate);
+        }
+
+        logger.info(
+          QUEUE_NAME,
+          JSON.stringify({
+            topic: "backfillTransferActivities",
+            start: start.toISOString(),
+            end: end.toISOString(),
+          })
+        );
+      };
+
+      const backfillSaleActivities = async () => {
+        const query =
+          "SELECT min(timestamp) AS min_timestamp, MAX(timestamp) AS max_timestamp from fill_events_2;";
+
+        const timestamps = await ridb.oneOrNone(query);
+
+        const start = new Date(timestamps.min_timestamp * 1000);
+        const end = new Date(timestamps.max_timestamp * 1000);
+
+        let loop = new Date(start);
+
+        while (loop <= end) {
+          const fromTimestamp = Math.floor(loop.getTime() / 1000);
+          const newDate = loop.setDate(loop.getDate() + 1);
+          const toTimestamp = Math.floor(newDate / 1000);
+
+          await addToQueueSales(undefined, fromTimestamp, toTimestamp);
+
+          loop = new Date(newDate);
+        }
+
+        logger.info(
+          QUEUE_NAME,
+          JSON.stringify({
+            topic: "backfillSaleActivities",
+            start: start.toISOString(),
+            end: end.toISOString(),
+          })
+        );
+      };
+
+      const backfillAskActivities = async () => {
+        const query =
+          "SELECT min(updated_at) AS min_timestamp, MAX(updated_at) AS max_timestamp from orders WHERE side = 'sell';";
+
+        const timestamps = await ridb.oneOrNone(query);
+
+        const start = new Date(timestamps.min_timestamp * 1000);
+        const end = new Date(timestamps.max_timestamp * 1000);
+
+        let loop = new Date(start);
+
+        while (loop <= end) {
+          const fromTimestamp = Math.floor(loop.getTime() / 1000);
+          const newDate = loop.setDate(loop.getDate() + 1);
+          const toTimestamp = Math.floor(newDate / 1000);
+
+          await addToQueueAsks(undefined, fromTimestamp, toTimestamp);
+
+          loop = new Date(newDate);
+        }
+
+        logger.info(
+          QUEUE_NAME,
+          JSON.stringify({
+            topic: "backfillAskActivities",
+            start: start.toISOString(),
+            end: end.toISOString(),
+          })
+        );
+      };
+
+      const backfillAskCancelActivities = async () => {
+        const query =
+          "SELECT min(updated_at) AS min_timestamp, MAX(updated_at) AS max_timestamp from orders WHERE side = 'sell' AND fillability_status = 'cancelled';";
+
+        const timestamps = await ridb.oneOrNone(query);
+
+        const start = new Date(timestamps.min_timestamp * 1000);
+        const end = new Date(timestamps.max_timestamp * 1000);
+
+        let loop = new Date(start);
+
+        while (loop <= end) {
+          const fromTimestamp = Math.floor(loop.getTime() / 1000);
+          const newDate = loop.setDate(loop.getDate() + 1);
+          const toTimestamp = Math.floor(newDate / 1000);
+
+          await addToQueueAsksCancel(undefined, fromTimestamp, toTimestamp);
+
+          loop = new Date(newDate);
+        }
+
+        logger.info(
+          QUEUE_NAME,
+          JSON.stringify({
+            topic: "backfillAskCancelActivities",
+            start: start.toISOString(),
+            end: end.toISOString(),
+          })
+        );
+      };
+
+      const backfillBidActivities = async () => {
+        const query =
+          "SELECT min(updated_at) AS min_timestamp, MAX(updated_at) AS max_timestamp from orders WHERE side = 'buy';";
+
+        const timestamps = await ridb.oneOrNone(query);
+
+        const start = new Date(timestamps.min_timestamp * 1000);
+        const end = new Date(timestamps.max_timestamp * 1000);
+
+        let loop = new Date(start);
+
+        while (loop <= end) {
+          const fromTimestamp = Math.floor(loop.getTime() / 1000);
+          const newDate = loop.setDate(loop.getDate() + 1);
+          const toTimestamp = Math.floor(newDate / 1000);
+
+          await addToQueueBids(undefined, fromTimestamp, toTimestamp);
+
+          loop = new Date(newDate);
+        }
+
+        logger.info(
+          QUEUE_NAME,
+          JSON.stringify({
+            topic: "backfillBidActivities",
+            start: start.toISOString(),
+            end: end.toISOString(),
+          })
+        );
+      };
+
+      const backfillBidCancelActivities = async () => {
+        const query =
+          "SELECT min(updated_at) AS min_timestamp, MAX(updated_at) AS max_timestamp from orders WHERE side = 'buy' AND fillability_status = 'cancelled';";
+
+        const timestamps = await ridb.oneOrNone(query);
+
+        const start = new Date(timestamps.min_timestamp * 1000);
+        const end = new Date(timestamps.max_timestamp * 1000);
+
+        let loop = new Date(start);
+
+        while (loop <= end) {
+          const fromTimestamp = Math.floor(loop.getTime() / 1000);
+          const newDate = loop.setDate(loop.getDate() + 1);
+          const toTimestamp = Math.floor(newDate / 1000);
+
+          await addToQueueBidsCancel(undefined, fromTimestamp, toTimestamp);
+
+          loop = new Date(newDate);
+        }
+
+        logger.info(
+          QUEUE_NAME,
+          JSON.stringify({
+            topic: "backfillBidCancelActivities",
+            start: start.toISOString(),
+            end: end.toISOString(),
+          })
+        );
+      };
+
+      await Promise.all([
+        backfillTransferActivities(),
+        backfillSaleActivities(),
+        backfillAskActivities(),
+        backfillAskCancelActivities(),
+        backfillBidActivities(),
+        backfillBidCancelActivities(),
+      ]);
     },
     { connection: redis.duplicate(), concurrency: 1 }
   );
