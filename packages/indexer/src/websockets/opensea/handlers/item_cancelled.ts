66795b3bd835adcd57a4b3bd059eb9cd8e55837d
George Roman <george.roman.99@gmail.com>
2023-06-12 11:44:20 +0200

    fix: debug

--- packages/indexer/src/websockets/opensea/handlers/item_cancelled.ts
+++ packages/indexer/src/websockets/opensea/handlers/item_cancelled.ts
@@ -1,9 +1,12 @@
 import { ItemCancelledEventPayload } from "@opensea/stream-js/dist/types";
 
+import { logger } from "@/common/logger";
 import * as openseaOffChainCancellations from "@/jobs/order-updates/misc/opensea-off-chain-cancellations";
 import { getSupportedChainName } from "@/websockets/opensea/utils";
 
 export const handleEvent = async (payload: ItemCancelledEventPayload) => {
+  logger.info("opensea-debug", JSON.stringify({ payload }));
+
   if (getSupportedChainName() != payload.item.chain.name) {
     return null;
   }
