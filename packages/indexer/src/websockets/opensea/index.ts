00b4a6abf108d742c42ba81a6b1ae20fc897c085
George Roman <george.roman.99@gmail.com>
2023-06-15 11:32:46 +0200

    fix: disable os websocket offers

--- packages/indexer/src/websockets/opensea/index.ts
+++ packages/indexer/src/websockets/opensea/index.ts
@@ -56,7 +56,7 @@ if (config.doWebsocketWork && config.openSeaApiKey) {
     "*",
     [
       EventType.ITEM_LISTED,
-      EventType.ITEM_RECEIVED_BID,
+      //EventType.ITEM_RECEIVED_BID,
       //EventType.COLLECTION_OFFER,
       //EventType.TRAIT_OFFER,
       EventType.ITEM_CANCELLED,
