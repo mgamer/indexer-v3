0b7c49c7a846634d8eff63401d2eea3552c4387f
Idan Peleg <ipeleg@hotmail.com>
2023-06-20 19:27:57 -0400

    feat: disable collection-recalc-owner-count-queue queues

--- packages/indexer/src/jobs/index.ts
+++ packages/indexer/src/jobs/index.ts
@@ -148,20 +148,20 @@ import { PausedRabbitMqQueues } from "@/models/paused-rabbit-mq-queues";
 import { logger } from "@/common/logger";
 import { tokenReclacSupplyJob } from "@/jobs/token-updates/token-reclac-supply-job";
 // import { tokenRefreshCacheJob } from "@/jobs/token-updates/token-refresh-cache-job";
-import { recalcOwnerCountQueueJob } from "@/jobs/collection-updates/recalc-owner-count-queue-job";
+// import { recalcOwnerCountQueueJob } from "@/jobs/collection-updates/recalc-owner-count-queue-job";
 // import { recalcTokenCountQueueJob } from "@/jobs/collection-updates/recalc-token-count-queue-job";
 import { normalizedFloorQueueJob } from "@/jobs/token-updates/normalized-floor-queue-job";
 import { mintQueueJob } from "@/jobs/token-updates/mint-queue-job";
 import { tokenFloorQueueJob } from "@/jobs/token-updates/token-floor-queue-job";
 import { fetchCollectionMetadataJob } from "@/jobs/token-updates/fetch-collection-metadata-job";
-// import { handleNewBuyOrderJob } from "@/jobs/update-attribute/handle-new-buy-order-job";
-// import { handleNewSellOrderJob } from "@/jobs/update-attribute/handle-new-sell-order-job";
-// import { resyncAttributeCacheJob } from "@/jobs/update-attribute/resync-attribute-cache-job";
-// import { resyncAttributeCollectionJob } from "@/jobs/update-attribute/resync-attribute-collection-job";
-// import { resyncAttributeFloorSellJob } from "@/jobs/update-attribute/resync-attribute-floor-sell-job";
-// import { resyncAttributeKeyCountsJob } from "@/jobs/update-attribute/resync-attribute-key-counts-job";
-// import { resyncAttributeValueCountsJob } from "@/jobs/update-attribute/resync-attribute-value-counts-job";
-// import { resyncAttributeCountsJob } from "@/jobs/update-attribute/update-attribute-counts-job";
+import { handleNewBuyOrderJob } from "@/jobs/update-attribute/handle-new-buy-order-job";
+import { handleNewSellOrderJob } from "@/jobs/update-attribute/handle-new-sell-order-job";
+import { resyncAttributeCacheJob } from "@/jobs/update-attribute/resync-attribute-cache-job";
+import { resyncAttributeCollectionJob } from "@/jobs/update-attribute/resync-attribute-collection-job";
+import { resyncAttributeFloorSellJob } from "@/jobs/update-attribute/resync-attribute-floor-sell-job";
+import { resyncAttributeKeyCountsJob } from "@/jobs/update-attribute/resync-attribute-key-counts-job";
+import { resyncAttributeValueCountsJob } from "@/jobs/update-attribute/resync-attribute-value-counts-job";
+import { resyncAttributeCountsJob } from "@/jobs/update-attribute/update-attribute-counts-job";
 import { topBidQueueJob } from "@/jobs/token-set-updates/top-bid-queue-job";
 import { topBidSingleTokenQueueJob } from "@/jobs/token-set-updates/top-bid-single-token-queue-job";
 // import { fetchSourceInfoJob } from "@/jobs/sources/fetch-source-info-job";
@@ -323,20 +323,20 @@ export class RabbitMqJobsConsumer {
     return [
       tokenReclacSupplyJob,
       // tokenRefreshCacheJob,
-      recalcOwnerCountQueueJob,
+      // recalcOwnerCountQueueJob,
       // recalcTokenCountQueueJob,
       normalizedFloorQueueJob,
       mintQueueJob,
       tokenFloorQueueJob,
       fetchCollectionMetadataJob,
-      // handleNewBuyOrderJob,
-      // handleNewSellOrderJob,
-      // resyncAttributeCacheJob,
-      // resyncAttributeCollectionJob,
-      // resyncAttributeFloorSellJob,
-      // resyncAttributeKeyCountsJob,
-      // resyncAttributeValueCountsJob,
-      // resyncAttributeCountsJob,
+      handleNewBuyOrderJob,
+      handleNewSellOrderJob,
+      resyncAttributeCacheJob,
+      resyncAttributeCollectionJob,
+      resyncAttributeFloorSellJob,
+      resyncAttributeKeyCountsJob,
+      resyncAttributeValueCountsJob,
+      resyncAttributeCountsJob,
       topBidQueueJob,
       topBidSingleTokenQueueJob,
       // fetchSourceInfoJob,
