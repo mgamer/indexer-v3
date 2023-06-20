fd0bcb92fb81afa34b21a174fc106c7661aa9703
Idan Peleg <ipeleg@hotmail.com>
2023-06-20 12:35:39 -0400

    feat: wip

--- packages/indexer/src/jobs/index.ts
+++ packages/indexer/src/jobs/index.ts
@@ -424,19 +424,19 @@ export class RabbitMqJobsConsumer {
 
     await channel.prefetch(job.getConcurrency()); // Set the number of messages to consume simultaneously
 
-    // Subscribe to the queue
-    await channel.consume(
-      job.getQueue(),
-      async (msg) => {
-        if (!_.isNull(msg)) {
-          await job.consume(channel, msg);
-        }
-      },
-      {
-        consumerTag: RabbitMqJobsConsumer.getConsumerTag(job.getQueue()),
-      }
-    );
-
+    // // Subscribe to the queue
+    // await channel.consume(
+    //   job.getQueue(),
+    //   async (msg) => {
+    //     if (!_.isNull(msg)) {
+    //       await job.consume(channel, msg);
+    //     }
+    //   },
+    //   {
+    //     consumerTag: RabbitMqJobsConsumer.getConsumerTag(job.getQueue()),
+    //   }
+    // );
+    //
     // // Subscribe to the retry queue
     // await channel.consume(
     //   job.getRetryQueue(),
@@ -449,19 +449,19 @@ export class RabbitMqJobsConsumer {
     //     consumerTag: RabbitMqJobsConsumer.getConsumerTag(job.getRetryQueue()),
     //   }
     // );
-    //
-    // // Subscribe to the queue
-    // await channel.consume(
-    //   _.replace(job.getQueue(), ".new", ""),
-    //   async (msg) => {
-    //     if (!_.isNull(msg)) {
-    //       await job.consume(channel, msg);
-    //     }
-    //   },
-    //   {
-    //     consumerTag: RabbitMqJobsConsumer.getConsumerTag(_.replace(job.getQueue(), ".new", "")),
-    //   }
-    // );
+
+    // Subscribe to the queue
+    await channel.consume(
+      _.replace(job.getQueue(), ".new", ""),
+      async (msg) => {
+        if (!_.isNull(msg)) {
+          await job.consume(channel, msg);
+        }
+      },
+      {
+        consumerTag: RabbitMqJobsConsumer.getConsumerTag(_.replace(job.getQueue(), ".new", "")),
+      }
+    );
 
     // Subscribe to the retry queue
     await channel.consume(
