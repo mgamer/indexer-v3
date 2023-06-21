c37a4bc53d7d8d3dd6b6fb79b033b6ff4ab1b3b9
Filipe Mota <filipe@unevenlabs.com>
2023-06-21 11:13:11 +0200

    feat: turned off ITEM_RECEIVED_BID temporarily
    Signed-off-by: Filipe Mota <filipe@unevenlabs.com>

--- packages/indexer/src/websockets/opensea/index.ts
+++ packages/indexer/src/websockets/opensea/index.ts
@@ -57,7 +57,7 @@ if (config.doWebsocketWork && config.openSeaApiKey) {
     [
       EventType.ITEM_LISTED,
       EventType.COLLECTION_OFFER,
-      EventType.ITEM_RECEIVED_BID,
+      // EventType.ITEM_RECEIVED_BID,
       EventType.TRAIT_OFFER,
       EventType.ITEM_CANCELLED,
       EventType.ORDER_REVALIDATE,
@@ -212,17 +212,17 @@ export const handleEvent = async (
 
 type ProtocolData =
   | {
-      kind: "seaport";
-      order: Sdk.SeaportV11.Order;
-    }
+    kind: "seaport";
+    order: Sdk.SeaportV11.Order;
+  }
   | {
-      kind: "seaport-v1.4";
-      order: Sdk.SeaportV14.Order;
-    }
+    kind: "seaport-v1.4";
+    order: Sdk.SeaportV14.Order;
+  }
   | {
-      kind: "seaport-v1.5";
-      order: Sdk.SeaportV15.Order;
-    };
+    kind: "seaport-v1.5";
+    order: Sdk.SeaportV15.Order;
+  };
 
 export const parseProtocolData = (payload: unknown): ProtocolData | undefined => {
   try {
