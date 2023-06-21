14ea61dbce6f05817aa0ac135a25845bf2b7202d
Idan Peleg <ipeleg@hotmail.com>
2023-06-20 20:29:38 -0400

    feat: enable nonFlaggedFloor queue

--- packages/indexer/src/jobs/index.ts
+++ packages/indexer/src/jobs/index.ts
@@ -169,7 +169,7 @@ import { topBidSingleTokenQueueJob } from "@/jobs/token-set-updates/top-bid-sing
 // import { fixActivitiesMissingCollectionJob } from "@/jobs/activities/fix-activities-missing-collection-job";
 // import { collectionMetadataQueueJob } from "@/jobs/collection-updates/collection-metadata-queue-job";
 // import { rarityQueueJob } from "@/jobs/collection-updates/rarity-queue-job";
-// import { nonFlaggedFloorQueueJob } from "@/jobs/collection-updates/non-flagged-floor-queue-job";
+import { nonFlaggedFloorQueueJob } from "@/jobs/collection-updates/non-flagged-floor-queue-job";
 // import { refreshContractCollectionsMetadataQueueJob } from "@/jobs/collection-updates/refresh-contract-collections-metadata-queue-job";
 // import { setCommunityQueueJob } from "@/jobs/collection-updates/set-community-queue-job";
 import { topBidCollectionJob } from "@/jobs/collection-updates/top-bid-collection-job";
@@ -344,7 +344,7 @@ export class RabbitMqJobsConsumer {
       // fixActivitiesMissingCollectionJob,
       // collectionMetadataQueueJob,
       // rarityQueueJob,
-      // nonFlaggedFloorQueueJob,
+      nonFlaggedFloorQueueJob,
       // refreshContractCollectionsMetadataQueueJob,
       // setCommunityQueueJob,
       topBidCollectionJob,
