f885f1e7f82868868c04c042bde72d50519e2a36
Idan Peleg <ipeleg@hotmail.com>
2023-06-22 14:12:38 -0400

    feat: move process backfill to rabbit

---
+++ packages/indexer/src/jobs/events-sync/process/events-sync-process-backfill.ts
@@ -0,0 +1,28 @@
+import { AbstractRabbitMqJobHandler } from "@/jobs/abstract-rabbit-mq-job-handler";
+
+import { logger } from "@/common/logger";
+import { EventsBatch, processEventsBatch } from "@/events-sync/handlers";
+
+export class EventsSyncProcessBackfillJob extends AbstractRabbitMqJobHandler {
+  queueName = "events-sync-process-backfill";
+  maxRetries = 10;
+  concurrency = 15;
+  lazyMode = true;
+
+  protected async process(payload: EventsBatch) {
+    const { id, events, backfill } = payload;
+
+    try {
+      await processEventsBatch({ id, events, backfill });
+    } catch (error) {
+      logger.error(this.queueName, `Events processing failed: ${error}`);
+      throw error;
+    }
+  }
+
+  public async addToQueue(batches: EventsBatch[]) {
+    await this.sendBatch(batches.map((batch) => ({ payload: batch })));
+  }
+}
+
+export const eventsSyncProcessBackfillJob = new EventsSyncProcessBackfillJob();
