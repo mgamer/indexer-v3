f87dca0333e1669030713924e7509adc75fbd001
lljxx1 <lljxx1@gmail.com>
2023-06-26 13:44:31 +0800

    fix: merge with upstream

--- packages/indexer/src/sync/events/handlers/payment-processor.ts
+++ packages/indexer/src/sync/events/handlers/payment-processor.ts
@@ -41,7 +41,7 @@ export const handleEvents = async (events: EnhancedEvent[], onChainData: OnChain
 
         // Cancel all related orders across maker
         onChainData.bulkCancelEvents.push({
-          orderKind: "element-erc721",
+          orderKind: "payment-processor",
           maker,
           minNonce: newNonce,
           acrossAll: true,
@@ -84,6 +84,7 @@ export const handleEvents = async (events: EnhancedEvent[], onChainData: OnChain
             );
             if (executeCallTrace) {
               executeCallTraceInput = executeCallTrace.input;
+              trades.order.set(`${txHash}-${exchangeAddress}`, tradeRank + 1);
             }
           } else {
             executeCallTraceInput = transaction.data;
@@ -234,8 +235,6 @@ export const handleEvents = async (events: EnhancedEvent[], onChainData: OnChain
             baseEventParams.timestamp
           );
 
-          priceData.nativePrice = currencyPrice;
-
           if (!priceData.nativePrice) {
             // We must always have the native price
             break;
@@ -277,63 +276,61 @@ export const handleEvents = async (events: EnhancedEvent[], onChainData: OnChain
         break;
       }
 
-      // case "payment-processor-sweep-collection-erc721":
-      // case "payment-processor-sweep-collection-erc1155": {
-      //   const isERC1155 = subKind === "payment-processor-sweep-collection-erc1155";
-      //   const parsedLog = eventData.abi.parseLog(log);
+      case "payment-processor-sweep-collection-erc721":
+      case "payment-processor-sweep-collection-erc1155": {
+        const isERC1155 = subKind === "payment-processor-sweep-collection-erc1155";
+        const parsedLog = eventData.abi.parseLog(log);
 
-      //   const tokenAddress = parsedLog.args["tokenAddress"].toLowerCase();
-      //   const tokenIds = parsedLog.args["tokenIds"].map(String);
-      //   const currency = parsedLog.args["paymentCoin"].toLowerCase();
+        const tokenAddress = parsedLog.args["tokenAddress"].toLowerCase();
+        const tokenIds = parsedLog.args["tokenIds"].map(String);
+        const currency = parsedLog.args["paymentCoin"].toLowerCase();
 
-      //   const buyer = parsedLog.args["buyer"].toLowerCase();
+        const buyer = parsedLog.args["buyer"].toLowerCase();
 
-      //   for (let index = 0; index < tokenIds.length; index++) {
-      //     const tokenId = tokenIds[index];
-      //     const currencyPrice = parsedLog.args["salePrices"][index].toString();
-      //     const seller = parsedLog.args["sellers"][index].toLowerCase();
+        for (let index = 0; index < tokenIds.length; index++) {
+          const tokenId = tokenIds[index];
+          const currencyPrice = parsedLog.args["salePrices"][index].toString();
+          const seller = parsedLog.args["sellers"][index].toLowerCase();
 
-      //     const priceData = await getUSDAndNativePrices(
-      //       currency,
-      //       currencyPrice,
-      //       baseEventParams.timestamp
-      //     );
+          const priceData = await getUSDAndNativePrices(
+            currency,
+            currencyPrice,
+            baseEventParams.timestamp
+          );
 
-      //     if (!priceData.nativePrice) {
-      //       // We must always have the native price
-      //       break;
-      //     }
+          if (!priceData.nativePrice) {
+            // We must always have the native price
+            break;
+          }
 
-      //     // Handle: attribution
-      //     const orderKind = "payment-processor";
-      //     const orderId = "";
-      //     const attributionData = await utils.extractAttributionData(
-      //       baseEventParams.txHash,
-      //       orderKind,
-      //       { orderId }
-      //     );
+          // Handle: attribution
+          const orderKind = "payment-processor";
+          const attributionData = await utils.extractAttributionData(
+            baseEventParams.txHash,
+            orderKind
+          );
 
-      //     const amount = isERC1155 ? parsedLog.args["amounts"][index].toString() : "1";
-      //     onChainData.fillEvents.push({
-      //       orderKind: "payment-processor",
-      //       orderSide: "sell",
-      //       maker: seller,
-      //       taker: buyer,
-      //       price: priceData.nativePrice,
-      //       currency,
-      //       currencyPrice,
-      //       usdPrice: priceData.usdPrice,
-      //       contract: tokenAddress,
-      //       tokenId: tokenId,
-      //       amount: amount,
-      //       orderSourceId: attributionData.orderSource?.id,
-      //       aggregatorSourceId: attributionData.aggregatorSource?.id,
-      //       fillSourceId: attributionData.fillSource?.id,
-      //       baseEventParams,
-      //     });
-      //   }
-      //   break;
-      // }
+          const amount = isERC1155 ? parsedLog.args["amounts"][index].toString() : "1";
+          onChainData.fillEvents.push({
+            orderKind: "payment-processor",
+            orderSide: "sell",
+            maker: seller,
+            taker: buyer,
+            price: priceData.nativePrice,
+            currency,
+            currencyPrice,
+            usdPrice: priceData.usdPrice,
+            contract: tokenAddress,
+            tokenId: tokenId,
+            amount: amount,
+            orderSourceId: attributionData.orderSource?.id,
+            aggregatorSourceId: attributionData.aggregatorSource?.id,
+            fillSourceId: attributionData.fillSource?.id,
+            baseEventParams,
+          });
+        }
+        break;
+      }
     }
   }
 };
