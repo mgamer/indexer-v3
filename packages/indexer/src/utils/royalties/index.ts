dafe8c765cbc49240645f4b640d747d967ba614e
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-06-27 20:35:19 -0400

    Ofir/platf 1605 collection missing os royalties (#5331)
    * feat: added log
    
    * feat: updated log

--- packages/indexer/src/utils/royalties/index.ts
+++ packages/indexer/src/utils/royalties/index.ts
@@ -217,12 +217,16 @@ export const refreshDefaultRoyalties = async (collection: string) => {
     return [];
   }
 
+  let defaultRoyaltiesType = "None";
+
   // Default royalties priority: custom, on-chain, opensea
   let defaultRoyalties: Royalty[] = [];
   if (royaltiesResult.new_royalties["custom"]) {
     defaultRoyalties = royaltiesResult.new_royalties["custom"];
+    defaultRoyaltiesType = "custom";
   } else if (royaltiesResult.new_royalties["onchain"]) {
     defaultRoyalties = royaltiesResult.new_royalties["onchain"];
+    defaultRoyaltiesType = "onchain";
   } else if (
     // TODO: Remove (for backwards-compatibility only)
     Object.keys(royaltiesResult.new_royalties).find((kind) => !["custom", "opensea"].includes(kind))
@@ -231,8 +235,10 @@ export const refreshDefaultRoyalties = async (collection: string) => {
       (kind) => !["custom", "opensea"].includes(kind)
     );
     defaultRoyalties = royaltiesResult.new_royalties[oldSpec!];
+    defaultRoyaltiesType = "oldSpec";
   } else if (royaltiesResult.new_royalties["opensea"]) {
     defaultRoyalties = royaltiesResult.new_royalties["opensea"];
+    defaultRoyaltiesType = "opensea";
   }
 
   logger.info(
@@ -242,6 +248,7 @@ export const refreshDefaultRoyalties = async (collection: string) => {
       data: {
         collection,
         defaultRoyalties,
+        defaultRoyaltiesType,
         royaltiesResult,
       },
     })
