b8815c80f97a1d268f9cde8bd81b6457a3ad74ba
d3or <deorrunner@gmail.com>
2023-06-28 10:53:41 -0400

    fix: fix query to split on token set

--- packages/indexer/src/jobs/token-set-updates/top-bid-queue.ts
+++ packages/indexer/src/jobs/token-set-updates/top-bid-queue.ts
@@ -67,52 +67,91 @@ export const jobProcessor = async (job: Job) => {
   try {
     let tokenSetTopBid = await idb.manyOrNone(
       `
-                WITH x AS (
-                  SELECT
-                    token_sets.id AS token_set_id,
-                    y.*,
-                    z.*
-                  FROM token_sets
-                  LEFT JOIN LATERAL (
-                    SELECT
-                      orders.id AS order_id,
-                      orders.value,
-                      orders.maker
-                    FROM orders
-                    WHERE orders.token_set_id = token_sets.id
-                      AND orders.side = 'buy'
-                      AND orders.fillability_status = 'fillable'
-                      AND orders.approval_status = 'approved'
-                      AND (orders.taker = '\\x0000000000000000000000000000000000000000' OR orders.taker IS NULL)
-                    ORDER BY orders.value DESC
-                    LIMIT 1
-                  ) y ON TRUE
-                  LEFT JOIN LATERAL (
-                    SELECT
-                      top_buy_value
-                    FROM collections
-                    WHERE collections.id = token_sets.collection_id
-                  ) z ON TRUE
-                  WHERE token_sets.id = $/tokenSetId/
-                )
-                UPDATE token_sets SET
-                  top_buy_id = x.order_id,
-                  top_buy_value = x.value,
-                  top_buy_maker = x.maker,
-                  attribute_id = token_sets.attribute_id,
-                  collection_id = token_sets.collection_id
-                FROM x
-                WHERE token_sets.id = x.token_set_id
-                  AND (
-                    token_sets.top_buy_id IS DISTINCT FROM x.order_id
-                    OR token_sets.top_buy_value IS DISTINCT FROM x.value
-                  )
-                RETURNING
-                  collection_id AS "collectionId",
-                  attribute_id AS "attributeId",
-                  top_buy_value AS "topBuyValue",
-                  top_buy_id AS "topBuyId",
-                  collection_top_buy_value AS "collectionTopBuyValue"
+                          WITH x AS
+              (SELECT token_sets.id AS token_set_id,
+                      y.*,
+                      z.*,
+                      o.*
+              FROM token_sets
+              LEFT JOIN LATERAL
+                (SELECT orders.id AS order_id,
+                        orders.value,
+                        orders.maker
+                  FROM orders
+                  WHERE orders.token_set_id = token_sets.id
+                    AND orders.side = 'buy'
+                    AND orders.fillability_status = 'fillable'
+                    AND orders.approval_status = 'approved'
+                    AND (orders.taker = '\\x0000000000000000000000000000000000000000'
+                        OR orders.taker IS NULL)
+                  ORDER BY orders.value DESC
+                  LIMIT 1) y ON TRUE
+              LEFT JOIN LATERAL
+                (SELECT (CASE
+                              WHEN $/tokenSetId/ LIKE 'token:%' THEN
+                                    (SELECT collections.id
+                                      FROM tokens
+                                      JOIN collections ON tokens.collection_id = collections.id
+                                      WHERE tokens.contract = decode(substring(split_part($/tokenSetId/, ':', 2)
+                                                                              FROM 3), 'hex')
+                                        AND tokens.token_id = (split_part($/tokenSetId/, ':', 3)::NUMERIC(78, 0)))
+                              WHEN $/tokenSetId/ LIKE 'contract:%' THEN
+                                    (SELECT collections.id
+                                      FROM collections
+                                      WHERE collections.id = substring($/tokenSetId/
+                                                                      FROM 10))
+                              WHEN $/tokenSetId/ LIKE 'range:%' THEN
+                                    (SELECT collections.id
+                                      FROM collections
+                                      WHERE collections.id = substring($/tokenSetId/
+                                                                      FROM 7))
+                              WHEN $/tokenSetId/ LIKE 'list:%' THEN
+                                    (SELECT CASE
+                                                WHEN token_sets.collection_id IS NULL
+                                                      AND token_sets.attribute_id IS NULL THEN
+                                                        (SELECT NULL)
+                                                WHEN token_sets.attribute_id IS NULL THEN
+                                                        (SELECT collections.id
+                                                        FROM collections
+                                                        WHERE token_sets.collection_id = collections.id)
+                                                ELSE
+                                                        (SELECT collections.id
+                                                        FROM attributes
+                                                        JOIN attribute_keys ON attributes.attribute_key_id = attribute_keys.id
+                                                        JOIN collections ON attribute_keys.collection_id = collections.id
+                                                        WHERE token_sets.attribute_id = attributes.id)
+                                            END
+                                      FROM token_sets
+                                      WHERE token_sets.id = $/tokenSetId/
+                                      LIMIT 1)
+                              WHEN $/tokenSetId/ LIKE 'dynamic:collection-non-flagged:%' THEN
+                                    (SELECT collections.id
+                                      FROM collections
+                                      WHERE collections.id = substring($/tokenSetId/
+                                                                      FROM 32))
+                              ELSE NULL
+                          END) AS "collectionId") o ON TRUE
+              LEFT JOIN LATERAL
+                (SELECT top_buy_value AS collection_top_buy_value
+                  FROM collections
+                  WHERE collections.id = o."collectionId" ) z ON TRUE
+              WHERE token_sets.id = $/tokenSetId/ )
+            UPDATE token_sets
+            SET top_buy_id = x.order_id,
+                top_buy_value = x.value,
+                top_buy_maker = x.maker,
+                attribute_id = token_sets.attribute_id,
+                collection_id = token_sets.collection_id
+            FROM x
+            WHERE token_sets.id = x.token_set_id
+              AND (token_sets.top_buy_id IS DISTINCT
+                  FROM x.order_id
+                  OR token_sets.top_buy_value IS DISTINCT
+                  FROM x.value) RETURNING collection_id AS "collectionId",
+                                          attribute_id AS "attributeId",
+                                          top_buy_value AS "topBuyValue",
+                                          top_buy_id AS "topBuyId",
+                                          collection_top_buy_value AS "collectionTopBuyValue"
               `,
       { tokenSetId }
     );
