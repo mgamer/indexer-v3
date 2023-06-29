5e65e4db949b0ca63062d7ba00900791f13117ca
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-06-29 09:32:19 -0400

    Feat/reduce orderbook concurrency (#5355)
    * feat: reduced concurrency
    
    * feat: reduce bids concurrency

--- packages/indexer/src/jobs/orderbook/opensea-bids-queue-job.ts
+++ packages/indexer/src/jobs/orderbook/opensea-bids-queue-job.ts
@@ -11,7 +11,7 @@ export type OpenseaBidsQueueJobPayload = {
 export class OpenseaBidsQueueJob extends AbstractRabbitMqJobHandler {
   queueName = "orderbook-opensea-bids-queue";
   maxRetries = 10;
-  concurrency = 100;
+  concurrency = 40;
   lazyMode = true;
   consumerTimeout = 30000;
   disableConsuming = config.chainId === 1 ? true : false;
