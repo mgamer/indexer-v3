e2134bd10cb5b9076870f19760f9507c7fc30199
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-06-29 09:29:01 -0400

    feat: reduced concurrency (#5354)

--- packages/indexer/src/websockets/opensea/index.ts
+++ packages/indexer/src/websockets/opensea/index.ts
@@ -56,21 +56,23 @@ if (config.doWebsocketWork && config.openSeaApiKey) {
 
   client.onEvents(
     "*",
-    config.chainId === 1 ? [
-      EventType.ITEM_LISTED,
-      EventType.COLLECTION_OFFER,
-      // EventType.ITEM_RECEIVED_BID,
-      EventType.TRAIT_OFFER,
-      EventType.ITEM_CANCELLED,
-      EventType.ORDER_REVALIDATE,
-    ] : [
-      EventType.ITEM_LISTED,
-      EventType.COLLECTION_OFFER,
-      EventType.ITEM_RECEIVED_BID,
-      EventType.TRAIT_OFFER,
-      EventType.ITEM_CANCELLED,
-      EventType.ORDER_REVALIDATE,
-    ],
+    config.chainId === 1
+      ? [
+          EventType.ITEM_LISTED,
+          EventType.COLLECTION_OFFER,
+          // EventType.ITEM_RECEIVED_BID,
+          EventType.TRAIT_OFFER,
+          EventType.ITEM_CANCELLED,
+          EventType.ORDER_REVALIDATE,
+        ]
+      : [
+          EventType.ITEM_LISTED,
+          EventType.COLLECTION_OFFER,
+          EventType.ITEM_RECEIVED_BID,
+          EventType.TRAIT_OFFER,
+          EventType.ITEM_CANCELLED,
+          EventType.ORDER_REVALIDATE,
+        ],
     async (event) => {
       try {
         if (await isDuplicateEvent(event)) {
