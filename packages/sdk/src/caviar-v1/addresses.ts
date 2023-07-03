168abcec3a1362756744a52743c7fabfb650cc80
outdoteth <outdoteth@users.noreply.github.com>
2023-07-03 15:39:04 +0100

    fix: lowecrase addy

--- packages/sdk/src/caviar-v1/addresses.ts
+++ packages/sdk/src/caviar-v1/addresses.ts
@@ -1,5 +1,5 @@
 import { ChainIdToAddress, Network } from "../utils";
 
 export const CaviarFactoryContract: ChainIdToAddress = {
-  [Network.Ethereum]: "0xa964D6E8d90E5Cd12592a8EF2b1735daE9Ba0840",
+  [Network.Ethereum]: "0xa964d6e8d90e5cd12592a8ef2b1735dae9ba0840",
 };
