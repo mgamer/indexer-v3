44bb5b18b3eda44cf4f75a2c1b5b5b5d9e8702fc
d3or <deorrunner@gmail.com>
2023-07-11 23:46:27 -0400

    feat: disable approval ws

--- packages/indexer/src/jobs/websocket-events/index.ts
+++ packages/indexer/src/jobs/websocket-events/index.ts
@@ -1,6 +1,5 @@
 import "@/jobs/websocket-events/ask-websocket-events-trigger-queue";
 import "@/jobs/websocket-events/bid-websocket-events-trigger-queue";
-import "@/jobs/websocket-events/approval-websocket-events-trigger-queue";
 import "@/jobs/websocket-events/transfer-websocket-events-trigger-queue";
 import "@/jobs/websocket-events/token-websocket-events-trigger-queue";
 import "@/jobs/websocket-events/sale-websocket-events-trigger-queue";
