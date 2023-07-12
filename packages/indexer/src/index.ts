dd7b34cb3821e84a621dd843e4efafa59e985ea1
Idan Peleg <ipeleg@hotmail.com>
2023-07-12 17:03:39 -0400

    feat: update comment

--- packages/indexer/src/index.ts
+++ packages/indexer/src/index.ts
@@ -10,7 +10,6 @@ import { config } from "@/config/index";
 if (process.env.LOCAL_TESTING) {
   import("./setup");
 } else {
-  // First assert queues / exchanges and connect to rabbit, then import setup and start the server
   RabbitMq.connect().then(async () => {
     // Sync the pods so rabbit queues assertion will run only once per deployment by a single pod
     if (await acquireLock(config.imageTag, 60 * 60 * 24)) {
