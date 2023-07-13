9d444d200c32bbdcf464198854f0bc6dbabc619c
Idan Peleg <ipeleg@hotmail.com>
2023-07-13 15:59:42 -0400

    feat: update comment

--- packages/indexer/src/index.ts
+++ packages/indexer/src/index.ts
@@ -14,7 +14,7 @@ if (process.env.LOCAL_TESTING) {
     // Sync the pods so rabbit queues assertion will run only once per deployment by a single pod
     if (await acquireLock(config.imageTag, 75)) {
       await RabbitMq.assertQueuesAndExchanges();
-      await redis.set(config.imageTag, "DONE", "EX", 60 * 60 * 24);
+      await redis.set(config.imageTag, "DONE", "EX", 60 * 60 * 24); // Update the lock ttl
       import("./setup");
     } else {
       // Check every 1s if the rabbit queues assertion completed
