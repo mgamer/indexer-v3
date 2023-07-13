2c3464cfcf279be03b08df71d9dd00c8419f1be6
lljxx1 <lljxx1@gmail.com>
2023-07-13 16:39:11 +0800

    feat: looksrare seaport order support

--- packages/indexer/src/jobs/orderbook/post-order-external/api/looksrare/index.ts
+++ packages/indexer/src/jobs/orderbook/post-order-external/api/looksrare/index.ts
@@ -87,3 +87,71 @@ export const postOrder = async (order: Sdk.LooksRareV2.Order, apiKey: string) =>
     },
   ]);
 };
+
+export const postSeaportOrder = async (order: Sdk.SeaportV15.Order, apiKey: string) => {
+  const lrOrder = {
+    parameters: {
+      ...order.params,
+      totalOriginalConsiderationItems: order.params.consideration.length,
+    },
+    signature: order.params.signature!,
+  };
+
+  await axios
+    .post(
+      `https://${config.chainId === 5 ? "api-goerli." : "api."}looksrare.org/api/v2/orders/seaport`,
+      JSON.stringify(lrOrder),
+      {
+        headers:
+          config.chainId === 1
+            ? {
+                "Content-Type": "application/json",
+                "X-Looks-Api-Key": apiKey || config.looksRareApiKey,
+              }
+            : {
+                "Content-Type": "application/json",
+              },
+      }
+    )
+    .catch((error) => {
+      if (error.response) {
+        logger.error(
+          "looksrare-orderbook-api",
+          `Failed to post order to LooksRare. order=${JSON.stringify(
+            lrOrder
+          )}, apiKey=${apiKey}, status=${error.response.status}, data=${JSON.stringify(
+            error.response.data
+          )}`
+        );
+
+        switch (error.response.status) {
+          case 429: {
+            throw new RequestWasThrottledError(
+              "Request was throttled by LooksRare",
+              RATE_LIMIT_INTERVAL
+            );
+          }
+          case 400:
+          case 401:
+            throw new InvalidRequestError(
+              `Request was rejected by LooksRare. error=${JSON.stringify(
+                error.response.data.errors ?? error.response.data.message
+              )}`
+            );
+        }
+      }
+
+      throw new Error(`Failed to post order to LooksRare`);
+    });
+
+  // If the cross-posting was successful, save the order directly
+  await orderbookOrdersJob.addToQueue([
+    {
+      kind: "seaport-v1.5",
+      info: {
+        orderParams: order.params,
+        metadata: {},
+      },
+    },
+  ]);
+};
