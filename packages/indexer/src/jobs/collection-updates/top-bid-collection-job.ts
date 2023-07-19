c0d6b2f932b5e798ec53205c759cdea6c31e2457
d3or <deorrunner@gmail.com>
2023-07-19 15:32:46 -0400

    feat: add log for clearing collection cache

--- packages/indexer/src/jobs/collection-updates/top-bid-collection-job.ts
+++ packages/indexer/src/jobs/collection-updates/top-bid-collection-job.ts
@@ -143,6 +143,10 @@ export class TopBidCollectionJob extends AbstractRabbitMqJobHandler {
           }
         } else {
           // clear the cache
+          logger.info(
+            this.queueName,
+            `Clearing collection top-bid value for collection ${payload.collectionId}`
+          );
           await topBidsCache.clearCacheCollectionTopBidValue(payload.collectionId);
         }
       } catch (error) {
