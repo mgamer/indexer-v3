c88db0290e3e28b19d1eaf673be8cad1ad8e96b4
George Roman <george.roman.99@gmail.com>
2023-07-24 15:02:39 +0200

    feat: deploy contracts to linea

--- packages/sdk/src/common/addresses.ts
+++ packages/sdk/src/common/addresses.ts
@@ -72,6 +72,7 @@ export const RoyaltyEngine: ChainIdToAddress = {
   [Network.ArbitrumNova]: "0x60b433ee91680189deb2c94b0b062ea283f6b4dd",
   [Network.Zora]: "0x8755310f937528173e7c5a106131d79a3601c9d9",
   [Network.Base]: "0x8755310f937528173e7c5a106131d79a3601c9d9",
+  [Network.Linea]: "0x8755310f937528173e7c5a106131d79a3601c9d9",
 };
 
 // Uniswap SwapRouter
@@ -100,6 +101,7 @@ export const Create3Factory: ChainIdToAddress = {
   [Network.BaseGoerli]: "0x0000000000000a9efe52e741bcb25da0e4438e71",
   [Network.Zora]: "0x0000000000000a9efe52e741bcb25da0e4438e71",
   [Network.Base]: "0x0000000000000a9efe52e741bcb25da0e4438e71",
+  [Network.Linea]: "0x0000000000000a9efe52e741bcb25da0e4438e71",
 };
 
 export const GelatoRelay1BalanceERC2771: ChainIdToAddress = {
