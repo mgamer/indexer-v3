5285e3d6e10a36c0dfde593ebce6defc3a9c42ff
Idan Peleg <ipeleg@hotmail.com>
2023-07-25 10:50:54 -0400

    feat: set completed time

--- packages/indexer/src/jobs/abstract-rabbit-mq-job-handler.ts
+++ packages/indexer/src/jobs/abstract-rabbit-mq-job-handler.ts
@@ -59,8 +59,8 @@ export abstract class AbstractRabbitMqJobHandler extends (EventEmitter as new ()
       const processResult = await this.process(this.rabbitMqMessage.payload); // Process the message
 
       await channel.ack(consumeMessage); // Ack the message with rabbit
-      this.emit("onCompleted", this.rabbitMqMessage, processResult); // Emit on Completed event
       this.rabbitMqMessage.completeTime = _.now(); // Set the complete time
+      this.emit("onCompleted", this.rabbitMqMessage, processResult); // Emit on Completed event
 
       // Release lock if there's a job id with no delay
       if (this.rabbitMqMessage.jobId && !this.rabbitMqMessage.delay) {
