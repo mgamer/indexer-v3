1c3fa20d0e5041577dc900a2f7f4dab6745502c5
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-07-31 16:10:27 -0400

    Ofir/platf 1899 metadata queue size alarm (#5798)
    * feat: updated log
    
    * feat: changed metric logic

--- packages/indexer/src/jobs/monitoring/queue-monitoring.ts
+++ packages/indexer/src/jobs/monitoring/queue-monitoring.ts
@@ -6,28 +6,43 @@ import { eventsSyncProcessRealtimeJob } from "@/jobs/events-sync/process/events-
 import { RabbitMq } from "@/common/rabbit-mq";
 import { eventsSyncRealtimeJob } from "@/jobs/events-sync/events-sync-realtime-job";
 import { openseaBidsQueueJob } from "@/jobs/orderbook/opensea-bids-queue-job";
+import { redlock } from "@/common/redis";
 
 if (config.doBackgroundWork) {
-  cron.schedule("* * * * *", async () => {
-    // Log metadata queue length
-    for (const method of ["opensea"]) {
-      const pendingRefreshTokens = new PendingRefreshTokens(method);
-      const queueLength = await pendingRefreshTokens.length();
-      logger.info("metadata-queue-length", JSON.stringify({ method, queueLength }));
-    }
+  cron.schedule(
+    "* * * * *",
+    async () =>
+      await redlock
+        .acquire([`queue-monitoring-cron-lock`], (60 - 5) * 1000)
+        .then(async () => {
+          // Log token metadata queue length
+          const pendingRefreshTokens = new PendingRefreshTokens(config.metadataIndexingMethod);
+          const pendingRefreshTokensCount = await pendingRefreshTokens.length();
 
-    for (const queue of [
-      eventsSyncProcessRealtimeJob,
-      eventsSyncRealtimeJob,
-      openseaBidsQueueJob,
-    ]) {
-      const queueSize = await RabbitMq.getQueueSize(queue.getQueue());
-      const retryQueueSize = await RabbitMq.getQueueSize(queue.getRetryQueue());
+          logger.info(
+            "pending-refresh-tokens-metric",
+            JSON.stringify({
+              metadataIndexingMethod: config.metadataIndexingMethod,
+              pendingRefreshTokensCount,
+            })
+          );
 
-      logger.info(
-        "queue-monitoring",
-        JSON.stringify({ queue: queue.queueName, jobCount: queueSize + retryQueueSize })
-      );
-    }
-  });
+          for (const queue of [
+            eventsSyncProcessRealtimeJob,
+            eventsSyncRealtimeJob,
+            openseaBidsQueueJob,
+          ]) {
+            const queueSize = await RabbitMq.getQueueSize(queue.getQueue());
+            const retryQueueSize = await RabbitMq.getQueueSize(queue.getRetryQueue());
+
+            logger.info(
+              "queue-monitoring",
+              JSON.stringify({ queue: queue.queueName, jobCount: queueSize + retryQueueSize })
+            );
+          }
+        })
+        .catch(() => {
+          // Skip on any errors
+        })
+  );
 }
