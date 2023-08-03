5392481200dc6b0cee196b808222fd0d205181c7
Idan Peleg <ipeleg@hotmail.com>
2023-08-03 12:35:20 +0300

    feat: enable assertion for zksync

--- packages/indexer/src/index.ts
+++ packages/indexer/src/index.ts
@@ -15,7 +15,7 @@ if (process.env.LOCAL_TESTING) {
     .then(async () => {
       // Sync the pods so rabbit queues assertion will run only once per deployment by a single pod
       if (await acquireLock(config.imageTag, 75)) {
-        if (config.chainId === 5) {
+        if (config.chainId === 324) {
           await RabbitMq.assertQueuesAndExchanges();
         }
         await redis.set(config.imageTag, "DONE", "EX", 60 * 60 * 24); // Update the lock ttl
