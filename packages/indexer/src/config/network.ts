eb8389f3b621be47d61204f7743230876b169ddd
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-08-10 12:26:47 -0400

    feat: added support for all os chains (#5940)

--- packages/indexer/src/config/network.ts
+++ packages/indexer/src/config/network.ts
@@ -76,32 +76,38 @@ export const getNetworkName = () => {
 
 export const getOpenseaNetworkName = () => {
   switch (config.chainId) {
+    case 1:
+      return "ethereum";
     case 5:
       return "goerli";
-
-    case 10:
-      return "optimism";
-
-    case 56:
-      return "bsc";
-
     case 137:
       return "matic";
-
-    case 324:
-      return "zksync";
-
+    case 10:
+      return "optimism";
     case 42161:
       return "arbitrum";
-
-    case 80001:
-      return "mumbai";
-
+    case 42170:
+      return "arbitrum_nova";
+    case 56:
+      return "bsc";
+    case 43114:
+      return "avalanche";
     case 11155111:
       return "sepolia";
-
+    case 80001:
+      return "mumbai";
+    case 8453:
+      return "base";
+    case 84531:
+      return "base_goerli";
+    case 324:
+      return "zksync";
+    case 7777777:
+      return "zora";
+    case 999:
+      return "zora_testnet";
     default:
-      return "ethereum";
+      return null;
   }
 };
 
