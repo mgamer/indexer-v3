9bccefd23ba64b6218b7160a88f8abf77f3b5bde
tv <tv3636@gmail.com>
2023-08-17 14:34:40 -0700

    feat: add backfill job for collection creator

---
+++ packages/indexer/src/jobs/backfill/backfill-collection-creator-job.ts
@@ -0,0 +1,76 @@
+import { idb } from "@/common/db";
+import { fromBuffer } from "@/common/utils";
+import { config } from "@/config/index";
+import { fetchCollectionMetadataJob } from "@/jobs/token-updates/fetch-collection-metadata-job";
+import { AbstractRabbitMqJobHandler, BackoffStrategy } from "@/jobs/abstract-rabbit-mq-job-handler";
+
+export type BackfillCollectionCreatorJobPayload = {
+  continuation?: string;
+};
+
+export class BackfillCollectionCreatorJob extends AbstractRabbitMqJobHandler {
+  queueName = "backfill-collection-creator";
+  maxRetries = 1;
+  concurrency = 1;
+  singleActiveConsumer = true;
+  useSharedChannel = true;
+  backoff = {
+    type: "exponential",
+    delay: 10000,
+  } as BackoffStrategy;
+  intervalInSeconds = 5;
+
+  protected async process(payload: BackfillCollectionCreatorJobPayload) {
+    const limit = 100;
+    const { continuation } = payload;
+    const result = await idb.manyOrNone(
+      `
+        WITH x AS (
+          SELECT id AS collection_id
+          FROM collections c
+          WHERE creator IS NULL
+          ${continuation ? `AND id > '$/continuation/'` : ""}
+          ORDER BY id
+          LIMIT 100
+        )
+        SELECT x.*, y.* FROM x
+        LEFT JOIN LATERAL (
+            SELECT t.contract, t.token_id
+            FROM tokens t
+            WHERE t.collection_id = x.collection_id
+            LIMIT 1
+        ) y ON TRUE        
+      `,
+      { limit, continuation }
+    );
+
+    for (const { contract, token_id } of result) {
+      await fetchCollectionMetadataJob.addToQueue([
+        {
+          contract: fromBuffer(contract),
+          tokenId: token_id,
+          allowFallbackCollectionMetadata: false,
+          context: "post-refresh-collection",
+        },
+      ]);
+    }
+
+    if (result.length == limit) {
+      await this.addToQueue(result[result.length - 1].collection_id);
+    }
+  }
+
+  public async addToQueue(events: BackfillCollectionCreatorJobPayload[]) {
+    if (!config.doWebsocketServerWork) {
+      return;
+    }
+
+    await this.sendBatch(
+      events.map((event) => ({
+        payload: event,
+      }))
+    );
+  }
+}
+
+export const backfillCollectionCreatorJob = new BackfillCollectionCreatorJob();
