5e801dbb25bf49b757f3a35f09902c8a9ac521cd
lljxx1 <lljxx1@gmail.com>
2023-08-17 12:37:17 +0800

    feat: apply token-level royalties for payment processor

--- packages/indexer/src/orderbook/orders/payment-processor/build/sell/token.ts
+++ packages/indexer/src/orderbook/orders/payment-processor/build/sell/token.ts
@@ -5,6 +5,7 @@ import { redb } from "@/common/db";
 import { toBuffer } from "@/common/utils";
 import { config } from "@/config/index";
 import * as utils from "@/orderbook/orders/payment-processor/build/utils";
+import * as registry from "@/utils/royalties/registry";
 
 interface BuildOrderOptions extends utils.BaseOrderBuildOptions {
   tokenId: string;
@@ -36,8 +37,15 @@ export const build = async (options: BuildOrderOptions) => {
 
   const builder: BaseBuilder = new Sdk.PaymentProcessor.Builders.SingleToken(config.chainId);
 
+  const tokenRoyalties = await registry.getRegistryRoyalties(options.contract!, options.tokenId);
+  const tokenRoyaltiesBPS = tokenRoyalties.map((r) => r.bps).reduce((a, b) => a + b, 0);
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (buildInfo.params as any).tokenId = options.tokenId;
+  if (tokenRoyaltiesBPS > 0 && tokenRoyaltiesBPS != buildInfo.params.maxRoyaltyFeeNumerator) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (buildInfo.params as any).maxRoyaltyFeeNumerator = tokenRoyaltiesBPS;
+  }
 
   return builder?.build(buildInfo.params);
 };
--- packages/indexer/src/utils/royalties/registry.ts
+++ packages/indexer/src/utils/royalties/registry.ts
@@ -11,6 +11,52 @@ import { Royalty, updateRoyaltySpec } from "@/utils/royalties";
 const DEFAULT_PRICE = "1000000000000000000";
 
 // Assume there are no per-token royalties but everything is per-contract
+export const getRegistryRoyalties = async (token: string, tokenId: string) => {
+  const latestRoyalties: Royalty[] = [];
+  if (Sdk.Common.Addresses.RoyaltyEngine[config.chainId]) {
+    const royaltyEngine = new Contract(
+      Sdk.Common.Addresses.RoyaltyEngine[config.chainId],
+      new Interface([
+        `
+          function getRoyaltyView(
+            address token,
+            uint256 tokenId,
+            uint256 value
+          ) external view returns (
+            address[] recipients,
+            uint256[] amounts
+          )
+        `,
+      ]),
+      baseProvider
+    );
+
+    try {
+      // The royalties are returned in full amounts, but we store them as a percentage
+      // so here we just use a default price (which is a round number) and deduce then
+      // deduce the percentage taken as royalties from that
+      const { recipients, amounts } = await royaltyEngine
+        .getRoyaltyView(token, tokenId, DEFAULT_PRICE)
+        .catch(() => ({ recipients: [], amounts: [] }));
+
+      for (let i = 0; i < amounts.length; i++) {
+        const recipient = recipients[i].toLowerCase();
+        const amount = amounts[i];
+        if (bn(amount).gte(DEFAULT_PRICE)) {
+          throw new Error("Royalty exceeds price");
+        }
+        const bps = Math.round(bn(amount).mul(10000).div(DEFAULT_PRICE).toNumber());
+        latestRoyalties.push({ recipient, bps });
+      }
+    } catch {
+      // Skip errors
+    }
+  }
+
+  return latestRoyalties;
+};
+
+// Assume there are no per-token royalties but everything is per-contract
 export const refreshRegistryRoyalties = async (collection: string) => {
   // Fetch the collection's contract
   const collectionResult = await idb.oneOrNone(
@@ -44,52 +90,12 @@ export const refreshRegistryRoyalties = async (collection: string) => {
   const token = fromBuffer(collectionResult.contract);
   const tokenId = tokenResult.token_id;
 
-  if (Sdk.Common.Addresses.RoyaltyEngine[config.chainId]) {
-    const royaltyEngine = new Contract(
-      Sdk.Common.Addresses.RoyaltyEngine[config.chainId],
-      new Interface([
-        `
-          function getRoyaltyView(
-            address token,
-            uint256 tokenId,
-            uint256 value
-          ) external view returns (
-            address[] recipients,
-            uint256[] amounts
-          )
-        `,
-      ]),
-      baseProvider
-    );
+  const latestRoyalties = await getRegistryRoyalties(token, tokenId);
 
-    try {
-      // The royalties are returned in full amounts, but we store them as a percentage
-      // so here we just use a default price (which is a round number) and deduce then
-      // deduce the percentage taken as royalties from that
-      const { recipients, amounts } = await royaltyEngine
-        .getRoyaltyView(token, tokenId, DEFAULT_PRICE)
-        .catch(() => ({ recipients: [], amounts: [] }));
-
-      const latestRoyalties: Royalty[] = [];
-      for (let i = 0; i < amounts.length; i++) {
-        const recipient = recipients[i].toLowerCase();
-        const amount = amounts[i];
-        if (bn(amount).gte(DEFAULT_PRICE)) {
-          throw new Error("Royalty exceeds price");
-        }
-
-        const bps = Math.round(bn(amount).mul(10000).div(DEFAULT_PRICE).toNumber());
-        latestRoyalties.push({ recipient, bps });
-      }
-
-      // Save the retrieved royalty spec
-      await updateRoyaltySpec(
-        collection,
-        "onchain",
-        latestRoyalties.length ? latestRoyalties : undefined
-      );
-    } catch {
-      // Skip errors
-    }
-  }
+  // Save the retrieved royalty spec
+  await updateRoyaltySpec(
+    collection,
+    "onchain",
+    latestRoyalties.length ? latestRoyalties : undefined
+  );
 };
