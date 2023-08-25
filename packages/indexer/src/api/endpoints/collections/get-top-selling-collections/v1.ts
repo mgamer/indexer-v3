f52e6ad6361361e9c1879f9f6fd7badc18dafd4e
pedromcunha <pedro.spinsomewebs@gmail.com>
2023-08-25 15:42:55 -0400

    feat: collections-top-selling-v2

--- packages/indexer/src/api/endpoints/collections/get-top-selling-collections/v1.ts
+++ packages/indexer/src/api/endpoints/collections/get-top-selling-collections/v1.ts
@@ -3,11 +3,9 @@
 import { Request, RouteOptions } from "@hapi/hapi";
 import _ from "lodash";
 import Joi from "joi";
+
 import { logger } from "@/common/logger";
-import { fromBuffer, regex, toBuffer } from "@/common/utils";
-import { redb } from "@/common/db";
-import * as Sdk from "@reservoir0x/sdk";
-import { config } from "@/config/index";
+import { regex } from "@/common/utils";
 
 import {
   getTopSellingCollections,
@@ -15,13 +13,13 @@ import {
 } from "@/elasticsearch/indexes/activities";
 
 import { getJoiPriceObject, JoiPrice } from "@/common/joi";
-import { Sources } from "@/models/sources";
 
-const version = "v5";
+const version = "v1";
 
-export const getTopSellingCollectionsOptions: RouteOptions = {
+export const getTopSellingCollectionsV1Options: RouteOptions = {
   cache: {
     privacy: "public",
+    expiresIn: 10000,
   },
   description: "Top Selling Collections",
   notes: "Get top selling and minting collections",
@@ -33,7 +31,13 @@ export const getTopSellingCollectionsOptions: RouteOptions = {
   },
   validate: {
     query: Joi.object({
-      period: Joi.string().valid("5m", "30m", "1h", "6h", "24h").default("24h"),
+      startTime: Joi.number()
+        .greater(Math.floor((Date.now() - 14 * 24 * 60 * 60 * 1000) / 1000))
+        .default(Math.floor((Date.now() - 24 * 60 * 60 * 1000) / 1000))
+        .description(
+          "Start time in unix timestamp. Must be less than 2 weeks ago. defaults to 24 hours"
+        ),
+      endTime: Joi.number().description("End time in unix timestamp. defaults to now"),
       fillType: Joi.string()
         .lowercase()
         .valid(..._.values(TopSellingFillOptions))
@@ -50,18 +54,6 @@ export const getTopSellingCollectionsOptions: RouteOptions = {
       includeRecentSales: Joi.boolean()
         .default(false)
         .description("If true, 8 recent sales will be included in the response"),
-      normalizeRoyalties: Joi.boolean()
-        .default(false)
-        .description("If true, prices will include missing royalties to be added on-top."),
-      useNonFlaggedFloorAsk: Joi.boolean()
-        .when("normalizeRoyalties", {
-          is: Joi.boolean().valid(true),
-          then: Joi.valid(false),
-        })
-        .default(false)
-        .description(
-          "If true, return the non flagged floor ask. Supported only when `normalizeRoyalties` is false."
-        ),
     }),
   },
   response: {
@@ -71,26 +63,9 @@ export const getTopSellingCollectionsOptions: RouteOptions = {
           id: Joi.string().description("Collection id"),
           name: Joi.string().allow("", null),
           image: Joi.string().allow("", null),
-          description: Joi.string().allow("", null),
           primaryContract: Joi.string().lowercase().pattern(regex.address),
           count: Joi.number().integer(),
           volume: Joi.number(),
-          floorAsk: {
-            id: Joi.string().allow(null),
-            sourceDomain: Joi.string().allow("", null),
-            price: JoiPrice.allow(null),
-            maker: Joi.string().lowercase().pattern(regex.address).allow(null),
-            validFrom: Joi.number().unsafe().allow(null),
-            validUntil: Joi.number().unsafe().allow(null),
-            token: Joi.object({
-              contract: Joi.string().lowercase().pattern(regex.address).allow(null),
-              tokenId: Joi.string().pattern(regex.number).allow(null),
-              name: Joi.string().allow(null),
-              image: Joi.string().allow("", null),
-            })
-              .allow(null)
-              .description("Lowest Ask Price."),
-          },
           recentSales: Joi.array().items(
             Joi.object({
               contract: Joi.string(),
@@ -121,163 +96,51 @@ export const getTopSellingCollectionsOptions: RouteOptions = {
       throw error;
     },
   },
-  handler: async (request: Request, h) => {
-    let cacheTime = 60 * 60;
-    const {
-      period,
-      fillType,
-      limit,
-      includeRecentSales,
-      normalizeRoyalties,
-      useNonFlaggedFloorAsk,
-    } = request.query;
+  handler: async (request: Request) => {
+    const { startTime, endTime, fillType, limit, includeRecentSales } = request.query;
 
     try {
-      let startTime = Math.floor(new Date().getTime() / 1000) - 60 * 24 * 60;
-
-      switch (period) {
-        case "5m": {
-          startTime = Math.floor(new Date().getTime() / 1000) - 5 * 60;
-          cacheTime = 60 * 1;
-          break;
-        }
-        case "30m": {
-          startTime = Math.floor(new Date().getTime() / 1000) - 30 * 60;
-          cacheTime = 60 * 10;
-          break;
-        }
-        case "1h": {
-          startTime = Math.floor(new Date().getTime() / 1000) - 60 * 1 * 60;
-          cacheTime = 60 * 30;
-          break;
-        }
-        case "6h": {
-          startTime = Math.floor(new Date().getTime() / 1000) - 60 * 6 * 60;
-          break;
-        }
-      }
-
       const collectionsResult = await getTopSellingCollections({
         startTime,
+        endTime,
         fillType,
         limit,
         includeRecentSales,
       });
 
-      let floorAskSelectQuery;
-
-      if (normalizeRoyalties) {
-        floorAskSelectQuery = `
-            collections.normalized_floor_sell_id AS floor_sell_id,
-            collections.normalized_floor_sell_value AS floor_sell_value,
-            collections.normalized_floor_sell_maker AS floor_sell_maker,
-            least(2147483647::NUMERIC, date_part('epoch', lower(collections.normalized_floor_sell_valid_between)))::INT AS floor_sell_valid_from,
-            least(2147483647::NUMERIC, coalesce(nullif(date_part('epoch', upper(collections.normalized_floor_sell_valid_between)), 'Infinity'),0))::INT AS floor_sell_valid_until,
-            collections.normalized_floor_sell_source_id_int AS floor_sell_source_id_int
-            `;
-      } else if (useNonFlaggedFloorAsk) {
-        floorAskSelectQuery = `
-            collections.non_flagged_floor_sell_id AS floor_sell_id,
-            collections.non_flagged_floor_sell_value AS floor_sell_value,
-            collections.non_flagged_floor_sell_maker AS floor_sell_maker,
-            least(2147483647::NUMERIC, date_part('epoch', lower(collections.non_flagged_floor_sell_valid_between)))::INT AS floor_sell_valid_from,
-            least(2147483647::NUMERIC, coalesce(nullif(date_part('epoch', upper(collections.non_flagged_floor_sell_valid_between)), 'Infinity'),0))::INT AS floor_sell_valid_until,
-            collections.non_flagged_floor_sell_source_id_int AS floor_sell_source_id_int
-            `;
-      } else {
-        floorAskSelectQuery = `
-            collections.floor_sell_id,
-            collections.floor_sell_value,
-            collections.floor_sell_maker,
-            least(2147483647::NUMERIC, date_part('epoch', lower(collections.floor_sell_valid_between)))::INT AS floor_sell_valid_from,
-            least(2147483647::NUMERIC, coalesce(nullif(date_part('epoch', upper(collections.floor_sell_valid_between)), 'Infinity'),0))::INT AS floor_sell_valid_until,
-            collections.floor_sell_source_id_int
-      `;
-      }
-
-      request.query.contract = collectionsResult.map((collection: any) =>
-        toBuffer(collection.primaryContract)
-      );
-
-      const baseQuery = `
-        SELECT
-          collections.id,
-          collections.contract,
-          (collections.metadata ->> 'description')::TEXT AS "description",
-          ${floorAskSelectQuery}
-          FROM collections
-          WHERE collections.contract IN ($/contract:csv/)
-      `;
-
-      const resultsPromise = redb.manyOrNone(baseQuery, request.query);
-      const recentSalesPromise = collectionsResult.map(async (collection: any) => {
-        return {
-          ...collection,
-          recentSales: await Promise.all(
-            collection.recentSales.map(async (sale: any) => {
-              const { pricing, ...salesData } = sale;
-              const price = pricing
-                ? await getJoiPriceObject(
-                    {
-                      gross: {
-                        amount: String(pricing?.currencyPrice ?? pricing?.price ?? 0),
-                        nativeAmount: String(pricing?.price ?? 0),
-                        usdAmount: String(pricing.usdPrice ?? 0),
-                      },
-                    },
-                    pricing.currency
-                  )
-                : null;
-
-              return {
-                ...salesData,
-                price,
-              };
-            })
-          ),
-        };
-      });
-
-      const responses = await Promise.all([resultsPromise, ...recentSalesPromise]);
-      const collectionMetadataResponse = responses.shift();
-      const collectionsMetadata: Record<string, any> = {};
-      if (collectionMetadataResponse && Array.isArray(collectionMetadataResponse)) {
-        collectionMetadataResponse.forEach((metadata: any) => {
-          collectionsMetadata[fromBuffer(metadata.contract)] = metadata;
-        });
-      }
-      const sources = await Sources.getInstance();
       const collections = await Promise.all(
-        responses.map(async (response) => {
-          const metadata = collectionsMetadata[(response as any).primaryContract] || {};
-          let floorAsk;
-          if (metadata) {
-            const floorAskCurrency = metadata.floor_sell_currency
-              ? fromBuffer(metadata.floor_sell_currency)
-              : Sdk.Common.Addresses.Native[config.chainId];
-            floorAsk = {
-              id: metadata.floor_sell_id,
-              sourceDomain: sources.get(metadata.floor_sell_source_id_int)?.domain,
-              price: metadata.floor_sell_id
-                ? await getJoiPriceObject(
-                    {
-                      gross: {
-                        amount: metadata.floor_sell_currency_value ?? metadata.floor_sell_value,
-                        nativeAmount: metadata.floor_sell_value,
+        collectionsResult.map(async (collection: any) => {
+          return {
+            ...collection,
+            recentSales: await Promise.all(
+              collection.recentSales.map(async (sale: any) => {
+                const { pricing, ...salesData } = sale;
+                const price = pricing
+                  ? await getJoiPriceObject(
+                      {
+                        gross: {
+                          amount: String(pricing?.currencyPrice ?? pricing?.price ?? 0),
+                          nativeAmount: String(pricing?.price ?? 0),
+                          usdAmount: String(pricing.usdPrice ?? 0),
+                        },
                       },
-                    },
-                    floorAskCurrency
-                  )
-                : null,
-            };
-          }
+                      pricing.currency
+                    )
+                  : null;
 
-          return { ...response, description: metadata.description, floorAsk };
+                return {
+                  ...salesData,
+                  price,
+                };
+              })
+            ),
+          };
         })
       );
-      const response = h.response({ collections });
-      response.header("cache-control", `${cacheTime}`);
-      return response;
+
+      return {
+        collections,
+      };
     } catch (error) {
       logger.error(`get-top-selling-collections-${version}-handler`, `Handler failure: ${error}`);
       throw error;
