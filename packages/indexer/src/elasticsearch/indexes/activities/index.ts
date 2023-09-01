f7cf0838ddf647957e12711d772aadd685acda92
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-09-01 12:47:04 -0400

    Feat/bridge to base (#6172)
    * feat: inc concurrency
    
    * feat: add bulk logic
    
    * feat: fix add to queue
    
    * feat: added log
    
    * feat: added log
    
    * feat: disable logic
    
    * feat: fix logic
    
    * feat: fix query
    
    * feat: disable logic
    
    * feat: fix logic
    
    * feat: fix query
    
    * feat: added logs
    
    * feat: cleanup
    
    * feat: added job id logic
    
    * feat: updated batch logic
    
    * feat: added fill event support
    
    * feat: added more events
    
    * feat: fix for cancel, cleanup payload
    
    * feat: fix config
    
    * feat: fix monitor
    
    * feat: inc limit
    
    * feat: removed log
    
    * feat: added lock
    
    * feat: release lock
    
    * feat: added sepolia protected zone
    
    * feat: disable job id
    
    * feat: disable job trigger
    
    * feat: added log
    
    * feat: remove logs
    
    * feat: add delay
    
    * feat: updated logs
    
    * feat: updated log
    
    * feat: fix network, added log
    
    * feat: fix network file
    
    * feat: fixed base network
    
    * feat: disable linea delayed jobs
    
    * feat: fix chain id
    
    * feat: fixed format
    
    * feat: fixed optimsm
    
    * feat: updated logic
    
    * feat: removed logs
    
    * feat: removed logs

--- packages/indexer/src/elasticsearch/indexes/activities/index.ts
+++ packages/indexer/src/elasticsearch/indexes/activities/index.ts
@@ -40,16 +40,6 @@ export const save = async (activities: ActivityDocument[], upsert = true): Promi
             response,
           })
         );
-      } else {
-        logger.debug(
-          "elasticsearch-activities",
-          JSON.stringify({
-            message: "save activities conflicts",
-            topic: "save",
-            upsert,
-            response,
-          })
-        );
       }
     }
   } catch (error) {
@@ -640,17 +630,6 @@ export const initIndex = async (): Promise<void> => {
     // @ts-ignore
     const indexConfig = CONFIG[indexConfigName];
 
-    logger.info(
-      "elasticsearch-activities",
-      JSON.stringify({
-        topic: "initIndex",
-        message: "Start.",
-        indexName: INDEX_NAME,
-        indexConfig,
-        settings: getNetworkSettings().elasticsearch?.indexes?.activities,
-      })
-    );
-
     if (await elasticsearch.indices.exists({ index: INDEX_NAME })) {
       logger.info(
         "elasticsearch-activities",
@@ -659,6 +638,7 @@ export const initIndex = async (): Promise<void> => {
           message: "Index already exists.",
           indexName: INDEX_NAME,
           indexConfig,
+          indexSettings: getNetworkSettings().elasticsearch?.indexes?.activities,
         })
       );
 
@@ -670,6 +650,7 @@ export const initIndex = async (): Promise<void> => {
             message: "Mappings update disabled.",
             indexName: INDEX_NAME,
             indexConfig,
+            indexSettings: getNetworkSettings().elasticsearch?.indexes?.activities,
           })
         );
 
@@ -692,6 +673,7 @@ export const initIndex = async (): Promise<void> => {
           message: "Updated mappings.",
           indexName: INDEX_NAME,
           indexConfig,
+          indexSettings: getNetworkSettings().elasticsearch?.indexes?.activities,
           putMappingResponse,
         })
       );
@@ -703,6 +685,7 @@ export const initIndex = async (): Promise<void> => {
           message: "Creating Index.",
           indexName: INDEX_NAME,
           indexConfig,
+          indexSettings: getNetworkSettings().elasticsearch?.indexes?.activities,
         })
       );
 
@@ -723,6 +706,7 @@ export const initIndex = async (): Promise<void> => {
           message: "Index Created!",
           indexName: INDEX_NAME,
           indexConfig,
+          indexSettings: getNetworkSettings().elasticsearch?.indexes?.activities,
           params,
           createIndexResponse,
         })
@@ -737,6 +721,7 @@ export const initIndex = async (): Promise<void> => {
         topic: "initIndex",
         message: "Error.",
         indexName: INDEX_NAME,
+        indexSettings: getNetworkSettings().elasticsearch?.indexes?.activities,
         error,
       })
     );
@@ -827,21 +812,21 @@ export const updateActivitiesMissingCollection = async (
       } else {
         keepGoing = pendingUpdateActivities.length === 1000;
 
-        logger.info(
-          "elasticsearch-activities",
-          JSON.stringify({
-            topic: "updateActivitiesMissingCollection",
-            message: `Success`,
-            data: {
-              contract,
-              tokenId,
-              collection,
-            },
-            bulkParams,
-            response,
-            keepGoing,
-          })
-        );
+        // logger.info(
+        //   "elasticsearch-activities",
+        //   JSON.stringify({
+        //     topic: "updateActivitiesMissingCollection",
+        //     message: `Success`,
+        //     data: {
+        //       contract,
+        //       tokenId,
+        //       collection,
+        //     },
+        //     bulkParams,
+        //     response,
+        //     keepGoing,
+        //   })
+        // );
       }
     }
   } catch (error) {
@@ -963,22 +948,22 @@ export const updateActivitiesCollection = async (
       } else {
         keepGoing = pendingUpdateActivities.length === 1000;
 
-        logger.info(
-          "elasticsearch-activities",
-          JSON.stringify({
-            topic: "updateActivitiesCollection",
-            message: `Success`,
-            data: {
-              contract,
-              tokenId,
-              newCollection,
-              oldCollectionId,
-            },
-            bulkParams,
-            response,
-            keepGoing,
-          })
-        );
+        // logger.info(
+        //   "elasticsearch-activities",
+        //   JSON.stringify({
+        //     topic: "updateActivitiesCollection",
+        //     message: `Success`,
+        //     data: {
+        //       contract,
+        //       tokenId,
+        //       newCollection,
+        //       oldCollectionId,
+        //     },
+        //     bulkParams,
+        //     response,
+        //     keepGoing,
+        //   })
+        // );
       }
     }
   } catch (error) {
@@ -1174,21 +1159,21 @@ export const updateActivitiesTokenMetadata = async (
       } else {
         keepGoing = pendingUpdateActivities.length === 1000;
 
-        logger.info(
-          "elasticsearch-activities",
-          JSON.stringify({
-            topic: "updateActivitiesTokenMetadata",
-            message: `Success`,
-            data: {
-              contract,
-              tokenId,
-              tokenData,
-            },
-            bulkParams,
-            response,
-            keepGoing,
-          })
-        );
+        // logger.info(
+        //   "elasticsearch-activities",
+        //   JSON.stringify({
+        //     topic: "updateActivitiesTokenMetadata",
+        //     message: `Success`,
+        //     data: {
+        //       contract,
+        //       tokenId,
+        //       tokenData,
+        //     },
+        //     bulkParams,
+        //     response,
+        //     keepGoing,
+        //   })
+        // );
       }
     }
   } catch (error) {
@@ -1351,20 +1336,20 @@ export const updateActivitiesCollectionMetadata = async (
       } else {
         keepGoing = pendingUpdateActivities.length === 1000;
 
-        logger.info(
-          "elasticsearch-activities",
-          JSON.stringify({
-            topic: "updateActivitiesCollectionMetadata",
-            message: `Success`,
-            data: {
-              collectionId,
-              collectionData,
-            },
-            bulkParams,
-            response,
-            keepGoing,
-          })
-        );
+        // logger.info(
+        //   "elasticsearch-activities",
+        //   JSON.stringify({
+        //     topic: "updateActivitiesCollectionMetadata",
+        //     message: `Success`,
+        //     data: {
+        //       collectionId,
+        //       collectionData,
+        //     },
+        //     bulkParams,
+        //     response,
+        //     keepGoing,
+        //   })
+        // );
       }
     }
   } catch (error) {
@@ -1461,19 +1446,19 @@ export const deleteActivitiesByBlockHash = async (blockHash: string): Promise<bo
       } else {
         keepGoing = pendingDeleteActivities.length === 1000;
 
-        logger.info(
-          "elasticsearch-activities",
-          JSON.stringify({
-            topic: "deleteActivitiesByBlockHash",
-            message: `Success`,
-            data: {
-              blockHash,
-            },
-            bulkParams,
-            response,
-            keepGoing,
-          })
-        );
+        // logger.info(
+        //   "elasticsearch-activities",
+        //   JSON.stringify({
+        //     topic: "deleteActivitiesByBlockHash",
+        //     message: `Success`,
+        //     data: {
+        //       blockHash,
+        //     },
+        //     bulkParams,
+        //     response,
+        //     keepGoing,
+        //   })
+        // );
       }
     }
   } catch (error) {
