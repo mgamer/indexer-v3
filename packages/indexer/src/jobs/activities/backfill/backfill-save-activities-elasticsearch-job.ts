c3f92e611ab7fa5e3003fa3cf446bc5554677ef4
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-09-01 13:16:59 -0400

    Feat/bridge to base (#6173)
    * feat: inc concurrency
    
    * feat: add bulk logic
    
    * feat: fix add to queue
    
    * feat: added log
    
    * feat: added log
    
    * feat: disable logic
    
    * feat: fix logic
    
    * feat: fix query
    
    * feat: disable logic
    
    * feat: fix logic
    
    * feat: fix query
    
    * feat: added logs
    
    * feat: cleanup
    
    * feat: added job id logic
    
    * feat: updated batch logic
    
    * feat: added fill event support
    
    * feat: added more events
    
    * feat: fix for cancel, cleanup payload
    
    * feat: fix config
    
    * feat: fix monitor
    
    * feat: inc limit
    
    * feat: removed log
    
    * feat: added lock
    
    * feat: release lock
    
    * feat: added sepolia protected zone
    
    * feat: disable job id
    
    * feat: disable job trigger
    
    * feat: added log
    
    * feat: remove logs
    
    * feat: add delay
    
    * feat: updated logs
    
    * feat: updated log
    
    * feat: fix network, added log
    
    * feat: fix network file
    
    * feat: fixed base network
    
    * feat: disable linea delayed jobs
    
    * feat: fix chain id
    
    * feat: fixed format
    
    * feat: fixed optimsm
    
    * feat: updated logic
    
    * feat: removed logs
    
    * feat: removed logs
    
    * feat: updated log

--- packages/indexer/src/jobs/activities/backfill/backfill-save-activities-elasticsearch-job.ts
+++ packages/indexer/src/jobs/activities/backfill/backfill-save-activities-elasticsearch-job.ts
@@ -20,6 +20,7 @@ import { fromBuffer, toBuffer } from "@/common/utils";
 import { NftTransferEventCreatedEventHandler } from "@/elasticsearch/indexes/activities/event-handlers/nft-transfer-event-created";
 import { acquireLock, doesLockExist, redis, releaseLock } from "@/common/redis";
 import crypto from "crypto";
+import { ActivityDocument } from "@/elasticsearch/indexes/activities/base";
 
 export type BackfillSaveActivitiesElasticsearchJobPayload = {
   type: "ask" | "ask-cancel" | "bid" | "bid-cancel" | "sale" | "transfer";
@@ -97,6 +98,14 @@ export class BackfillSaveActivitiesElasticsearchJob extends AbstractRabbitMqJobH
             );
           }
 
+          let errorActivities: ActivityDocument[] = [];
+
+          if (bulkResponse.errors) {
+            const errorItems = bulkResponse.items.filter((item) => item.index?.error);
+            const errorItemsIds = errorItems.map((item) => item.index?._id);
+            errorActivities = activities.filter((activity) => errorItemsIds.includes(activity.id));
+          }
+
           logger.info(
             this.queueName,
             JSON.stringify({
@@ -114,7 +123,8 @@ export class BackfillSaveActivitiesElasticsearchJob extends AbstractRabbitMqJobH
               nextCursor,
               hasNextCursor: !!nextCursor,
               hasErrors: bulkResponse.errors,
-              errors: bulkResponse.items.filter((item) => item.index?.error),
+              errorItems: bulkResponse.items.filter((item) => item.index?.error),
+              errorActivities,
             })
           );
         } else if (keepGoing) {
