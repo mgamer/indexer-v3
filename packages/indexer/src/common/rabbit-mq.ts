ef33784e3e54567d825e07496cdc342e8647ddf4
Idan Peleg <ipeleg@hotmail.com>
2023-09-05 14:24:49 -0400

    feat: added log

--- packages/indexer/src/common/rabbit-mq.ts
+++ packages/indexer/src/common/rabbit-mq.ts
@@ -112,6 +112,10 @@ export class RabbitMq {
       );
     }
 
+    if (config.chainId === 137 && queueName === "events-sync-realtime") {
+      logger.info("events-sync-realtime", `publish realtime sync ${JSON.stringify(content)}`);
+    }
+
     try {
       const channelIndex = _.random(0, RabbitMq.maxPublisherChannelsCount - 1);
 
