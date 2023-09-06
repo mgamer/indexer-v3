c873a9f20eea595340dbbd1edce5adca7e3f69dd
d3or <deorrunner@gmail.com>
2023-09-06 14:31:52 -0400

    feat: add more logs

--- packages/indexer/src/jobs/token-updates/mint-queue-job.ts
+++ packages/indexer/src/jobs/token-updates/mint-queue-job.ts
@@ -61,6 +61,11 @@ export class MintQueueJob extends AbstractRabbitMqJobHandler {
       // check if there are any tokens that exist already for the collection
       // if there are not, we need to fetch the collection metadata from upstream
       if (collection) {
+        logger.info(
+          this.queueName,
+          `Minted token ${contract}:${tokenId} for collection ${collection.id}`
+        );
+
         const existingToken = await idb.oneOrNone(
           `
             SELECT 1 FROM tokens
