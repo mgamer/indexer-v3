333cdf64637d4406902d6ace19a7dd2081aa1c8e
Idan Peleg <ipeleg@hotmail.com>
2023-09-07 10:36:31 -0400

    feat: update batch size

--- packages/indexer/src/sync/events/storage/nft-transfer-events.ts
+++ packages/indexer/src/sync/events/storage/nft-transfer-events.ts
@@ -281,7 +281,7 @@ async function insertQueries(queries: string[], backfill: boolean) {
     // on the events to have been written to the database at the time
     // they get to run and we have no way to easily enforce this when
     // using the write buffer.
-    for (const query of _.chunk(queries, 650)) {
+    for (const query of _.chunk(queries, [80001, 84531].includes(config.chainId) ? 250 : 650)) {
       promises.push(idb.none(pgp.helpers.concat(query)));
     }
 
