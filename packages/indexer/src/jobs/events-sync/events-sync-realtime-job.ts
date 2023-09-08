1c639bc0d86f83d33e57d925196b5aa70ffb72d9
Idan Peleg <ipeleg@hotmail.com>
2023-09-08 19:58:06 -0400

    feat: update concurrency

--- packages/indexer/src/jobs/events-sync/events-sync-realtime-job.ts
+++ packages/indexer/src/jobs/events-sync/events-sync-realtime-job.ts
@@ -11,7 +11,7 @@ export type EventsSyncRealtimeJobPayload = {
 export class EventsSyncRealtimeJob extends AbstractRabbitMqJobHandler {
   queueName = "events-sync-realtime";
   maxRetries = 30;
-  concurrency = [137, 84531, 80001, 11155111].includes(config.chainId) ? 1 : 5;
+  concurrency = [84531, 80001, 11155111].includes(config.chainId) ? 1 : 5;
   timeout = 10 * 60 * 1000;
   backoff = {
     type: "fixed",
