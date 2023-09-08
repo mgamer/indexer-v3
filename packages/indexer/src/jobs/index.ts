fb38b199562fe8cb5e504d79842d7bd8d7fccf70
Idan Peleg <ipeleg@hotmail.com>
2023-09-08 13:47:24 -0400

    feat: remove log

--- packages/indexer/src/jobs/index.ts
+++ packages/indexer/src/jobs/index.ts
@@ -25,6 +25,7 @@ import amqplibConnectionManager, {
   ChannelWrapper,
 } from "amqp-connection-manager";
 
+import * as backfillWrongNftBalances from "@/jobs/backfill/backfill-wrong-nft-balances";
 import * as backfillExpiredOrders from "@/jobs/backfill/backfill-expired-orders";
 import * as backfillExpiredOrders2 from "@/jobs/backfill/backfill-expired-orders-2";
 import * as backfillRefreshCollectionMetadata from "@/jobs/backfill/backfill-refresh-collections-metadata";
@@ -151,6 +152,7 @@ import { blockGapCheckJob } from "@/jobs/events-sync/block-gap-check";
 import { backfillTokensTimeToMetadataJob } from "@/jobs/backfill/backfill-tokens-time-to-metadata-job";
 
 export const allJobQueues = [
+  backfillWrongNftBalances.queue,
   backfillExpiredOrders.queue,
   backfillExpiredOrders2.queue,
   backfillRefreshCollectionMetadata.queue,
