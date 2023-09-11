82f6d57b142a4e7e6b62a7163e3f3a3ddb4fd3ce
d3or <deorrunner@gmail.com>
2023-09-11 14:59:15 -0400

    feat: fix migraiton

--- packages/indexer/src/migrations/1693540314231_contracts_core_metadata.sql
+++ packages/indexer/src/migrations/1693540314231_contracts_core_metadata.sql
@@ -2,7 +2,7 @@
 
 ALTER TABLE "contracts" ADD COLUMN "symbol" TEXT;
 ALTER TABLE "contracts" ADD COLUMN "name" TEXT;
-ALTER TABLE "contracts" ADD COLUMN "deployed_at" TIMESTAMPTZ
+ALTER TABLE "contracts" ADD COLUMN "deployed_at" TIMESTAMPTZ;
 ALTER TABLE "contracts" ADD COLUMN "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW();
 
 -- Down Migration
