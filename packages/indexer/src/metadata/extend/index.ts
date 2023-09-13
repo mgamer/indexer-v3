cb80c6d7c4e41c730847beaf409318214c868dc1
Zachary Salmon <zak@usefirstmate.xyz>
2023-09-13 17:15:53 -0400

    index proof shared contract that has already been split by opensea (#6289)

--- packages/indexer/src/metadata/extend/index.ts
+++ packages/indexer/src/metadata/extend/index.ts
@@ -104,6 +104,7 @@ extendCollection["1,0x7a15b36cb834aea88553de69077d3777460d73ac"] = ExtendLogicCl
 extendCollection["1,0x68d0f6d1d99bb830e17ffaa8adb5bbed9d6eec2e"] = ExtendLogicClasses.opensea;
 extendCollection["1,0x33eecbf908478c10614626a9d304bfe18b78dd73"] = ExtendLogicClasses.opensea;
 extendCollection["1,0x495f947276749ce646f68ac8c248420045cb7b5e"] = ExtendLogicClasses.opensea;
+extendCollection["1,0x48b17a2c46007471b3eb72d16268eaecdd1502b7"] = ExtendLogicClasses.opensea;
 
 // Courtyard
 extendCollection["1,0xd4ac3ce8e1e14cd60666d49ac34ff2d2937cf6fa"] = ExtendLogicClasses.courtyard;
@@ -195,6 +196,8 @@ extend["1,0x7a15b36cb834aea88553de69077d3777460d73ac"] = ExtendLogicClasses.open
 extend["1,0x68d0f6d1d99bb830e17ffaa8adb5bbed9d6eec2e"] = ExtendLogicClasses.opensea;
 extend["1,0x33eecbf908478c10614626a9d304bfe18b78dd73"] = ExtendLogicClasses.opensea;
 extend["1,0x495f947276749ce646f68ac8c248420045cb7b5e"] = ExtendLogicClasses.opensea;
+extend["1,0x48b17a2c46007471b3eb72d16268eaecdd1502b7"] = ExtendLogicClasses.opensea;
+
 
 // Courtyard
 extend["1,0xd4ac3ce8e1e14cd60666d49ac34ff2d2937cf6fa"] = courtyard;
