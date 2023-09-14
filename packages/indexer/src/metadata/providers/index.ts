0e382cdd99214bd8e90aeef98e01b71a2bdcc871
d3or <deorrunner@gmail.com>
2023-09-14 12:16:29 -0400

    feat: remove centerdev

--- packages/indexer/src/metadata/providers/index.ts
+++ packages/indexer/src/metadata/providers/index.ts
@@ -1,5 +1,4 @@
 import _ from "lodash";
-import { CenterdevMetadataProvider } from "./centerdev-metadata-provicer";
 import { OnchainMetadataProvider } from "./onchain-metadata-provicer";
 import { RaribleMetadataProvider } from "./rarible-metadata-provider";
 import { SimplehashMetadataProvider } from "./simplehash-metadata-provider";
@@ -7,7 +6,6 @@ import { SoundxyzMetadataProvider } from "./soundxyz-metadata-provider";
 
 export const MetadataProviders = [
   new RaribleMetadataProvider(),
-  new CenterdevMetadataProvider(),
   new OnchainMetadataProvider(),
   new OnchainMetadataProvider(),
   new SimplehashMetadataProvider(),
