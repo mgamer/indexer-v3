03e5c1f613972e741ff40d00842582ca2875bafe
Idan Peleg <ipeleg@hotmail.com>
2023-09-19 16:33:42 -0400

    feat: update redis only if blocked synced

--- packages/indexer/src/sync/events/syncEventsV2.ts
+++ packages/indexer/src/sync/events/syncEventsV2.ts
@@ -471,13 +471,14 @@ export const checkForMissingBlocks = async (block: number) => {
   if (latestBlock) {
     const latestBlockNumber = Number(latestBlock);
     if (block > latestBlockNumber) {
-      await redis.set("latest-block-realtime", block);
       if (block - latestBlockNumber > 1) {
         // if we are missing more than 1 block, we need to sync the missing blocks
         for (let i = latestBlockNumber + 1; i < block; i++) {
           logger.info("sync-events-realtime", `Found missing block: ${i}`);
           await eventsSyncRealtimeJob.addToQueue({ block: i });
         }
+
+        await redis.set("latest-block-realtime", block);
       }
     }
   } else {
