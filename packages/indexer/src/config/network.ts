cfc2aabee0c0e48607b997ca1ce972548c7eb19a
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-09-22 15:58:20 -0400

    feat: added ancient8-testnet (#6415)

--- packages/indexer/src/config/network.ts
+++ packages/indexer/src/config/network.ts
@@ -72,6 +72,9 @@ export const getNetworkName = () => {
     case 1101:
       return "polygon-zkevm";
 
+    case 2863311531:
+      return "ancient8-testnet";
+
     default:
       return "unknown";
   }
@@ -1241,6 +1244,38 @@ export const getNetworkSettings = (): NetworkSettings => {
                   'MATIC',
                   18,
                   '{"coingeckoCurrencyId": "matic-network"}'
+                ) ON CONFLICT DO NOTHING
+              `
+            ),
+          ]);
+        },
+      };
+    }
+    // Polygon zkEVM
+    case 2863311531: {
+      return {
+        ...defaultNetworkSettings,
+        enableWebSocket: false,
+        realtimeSyncMaxBlockLag: 32,
+        realtimeSyncFrequencySeconds: 5,
+        lastBlockLatency: 5,
+        onStartup: async () => {
+          // Insert the native currency
+          await Promise.all([
+            idb.none(
+              `
+                INSERT INTO currencies (
+                  contract,
+                  name,
+                  symbol,
+                  decimals,
+                  metadata
+                ) VALUES (
+                  '\\x0000000000000000000000000000000000000000',
+                  'Ether',
+                  'ETH',
+                  18,
+                  '{"coingeckoCurrencyId": "ethereum", "image": "https://assets.coingecko.com/coins/images/279/large/ethereum.png"}'
                 ) ON CONFLICT DO NOTHING
               `
             ),
