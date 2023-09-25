433b55337029b8603ebb62d20811716465fd8258
Michael Morrissey <mikehmorrissey@gmail.com>
2023-09-25 09:26:37 -0400

    feat: add new trending collections endpoint

--- packages/indexer/src/api/endpoints/collections/get-top-selling-collections/v2.ts
+++ packages/indexer/src/api/endpoints/collections/get-top-selling-collections/v2.ts
@@ -4,7 +4,7 @@ import { Request, RouteOptions } from "@hapi/hapi";
 import _ from "lodash";
 import Joi from "joi";
 import { logger } from "@/common/logger";
-import { fromBuffer, regex, toBuffer } from "@/common/utils";
+import { fromBuffer, regex } from "@/common/utils";
 import { redb } from "@/common/db";
 import * as Sdk from "@reservoir0x/sdk";
 import { config } from "@/config/index";
@@ -24,11 +24,12 @@ const version = "v2";
 
 export const getTopSellingCollectionsV2Options: RouteOptions = {
   cache: {
+    expiresIn: 60 * 1000,
     privacy: "public",
   },
   description: "Top Selling Collections",
   notes: "Get top selling and minting collections",
-  tags: ["api", "Collections"],
+  tags: ["api", "x-deprecated"],
   plugins: {
     "hapi-swagger": {
       order: 3,
@@ -213,13 +214,14 @@ export const getTopSellingCollectionsV2Options: RouteOptions = {
       let collections = [];
 
       if (collectionsResult.length) {
-        request.query.contract = collectionsResult.map((collection: any) =>
-          toBuffer(collection.primaryContract)
-        );
+        const collectionIdList = collectionsResult
+          .map((collection: any) => `'${collection.id}'`)
+          .join(", ");
 
         const baseQuery = `
         SELECT
           collections.id,
+          collections.name,
           collections.contract,
           collections.token_count,
           collections.owner_count,
@@ -227,13 +229,14 @@ export const getTopSellingCollectionsV2Options: RouteOptions = {
           collections.day7_volume_change,
           collections.day30_volume_change,
           (collections.metadata ->> 'bannerImageUrl')::TEXT AS "banner",
+          (collections.metadata ->> 'imageUrl')::TEXT AS "image",
           (collections.metadata ->> 'description')::TEXT AS "description",
           ${floorAskSelectQuery}
           FROM collections
-          WHERE collections.contract IN ($/contract:csv/)
+          WHERE collections.id IN (${collectionIdList})
       `;
 
-        const resultsPromise = redb.manyOrNone(baseQuery, request.query);
+        const resultsPromise = redb.manyOrNone(baseQuery);
         const recentSalesPromise = collectionsResult.map(async (collection: any) => {
           return {
             ...collection,
@@ -270,14 +273,14 @@ export const getTopSellingCollectionsV2Options: RouteOptions = {
         const collectionsMetadata: Record<string, any> = {};
         if (collectionMetadataResponse && Array.isArray(collectionMetadataResponse)) {
           collectionMetadataResponse.forEach((metadata: any) => {
-            collectionsMetadata[fromBuffer(metadata.contract)] = metadata;
+            collectionsMetadata[metadata.id] = metadata;
           });
         }
         const sources = await Sources.getInstance();
 
         collections = await Promise.all(
           responses.map(async (response: any) => {
-            const metadata = collectionsMetadata[(response as any).primaryContract] || {};
+            const metadata = collectionsMetadata[(response as any).id] || {};
             let floorAsk;
             if (metadata) {
               const floorAskCurrency = metadata.floor_sell_currency
@@ -307,8 +310,10 @@ export const getTopSellingCollectionsV2Options: RouteOptions = {
                 "7day": metadata.day7_volume_change,
                 "30day": metadata.day30_volume_change,
               },
+              name: metadata.name,
               tokenCount: Number(metadata.token_count || 0),
               ownerCount: Number(metadata.owner_count || 0),
+              image: metadata.image,
               banner: metadata.banner,
               description: metadata.description,
               floorAsk,
