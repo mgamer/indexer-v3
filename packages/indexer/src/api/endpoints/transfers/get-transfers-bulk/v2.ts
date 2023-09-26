2fc5f6a4b4a0db57b2187803a43affa355cb1985
Idan Peleg <ipeleg@hotmail.com>
2023-09-26 15:56:07 -0400

    feat: fix filtering by contract

--- packages/indexer/src/api/endpoints/transfers/get-transfers-bulk/v2.ts
+++ packages/indexer/src/api/endpoints/transfers/get-transfers-bulk/v2.ts
@@ -252,7 +252,7 @@ export const getTransfersBulkV2Options: RouteOptions = {
           ORDER BY
             nft_transfer_events.updated_at ${query.sortDirection},
             nft_transfer_events.address ${query.sortDirection},
-            ${query.token ? `nft_transfer_events.token_id ${query.sortDirection},` : ""}
+            nft_transfer_events.token_id ${query.sortDirection},
             nft_transfer_events.tx_hash ${query.sortDirection},
             nft_transfer_events.log_index ${query.sortDirection},
             nft_transfer_events.batch_index ${query.sortDirection}
