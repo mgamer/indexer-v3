fa46396e3950a28b15a2a92d90f6b40510e5cb61
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-09-27 14:47:55 -0400

    Ofir/platf 2246 investigate too many recalculations of floor ask for a (#6485)
    * feat: updated log, added polygon
    
    * feat: updated logs
    
    * feat: updated logs
    
    * feat: added log
    
    * feat: added job
    
    * feat: removed log
    
    * feat: cleanup
    
    * feat: added log
    
    * feat: fixed logic
    
    * feat: fixed logic
    
    * feat: fix logic
    
    * feat: fix logic
    
    * feat: fix logic
    
    * feat: set delay
    
    * feat: added log

--- packages/indexer/src/jobs/metadata-index/metadata-process-job.ts
+++ packages/indexer/src/jobs/metadata-index/metadata-process-job.ts
@@ -112,9 +112,13 @@ export class MetadataIndexProcessJob extends AbstractRabbitMqJobHandler {
   }
 
   public async onCompleted(rabbitMqMessage: RabbitMQMessage, processResult: undefined | number) {
+    logger.info(this.queueName, `Debug.processResult=${processResult}`);
+
     if (processResult) {
       const { method } = rabbitMqMessage.payload;
       await this.addToQueue({ method }, processResult * 1000);
+
+      logger.info(this.queueName, `Debug.processResult=${processResult}, method=${method}`);
     }
   }
 
