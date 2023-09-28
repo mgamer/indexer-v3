6c9926e7e9ca4853fa06e94433f51140dacf1a16
George Roman <george.roman.99@gmail.com>
2023-09-28 14:54:31 -0400

    feat: adjust erc721 test contract

--- packages/contracts/scripts/setup/trigger.ts
+++ packages/contracts/scripts/setup/trigger.ts
@@ -364,12 +364,16 @@ export const trigger = {
         DEPLOYER,
         "https://test-tokens-metadata.vercel.app/api/erc721/",
         "https://test-tokens-metadata.vercel.app/api/erc721/contract",
+        DEPLOYER,
+        1000,
       ]),
     Erc1155: async () =>
       dv("ReservoirErc1155", "v1", [
         DEPLOYER,
         "https://test-tokens-metadata.vercel.app/api/erc1155/",
         "https://test-tokens-metadata.vercel.app/api/erc1155/contract",
+        DEPLOYER,
+        1000,
       ]),
   },
 };
