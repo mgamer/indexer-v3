c19cc28c9d78c7da0fb011b3dd5222313c3a71b8
Idan Peleg <ipeleg@hotmail.com>
2023-09-30 14:08:25 -0400

    feat: check for audio url

--- packages/indexer/src/metadata/providers/simplehash-metadata-provider.ts
+++ packages/indexer/src/metadata/providers/simplehash-metadata-provider.ts
@@ -118,7 +118,7 @@ export class SimplehashMetadataProvider extends AbstractBaseMetadataProvider {
       animationOriginalUrl: animation_original_url,
       metadataOriginalUrl: metadata_original_url,
       imageProperties: metadata.image_properties,
-      mediaUrl: metadata.video_url,
+      mediaUrl: metadata.video_url ?? metadata.audio_url,
       attributes: (attributes || []).map((trait: any) => ({
         key: trait.trait_type ?? "property",
         value: trait.value,
