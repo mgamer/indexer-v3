938285baa81d7c5958ae4bfebc9fba096e6d7afd
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-10-02 19:16:47 -0400

    feat: activities fix (#6512)

--- packages/indexer/src/elasticsearch/indexes/base/index.ts
+++ packages/indexer/src/elasticsearch/indexes/base/index.ts
@@ -1,5 +1,6 @@
 import { config } from "@/config/index";
 import { getNetworkName } from "@/config/network";
+import { Network } from "@reservoir0x/sdk/dist/utils";
 
 export interface BaseDocument {
   chain: {
@@ -19,7 +20,7 @@ export abstract class DocumentBuilder {
   public buildDocument(data: BuildDocumentData): BaseDocument {
     return {
       chain: {
-        id: config.chainId,
+        id: config.chainId === Network.Ancient8Testnet ? 0 : config.chainId,
         name: getNetworkName(),
       },
       id: data.id,
