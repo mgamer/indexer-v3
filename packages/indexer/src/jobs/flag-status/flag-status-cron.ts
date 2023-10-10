7f5ff213a54e6027570767ec05228b3b33a111f2
d3or <deorrunner@gmail.com>
2023-10-10 14:44:42 -0400

    feat: dont check rate limit in cron

--- packages/indexer/src/jobs/flag-status/flag-status-cron.ts
+++ packages/indexer/src/jobs/flag-status/flag-status-cron.ts
@@ -1,5 +1,5 @@
 import cron from "node-cron";
-import { doesLockExist, redlock } from "@/common/redis";
+import { redlock } from "@/common/redis";
 import { config } from "@/config/index";
 import { tokenFlagStatusSyncJob } from "@/jobs/flag-status/token-flag-status-sync-job";
 import { collectionFlagStatusSyncJob } from "@/jobs/flag-status/collection-flag-status-sync-job";
@@ -15,16 +15,12 @@ if (config.doBackgroundWork && !config.disableFlagStatusRefreshJob) {
         .acquire(["flag-status-sync-cron"], (10 * 60 - 3) * 1000)
         .then(async () => {
           // check if a lock exists for tokens due to rate limiting
-          if (!(await doesLockExist(tokenFlagStatusSyncJob.getLockName()))) {
-            const tokensCount = await PendingFlagStatusSyncTokens.count();
-            if (tokensCount > 0) await tokenFlagStatusSyncJob.addToQueue();
-          }
+          const tokensCount = await PendingFlagStatusSyncTokens.count();
+          if (tokensCount > 0) await tokenFlagStatusSyncJob.addToQueue();
 
           // check if a lock exists for collections due to rate limiting
-          if (!(await doesLockExist(collectionFlagStatusSyncJob.getLockName()))) {
-            const collectionsCount = await PendingFlagStatusSyncCollections.count();
-            if (collectionsCount > 0) await collectionFlagStatusSyncJob.addToQueue();
-          }
+          const collectionsCount = await PendingFlagStatusSyncCollections.count();
+          if (collectionsCount > 0) await collectionFlagStatusSyncJob.addToQueue();
         })
         .catch(() => {
           // Skip any errors
