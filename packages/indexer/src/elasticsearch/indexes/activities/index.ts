48db9555c6e57e6f71557418a7810572410521f8
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2023-10-12 16:38:05 -0400

    Ofir/platf 1581 investigate missing activities when querying by collection (#6606)
    * feat: add debug
    
    * feat: fix syntax
    
    * feat: removed debug, updated job settings
    
    * feat: revert flag
    
    * feat: revert flag
    
    * feat: disable activity endpoints
    
    * feat: pagination fix
    
    * feat: disable job for non opensea chains
    
    * feat: fix bc logic

--- packages/indexer/src/elasticsearch/indexes/activities/index.ts
+++ packages/indexer/src/elasticsearch/indexes/activities/index.ts
@@ -499,7 +499,7 @@ export const search = async (
   }
 
   // Backward compatibility
-  if (searchAfter?.length != 1) {
+  if (searchAfter?.length != 1 && !params.continuationAsInt) {
     esSort.push({ id: { order: "desc" } });
   }
 
