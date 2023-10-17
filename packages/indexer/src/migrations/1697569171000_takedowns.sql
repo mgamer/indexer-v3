e7ef0207d38878ce7a4b04bd1901fce94aff0c09
tv <tv3636@gmail.com>
2023-10-17 12:22:23 -0700

    feat: create takedowns table

---
+++ packages/indexer/src/migrations/1697569171000_takedowns.sql
@@ -0,0 +1,25 @@
+-- Up Migration
+
+CREATE TYPE "takedown_type_t" AS ENUM (
+  'collection',
+  'token'
+);
+
+CREATE TABLE "takedowns" (  
+  "id" TEXT NOT NULL,
+  "type" "takedown_type_t" NOT NULL,  
+  "api_key" TEXT NOT NULL,
+  "created_at" TIMESTAMPTZ DEFAULT now(),
+  "updated_at" TIMESTAMPTZ DEFAULT now(),
+  "active" BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+ALTER TABLE "takedowns"
+  ADD CONSTRAINT "takedowns_pk"
+  PRIMARY KEY ("id", "type", "api_key");
+
+-- Down Migration
+
+DROP TABLE "takedowns";
+
+DROP TYPE "takedown_type_t";
