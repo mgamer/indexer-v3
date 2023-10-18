8063fe961c8aec0385faed785a588820657a05c8
Peter Watts <pwattsmail@gmail.com>
2023-10-18 19:59:44 -0400

    Update payment processor goerli address

--- packages/sdk/src/payment-processor/addresses.ts
+++ packages/sdk/src/payment-processor/addresses.ts
@@ -2,7 +2,7 @@ import { ChainIdToAddress, Network } from "../utils";
 
 export const Exchange: ChainIdToAddress = {
   [Network.Ethereum]: "0x009a1dc629242961c9e4f089b437afd394474cc0",
-  [Network.EthereumGoerli]: "0x009a1d88379a604664006fff6c32877bff6723bd",
+  [Network.EthereumGoerli]: "0x76bcffa7401ac8d21568c947070242d3473b3dd3",
   [Network.EthereumSepolia]: "0x009a1d8de8d80fcd9c6aaafe97a237dc663f2978",
   [Network.Polygon]: "0x009a1daf6b5b8adc6b3db720c58d43caa2fdac56",
   [Network.Mumbai]: "0x009a1d6bdf55b25ca21436ed30968c1fb7648244",
