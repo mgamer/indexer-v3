06b84f7c8466ecbe4ac3a94afb4afbaaa3f91580
lljxx1 <lljxx1@gmail.com>
2023-10-24 07:21:07 +0800

    feat: add tests

--- packages/indexer/src/tests/mints/detector.test.ts
+++ packages/indexer/src/tests/mints/detector.test.ts
@@ -12,8 +12,13 @@ describe("Mints - Detector", () => {
     if (config.chainId != 8453) {
       return;
     }
-    const txId = "0x13dcd467192096bbd652d934dd0d7a40581bc6d39d38d9e8a874e7a77151d732";
-    const mints = await detector.extractByTx(txId);
-    expect(mints.length).not.toBe(0);
+    const txIds = [
+      "0xa1e59fc5cbb627d981b356a017cdb53cfc40549ef68b562068fe792fd0d89c37",
+      "0x13dcd467192096bbd652d934dd0d7a40581bc6d39d38d9e8a874e7a77151d732",
+    ];
+    for (const txId of txIds) {
+      const mints = await detector.extractByTx(txId, true);
+      expect(mints.length).not.toBe(0);
+    }
   });
 });
