97ee829501a1c0400b2eb1720bc6857a45b4d63b
George Roman <george.roman.99@gmail.com>
2023-11-02 11:06:32 +0100

    feat: support more chains for cross-chain solving

--- packages/sdk/src/cross-chain/addresses.ts
+++ packages/sdk/src/cross-chain/addresses.ts
@@ -6,4 +6,6 @@ export const Exchange: ChainIdToAddress = {
   [Network.Zora]: "0x96fefdb0a543b3e30d8aaae865b7a48378d85382",
   [Network.EthereumGoerli]: "0x6afc65c964267bd4c10a488df5810d32a6c35b6e",
   [Network.EthereumSepolia]: "0x66f9085c4c751e35d473b51bb783579e9b5c8095",
+  [Network.ZoraTestnet]: "0x97292e65ab745747971c4b4e992ffc5a51186c6a",
+  [Network.BaseGoerli]: "0xb1096516fc33bb64a77158b10f155846e74bd7fa",
 };
