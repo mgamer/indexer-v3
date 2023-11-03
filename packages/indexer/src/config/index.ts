2d5d002892c3204662c8b636d97612be7498d3da
FIlipe <filipe@unevenlabs.com>
2023-11-03 14:28:38 +0100

    Update index.ts

--- packages/indexer/src/config/index.ts
+++ packages/indexer/src/config/index.ts
@@ -19,7 +19,7 @@ export const config = {
   openseaIndexerApiBaseUrl: String(process.env.OPENSEA_INDEXER_API_BASE_URL),
 
   // When running in liquidity-only mode, all metadata processes are disabled
-  liquidityOnly: true,
+  liquidityOnly: Boolean(Number(process.env.LIQUIDITY_ONLY)),
   metadataIndexingMethod: String(process.env.METADATA_INDEXING_METHOD || "opensea"),
   metadataIndexingMethodCollection: String(
     process.env.METADATA_INDEXING_METHOD_COLLECTION ||
