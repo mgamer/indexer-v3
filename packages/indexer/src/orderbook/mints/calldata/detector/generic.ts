50cdd47a76081c17257d647b8412508a4ad5069a
lljxx1 <lljxx1@gmail.com>
2023-11-06 00:21:57 +0800

    feat: guess possible constant params by statistics

--- packages/indexer/src/orderbook/mints/calldata/detector/generic.ts
+++ packages/indexer/src/orderbook/mints/calldata/detector/generic.ts
@@ -1,10 +1,12 @@
-import { ParamType } from "@ethersproject/abi";
+import { ParamType, JsonFragmentType } from "@ethersproject/abi";
 import { BigNumber } from "@ethersproject/bignumber";
 import { HashZero } from "@ethersproject/constants";
 import * as Sdk from "@reservoir0x/sdk";
-
+import { idb } from "@/common/db";
+import { AddressZero } from "@ethersproject/constants";
+import * as utils from "@/events-sync/utils";
 import { logger } from "@/common/logger";
-import { bn } from "@/common/utils";
+import { bn, toBuffer, fromBuffer } from "@/common/utils";
 import { config } from "@/config/index";
 import { Transaction } from "@/models/transactions";
 import {
@@ -14,18 +16,111 @@ import {
 } from "@/orderbook/mints";
 import { AbiParam } from "@/orderbook/mints/calldata";
 import { getMaxSupply } from "@/orderbook/mints/calldata/helpers";
-import { getMethodSignature } from "@/orderbook/mints/method-signatures";
+import { getMethodSignature, MethodSignature } from "@/orderbook/mints/method-signatures";
 
 const STANDARD = "unknown";
 
 const isEmptyOrZero = (array: string[], emptyValue: string) =>
   !array.length || array.every((i) => i === emptyValue);
 
+const checkIsComplexParam = (abiType: string) => {
+  // Support complex types as long as they're empty or contain "zero" values
+  const complexKeywords = ["(", ")", "[", "]", "bytes", "tuple"];
+  return complexKeywords.some((c) => abiType.includes(c));
+};
+
+export async function getSampleTxs(collection: string) {
+  const mintTxHashs = await idb
+    .manyOrNone(
+      `
+        SELECT
+          DISTINCT(nft_transfer_events.tx_hash) as tx_hash
+        FROM nft_transfer_events
+        WHERE 
+          nft_transfer_events.address = $/contract/
+          AND nft_transfer_events.from = $/from/
+        LIMIT 15
+      `,
+      {
+        contract: toBuffer(collection),
+        from: toBuffer(AddressZero),
+      }
+    )
+    .then((ts) =>
+      ts.map((t) => ({
+        txHash: fromBuffer(t.tx_hash),
+      }))
+    );
+  const mintTxs: Transaction[] = await Promise.all(
+    mintTxHashs.map((c) => utils.fetchTransaction(c.txHash))
+  );
+  return mintTxs;
+}
+
+export async function getConstantParamsWithSampleTxs(
+  methodSignature: MethodSignature,
+  sampleTxs: Transaction[]
+) {
+  // Guess possible constant params by statistics from multiple mint transactions
+  const constantParams: number[] = [];
+
+  const parsedMethodSignaturies: MethodSignature[] = [];
+  const valueStats: Map<string, number> = new Map();
+  for (const sampleTx of sampleTxs) {
+    const parsedMethodSignature = await getMethodSignature(sampleTx.data);
+    if (parsedMethodSignature && parsedMethodSignature.signature === methodSignature.signature) {
+      parsedMethodSignaturies.push(parsedMethodSignature);
+    }
+  }
+
+  // Sample data too small
+  if (parsedMethodSignaturies.length < 2) {
+    return constantParams;
+  }
+
+  for (const parsedMethodSignature of parsedMethodSignaturies) {
+    parsedMethodSignature.inputs.forEach((abi, i) => {
+      const decodedValue = parsedMethodSignature.decodedCalldata[i];
+      const strValue = decodedValue.toString();
+      const statKey = `param:${i}:${strValue}`;
+      const count = valueStats.get(statKey);
+      if (count != null) {
+        valueStats.set(statKey, count + 1);
+      } else {
+        valueStats.set(statKey, 1);
+      }
+    });
+  }
+
+  const totalSamples = sampleTxs.length;
+  const statPercentThreshold = 80;
+
+  methodSignature.inputs.forEach((abi, i) => {
+    const abiType = abi.type!;
+    const isComplexParam = checkIsComplexParam(abiType);
+    const decodedValue = methodSignature.decodedCalldata[i];
+    const strValue = decodedValue.toString();
+    if (isComplexParam) {
+      const statKey = `param:${i}:${strValue}`;
+      const statCount = valueStats.get(statKey);
+      if (statCount) {
+        const percent = (statCount * 100) / totalSamples;
+        if (percent > statPercentThreshold) {
+          constantParams.push(i);
+        }
+      }
+    }
+  });
+
+  return constantParams;
+}
+
 export const extractByTx = async (
   collection: string,
   tx: Transaction,
   pricePerAmountMinted: BigNumber,
-  amountMinted: BigNumber
+  amountMinted: BigNumber,
+  sampleTxs?: Transaction[]
 ): Promise<CollectionMint[]> => {
   const maxSupply = await getMaxSupply(collection);
 
@@ -60,20 +155,17 @@ export const extractByTx = async (
     return [];
   }
 
-  // Support complex types as long as they're empty or contain "zero" values
-  const complexKeywords = ["(", ")", "[", "]", "bytes", "tuple"];
-
   const parsedParams = methodSignature.inputs.map((c) => c.type!);
-  const hasComplexParams = parsedParams.some((abiType) =>
-    complexKeywords.some((x) => abiType.includes(x))
-  );
+  const hasComplexParams = parsedParams.some((abiType) => checkIsComplexParam(abiType));
 
   let emptyOrZero = false;
+  let possibleConstantParams: number[] = [];
+
   if (hasComplexParams) {
     parsedParams.forEach((abiType, i) => {
       const decodedValue = methodSignature.decodedCalldata[i];
 
-      const isComplexParam = complexKeywords.some((c) => abiType.includes(c));
+      const isComplexParam = checkIsComplexParam(abiType);
       if (isComplexParam && abiType.includes("tuple")) {
         const subParams = methodSignature.inputs[i].components!;
 
@@ -92,6 +184,16 @@ export const extractByTx = async (
     });
 
     if (!emptyOrZero) {
+      const sampleMintTxs = sampleTxs ? sampleTxs : await getSampleTxs(collection);
+      if (sampleMintTxs.length) {
+        possibleConstantParams = await getConstantParamsWithSampleTxs(
+          methodSignature,
+          sampleMintTxs
+        );
+      }
+    }
+
+    if (!emptyOrZero && possibleConstantParams.length === 0) {
       return [];
     }
   }
@@ -102,17 +204,41 @@ export const extractByTx = async (
     if (methodSignature.params.length) {
       parsedParams.forEach((abiType, i) => {
         const decodedValue = methodSignature.decodedCalldata[i];
-        if (abiType.includes("int") && bn(decodedValue).eq(amountMinted)) {
+        const isComplexParam = checkIsComplexParam(abiType);
+        const isConstantParam = possibleConstantParams.some((c) => c == i);
+
+        if (isConstantParam) {
+          params.push({
+            kind: "unknown",
+            abiType: ParamType.fromObject(methodSignature.inputs[i]).format(),
+            abiValue: decodedValue,
+          });
+        } else if (
+          abiType.includes("int") &&
+          (isComplexParam
+            ? decodedValue.length === 1 && bn(decodedValue[0]).eq(amountMinted)
+            : bn(decodedValue).eq(amountMinted))
+        ) {
           params.push({
             kind: "quantity",
             abiType,
           });
-        } else if (abiType.includes("address") && decodedValue.toLowerCase() === collection) {
+        } else if (
+          abiType.includes("address") &&
+          (isComplexParam
+            ? decodedValue.length === 1 && decodedValue[0].toLowerCase() === collection
+            : decodedValue.toLowerCase() === collection)
+        ) {
           params.push({
             kind: "contract",
             abiType,
           });
-        } else if (abiType.includes("address") && decodedValue.toLowerCase() === tx.from) {
+        } else if (
+          abiType.includes("address") &&
+          (isComplexParam
+            ? decodedValue.length === 1 && decodedValue[0].toLowerCase() === tx.from
+            : decodedValue.toLowerCase() === tx.from)
+        ) {
           params.push({
             kind: "recipient",
             abiType,
@@ -134,6 +260,10 @@ export const extractByTx = async (
     }
   } catch (error) {
     logger.error("mint-detector", JSON.stringify({ kind: STANDARD, error }));
+  }
+
+  if (params.length != parsedParams.length) {
+    return [];
   }
 
   const collectionMint: CollectionMint = {
