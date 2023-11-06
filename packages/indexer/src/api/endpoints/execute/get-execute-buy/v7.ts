b9ae28626735910072e23edef210a1db3df9b235
George Roman <george.roman.99@gmail.com>
2023-11-06 17:23:29 +0100

    feat: auto-test seaport intent fills

--- packages/indexer/src/api/endpoints/execute/get-execute-buy/v7.ts
+++ packages/indexer/src/api/endpoints/execute/get-execute-buy/v7.ts
@@ -1506,6 +1506,20 @@ export const getExecuteBuyV7Options: RouteOptions = {
         },
       ];
 
+      try {
+        // Simulate filling via a Seaport intent for testing things
+        if (config.seaportSolverBaseUrl) {
+          if (items.length === 1 && items[0].token && items[0].fillType !== "mint") {
+            await axios.post(`${config.seaportSolverBaseUrl}/simulate`, {
+              chainId: config.chainId,
+              token: items[0].token,
+            });
+          }
+        }
+      } catch {
+        // Skip errors
+      }
+
       // Seaport intent purchasing MVP
       if (payload.executionMethod === "seaport-intent") {
         if (!config.seaportSolverBaseUrl) {
