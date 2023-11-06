a29714cc0d5a7f5372fc820dee8bcbd4afe34922
tv <tv3636@gmail.com>
2023-11-06 09:59:39 -0800

    fix: remove log

--- packages/indexer/src/pubsub/events/metadata-reenable-event.ts
+++ packages/indexer/src/pubsub/events/metadata-reenable-event.ts
@@ -1,6 +1,4 @@
-import { logger } from "@/common/logger";
 import { MetadataStatus } from "@/models/metadata-status";
-import { Channel } from "@/pubsub/channels";
 
 export class MetadataReenabledEvent {
   public static async handleEvent(message: string) {
@@ -8,13 +6,5 @@ export class MetadataReenabledEvent {
     for (const id of parsedMessage.ids) {
       delete MetadataStatus.disabled[id];
     }
-
-    logger.info(
-      Channel.MetadataReenabled,
-      JSON.stringify({
-        message: `Reenabled collection metadata`,
-        ids: parsedMessage.ids,
-      })
-    );
   }
 }
