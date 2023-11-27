b0511f43ce808f3e44f2d05787e1448f9abb8152
apoorvlathey <latheyapoorv@gmail.com>
2023-11-27 22:47:24 +0530

    feat: deploy NFTXV3Module to sepolia

--- packages/sdk/src/nftx-v3/addresses.ts
+++ packages/sdk/src/nftx-v3/addresses.ts
@@ -3,29 +3,33 @@ import { ChainIdToAddress, Network } from "../utils";
 export const VaultFactory: ChainIdToAddress = {
   [Network.EthereumGoerli]: "0x1d552A0e6c2f680872C4a88b1e7def05F1858dF0",
   [Network.EthereumSepolia]: "0x31C56CaF49125043e80B4d3C7f8734f949d8178C",
+  [Network.EthereumSepolia]: "0x31C56CaF49125043e80B4d3C7f8734f949d8178C",
 };
 
 export const MarketplaceZap: ChainIdToAddress = {
   [Network.EthereumGoerli]: "0x5A40C0288d23E83a23bb16C29B790F7e49e49ee6",
+  [Network.EthereumSepolia]: "0x5Af324A8c90966Bef28386A67c6bE0A16aA03c19",
   [Network.EthereumSepolia]: "0x5Af324A8c90966Bef28386A67c6bE0A16aA03c19",
 };
 
 export const NFTXUniversalRouter: ChainIdToAddress = {
   [Network.EthereumGoerli]: "0xF7c4FC5C2e30258e1E4d1197fc63aeDE371508f3",
   [Network.EthereumSepolia]: "0x12156cCA1958B6591CC49EaE03a5553458a4b424",
+  [Network.EthereumSepolia]: "0x12156cCA1958B6591CC49EaE03a5553458a4b424",
 };
 
 export const CreateVaultZap: ChainIdToAddress = {
-  [Network.EthereumGoerli]: "0xd58A1627bF1C2F8C724A9493A979C0369ab17AaB",
+  [Network.EthereumGoerli]: "0x040fE06ABc3c099772DEe413dE138937bf053543",
   [Network.EthereumSepolia]: "0xE7d5d168A8812Dcbe372A44a7632d6156f4DB13b",
 };
 
 export const QuoterV2: ChainIdToAddress = {
   [Network.EthereumGoerli]: "0xBb473dbEF3363b5d7CDD5f12429Fd1C5F0c10499",
   [Network.EthereumSepolia]: "0xb8EB27ca4715f7A04228c6F83935379D1f5AbABd",
+  [Network.EthereumSepolia]: "0xb8EB27ca4715f7A04228c6F83935379D1f5AbABd",
 };
 
 export const NFTXV3Module: ChainIdToAddress = {
   [Network.EthereumGoerli]: "0xfe6A828fF94A0F027402A56711F71526593422e5",
-  [Network.EthereumSepolia]: "0xfe6A828fF94A0F027402A56711F71526593422e5",
+  [Network.EthereumSepolia]: "0xfe6a828ff94a0f027402a56711f71526593422e5",
 };
