2a18ee29889fb9f37e55b41a09b73cb78d325f45
Idan Peleg <ipeleg@hotmail.com>
2023-11-30 16:05:56 -0500

    feat: added supply backfill script

---
+++ packages/indexer/src/jobs/backfill/backfill-token-supply.ts
@@ -0,0 +1,86 @@
+import { idb } from "@/common/db";
+
+import { AbstractRabbitMqJobHandler } from "@/jobs/abstract-rabbit-mq-job-handler";
+import { RabbitMQMessage } from "@/common/rabbit-mq";
+import { fromBuffer } from "@/common/utils";
+// import { redis, redlock } from "@/common/redis";
+// import { config } from "@/config/index";
+import { tokenReclacSupplyJob } from "@/jobs/token-updates/token-reclac-supply-job";
+
+export type BackfillUserCollectionsJobCursorInfo = {
+  owner: string;
+  acquiredAt: string;
+};
+
+export class BackfillTokenSupplyJob extends AbstractRabbitMqJobHandler {
+  queueName = "backfill-token-supply";
+  maxRetries = 10;
+  concurrency = 1;
+  persistent = false;
+  lazyMode = false;
+  singleActiveConsumer = true;
+
+  protected async process() {
+    const values = {
+      limit: 100,
+    };
+
+    const tokensToSync = [];
+
+    const tokens = await idb.manyOrNone(
+      `
+        SELECT contract, token_id
+        FROM tokens
+        WHERE supply IS NULL
+        ORDER BY updated_at ASC
+        LIMIT $/limit/
+        `,
+      values
+    );
+
+    if (tokens) {
+      for (const token of tokens) {
+        tokensToSync.push({ contract: fromBuffer(token.contract), tokenId: token.token_id });
+      }
+
+      await tokenReclacSupplyJob.addToQueue(tokensToSync, 0);
+    }
+
+    // Check if there are more potential users to sync
+    if (tokensToSync.length == values.limit) {
+      return {
+        addToQueue: true,
+      };
+    }
+
+    return { addToQueue: false };
+  }
+
+  public async onCompleted(
+    rabbitMqMessage: RabbitMQMessage,
+    processResult: {
+      addToQueue: boolean;
+    }
+  ) {
+    if (processResult.addToQueue) {
+      await this.addToQueue();
+    }
+  }
+
+  public async addToQueue(delay = 0) {
+    await this.send({ payload: {} }, delay);
+  }
+}
+
+export const backfillTokenSupplyJob = new BackfillTokenSupplyJob();
+
+// if (config.chainId !== 1) {
+//   redlock
+//     .acquire(["backfill-user-collections-lock-4"], 60 * 60 * 24 * 30 * 1000)
+//     .then(async () => {
+//       await redis.expire("sync-user-collections", 10);
+//     })
+//     .catch(() => {
+//       // Skip on any errors
+//     });
+// }
--- packages/indexer/src/jobs/index.ts
+++ packages/indexer/src/jobs/index.ts
@@ -178,6 +178,7 @@ import { onchainMetadataProcessTokenUriJob } from "@/jobs/metadata-index/onchain
 import { updateUserCollectionsJob } from "@/jobs/nft-balance-updates/update-user-collections-job";
 import { resyncUserCollectionsJob } from "@/jobs/nft-balance-updates/reynsc-user-collections-job";
 import { backfillUserCollectionsJob } from "@/jobs/backfill/backfill-user-collections";
+import { backfillTokenSupplyJob } from "@/jobs/backfill/backfill-token-supply";
 
 export const allJobQueues = [
   backfillWrongNftBalances.queue,
@@ -338,6 +339,7 @@ export class RabbitMqJobsConsumer {
       updateUserCollectionsJob,
       resyncUserCollectionsJob,
       backfillUserCollectionsJob,
+      backfillTokenSupplyJob,
     ];
   }
 
