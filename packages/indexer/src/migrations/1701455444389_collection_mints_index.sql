817e3d23a45c9674c83d67f726072216ff546acf
armando <armandomvillanueva@icloud.com>
2023-12-01 10:56:39 -0800

    feat: update index

--- packages/indexer/src/migrations/1701455444389_collection_mints_index.sql
+++ packages/indexer/src/migrations/1701455444389_collection_mints_index.sql
@@ -1,6 +1,8 @@
 -- Up Migration
 
 CREATE INDEX "collection_mints_kind_status_price_index"
-  ON "collection_mints" ("kind", "status", "price");
+  ON "collection_mints" ("kind", "status", "price")
+  WHERE "kind" = 'public' AND "status" = 'open';
+
 
 -- Down Migration
