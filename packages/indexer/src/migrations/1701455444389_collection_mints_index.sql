5fe77154052dc198143dc865dd6dfdc207ddb460
armando <armandomvillanueva@icloud.com>
2023-12-04 09:11:52 -0800

    feat: remove migration and add to intial script

--- packages/indexer/src/migrations/1701455444389_collection_mints_index.sql
+++
@@ -1,8 +0,0 @@
--- Up Migration
-
-CREATE INDEX "collection_mints_kind_status_price_index"
-  ON "collection_mints" ("kind", "status", "price")
-  WHERE "kind" = 'public' AND "status" = 'open';
-
-
--- Down Migration
