21f601e9d2589bbf5aba17a4a04d222b0f368e61
Idan Peleg <ipeleg@hotmail.com>
2023-12-04 16:02:29 -0500

    feat: remove cursor

--- packages/indexer/src/jobs/backfill/backfill-token-supply.ts
+++ packages/indexer/src/jobs/backfill/backfill-token-supply.ts
@@ -6,11 +6,6 @@ import { fromBuffer } from "@/common/utils";
 import { redlock } from "@/common/redis";
 import { config } from "@/config/index";
 import { tokenReclacSupplyJob } from "@/jobs/token-updates/token-reclac-supply-job";
-import _ from "lodash";
-
-export type CursorInfo = {
-  cursor: string;
-};
 
 export class BackfillTokenSupplyJob extends AbstractRabbitMqJobHandler {
   queueName = "backfill-token-supply";
@@ -20,9 +15,7 @@ export class BackfillTokenSupplyJob extends AbstractRabbitMqJobHandler {
   lazyMode = false;
   singleActiveConsumer = true;
 
-  protected async process(payload: CursorInfo) {
-    const { cursor } = payload;
-
+  protected async process() {
     const values = {
       limit: 100,
     };
@@ -34,7 +27,6 @@ export class BackfillTokenSupplyJob extends AbstractRabbitMqJobHandler {
         SELECT contract, token_id, updated_at::text
         FROM tokens
         WHERE supply IS NULL
-        ${cursor ? `AND updated_at >= '${cursor}'` : ""}
         ORDER BY updated_at ASC
         LIMIT $/limit/
         `,
@@ -53,7 +45,6 @@ export class BackfillTokenSupplyJob extends AbstractRabbitMqJobHandler {
     if (tokensToSync.length == values.limit) {
       return {
         addToQueue: true,
-        cursor: _.last(tokens).updated_at,
       };
     }
 
@@ -64,16 +55,15 @@ export class BackfillTokenSupplyJob extends AbstractRabbitMqJobHandler {
     rabbitMqMessage: RabbitMQMessage,
     processResult: {
       addToQueue: boolean;
-      cursor?: string;
     }
   ) {
     if (processResult.addToQueue) {
-      await this.addToQueue({ cursor: processResult.cursor ?? "" });
+      await this.addToQueue();
     }
   }
 
-  public async addToQueue(cursor?: CursorInfo, delay = 0) {
-    await this.send({ payload: cursor ?? {} }, delay);
+  public async addToQueue(delay = 0) {
+    await this.send({ payload: {} }, delay);
   }
 }
 
