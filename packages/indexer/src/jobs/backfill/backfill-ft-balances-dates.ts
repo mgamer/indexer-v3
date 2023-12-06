f8b789a8bc20daded9c90d4e9cb4d9091033d49c
Idan Peleg <ipeleg@hotmail.com>
2023-12-06 16:05:17 -0500

    feat: added ft balances backfill

---
+++ packages/indexer/src/jobs/backfill/backfill-ft-balances-dates.ts
@@ -0,0 +1,97 @@
+import { idb } from "@/common/db";
+
+import { AbstractRabbitMqJobHandler } from "@/jobs/abstract-rabbit-mq-job-handler";
+import { RabbitMQMessage } from "@/common/rabbit-mq";
+import _ from "lodash";
+import { fromBuffer, toBuffer } from "@/common/utils";
+
+export type BackfillUserCollectionsJobCursorInfo = {
+  owner: string;
+  contract: string;
+};
+
+export class BackfillFtBalancesDatesJob extends AbstractRabbitMqJobHandler {
+  queueName = "backfill-ft-balances-dates";
+  maxRetries = 10;
+  concurrency = 1;
+  persistent = false;
+  lazyMode = false;
+  singleActiveConsumer = true;
+
+  protected async process(payload: BackfillUserCollectionsJobCursorInfo) {
+    const { owner, contract } = payload;
+    const values: {
+      limit: number;
+      contract?: Buffer;
+      owner?: Buffer;
+    } = {
+      limit: 500,
+    };
+
+    let cursor = "";
+
+    if (owner) {
+      cursor = `AND (owner, contract) > ($/owner/, $/contract/)`;
+      values.owner = toBuffer(owner);
+      values.contract = toBuffer(contract);
+    }
+
+    const results = await idb.manyOrNone(
+      `
+        UPDATE ft_balances
+        SET created_at = now(), updated_at = now()
+        WHERE (owner, contract) IN (
+          SELECT owner, contract
+          FROM ft_balances
+          WHERE created_at IS NULL
+          ${cursor}
+          ORDER BY owner, contract
+          LIMIT $/limit/
+        )
+        RETURNING owner, contract
+        `,
+      values
+    );
+
+    // Check if there are more potential users to sync
+    if (results.length == values.limit) {
+      const lastItem = _.last(results);
+
+      return {
+        addToQueue: true,
+        cursor: { owner: fromBuffer(lastItem.owner), acquiredAt: fromBuffer(lastItem.contract) },
+      };
+    }
+
+    return { addToQueue: false };
+  }
+
+  public async onCompleted(
+    rabbitMqMessage: RabbitMQMessage,
+    processResult: {
+      addToQueue: boolean;
+      cursor?: BackfillUserCollectionsJobCursorInfo;
+    }
+  ) {
+    if (processResult.addToQueue) {
+      await this.addToQueue(processResult.cursor);
+    }
+  }
+
+  public async addToQueue(cursor?: BackfillUserCollectionsJobCursorInfo, delay = 0) {
+    await this.send({ payload: cursor ?? {} }, delay);
+  }
+}
+
+export const backfillFtBalancesDatesJob = new BackfillFtBalancesDatesJob();
+
+// if (config.chainId !== 1) {
+//   redlock
+//     .acquire(["backfill-user-collections-lock-4"], 60 * 60 * 24 * 30 * 1000)
+//     .then(async () => {
+//       await backfillUserCollectionsJob.addToQueue().
+//     })
+//     .catch(() => {
+//       // Skip on any errors
+//     });
+// }
