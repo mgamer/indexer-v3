92a97bed206437bc39555560e351b2aa1ea47522
Idan Peleg <ipeleg@hotmail.com>
2023-12-12 17:23:40 -0500

    feat: update limit

--- packages/indexer/src/jobs/backfill/backfill-ft-balances-dates.ts
+++ packages/indexer/src/jobs/backfill/backfill-ft-balances-dates.ts
@@ -27,11 +27,7 @@ export class BackfillFtBalancesDatesJob extends AbstractRabbitMqJobHandler {
       contract?: Buffer;
       owner?: Buffer;
     } = {
-      limit: _.includes([56, 137, 324, 42161, 42170, 43114, 80001], config.chainId)
-        ? config.chainId === 324
-          ? 10
-          : 50
-        : 300,
+      limit: _.includes([56, 137, 324, 42161, 42170, 43114, 80001], config.chainId) ? 20 : 300,
     };
 
     let cursor = "";
--- packages/indexer/src/jobs/backfill/backfill-ft-transfer-events-dates.ts
+++ packages/indexer/src/jobs/backfill/backfill-ft-transfer-events-dates.ts
@@ -24,11 +24,7 @@ export class BackfillFtTransferEventsDatesJob extends AbstractRabbitMqJobHandler
       limit: number;
       block?: number;
     } = {
-      limit: _.includes([56, 137, 324, 42161, 42170, 43114, 80001], config.chainId)
-        ? config.chainId === 324
-          ? 10
-          : 50
-        : 500,
+      limit: _.includes([56, 137, 324, 42161, 42170, 43114, 80001], config.chainId) ? 20 : 500,
     };
 
     let cursor = "";
