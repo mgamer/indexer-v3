ac44a7b45dd74f023c59e60e6028ec2584d975ad
d3or <deorrunner@gmail.com>
2023-12-12 11:56:56 -0500

    feat: filter out tx data that starts with 0x64617461

--- packages/indexer/src/models/transactions/index.ts
+++ packages/indexer/src/models/transactions/index.ts
@@ -139,6 +139,9 @@ export const saveTransactionsV2 = async (transactions: Transaction[]) => {
   // filtter out transactions with same from and to
   if (config.chainId === 137) {
     transactions = transactions.filter((t) => t.from !== t.to);
+
+    // filter out tx data that starts with 0x64617461
+    transactions = transactions.filter((t) => !t.data.startsWith("0x64617461"));
   }
   const CHUNK_SIZE = 10;
 
