aa9dc9c58806b0a13d91cf03f87c2fa8c6bd9d7a
d3or <deorrunner@gmail.com>
2023-12-14 13:46:41 -0500

    feat: default to null

--- packages/indexer/src/migrations/17025792250000_fill_events_kind.sql
+++ packages/indexer/src/migrations/17025792250000_fill_events_kind.sql
@@ -7,7 +7,7 @@ CREATE TYPE "fill_event_kind" AS ENUM (
   'sale',
 );
 
-ALTER TYPE "fill_events_2" ADD COLUMN "kind" "fill_event_kind" DEFAULT 'mint';
+ALTER TYPE "fill_events_2" ADD COLUMN "kind" "fill_event_kind";
 
 -- Down Migration
 
