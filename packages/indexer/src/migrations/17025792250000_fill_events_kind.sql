11f46cb51f28c77037890983080d03775b518ee7
d3or <deorrunner@gmail.com>
2023-12-14 13:46:14 -0500

    feat: fix migration

--- packages/indexer/src/migrations/17025792250000_fill_events_kind.sql
+++ packages/indexer/src/migrations/17025792250000_fill_events_kind.sql
@@ -1,6 +1,13 @@
 -- Up Migration
+CREATE TYPE "fill_event_kind" AS ENUM (
+  'mint',
+  'transfer',
+  'burn',
+  'airdrop',
+  'sale',
+);
 
-ALTER TYPE "fill_events_2" ADD COLUMN "kind" TEXT;
+ALTER TYPE "fill_events_2" ADD COLUMN "kind" "fill_event_kind" DEFAULT 'mint';
 
 -- Down Migration
 
