dc9e50c01defbd06b41139b4f67937f6f67eacc3
lljxx1 <lljxx1@gmail.com>
2023-12-18 13:43:22 +0800

    feat: check order is offchain cancelable

--- packages/indexer/src/utils/payment-processor-v2/index.ts
+++ packages/indexer/src/utils/payment-processor-v2/index.ts
@@ -57,7 +57,7 @@ export const getCollectionPaymentSettings = async (
   contract: string,
   refresh?: boolean
 ): Promise<CollectionPaymentSettings | undefined> => {
-  const cacheKey = `payment-processor-v2-payment-settings-by-contract:v2:${contract}`;
+  const cacheKey = `payment-processor-v2-payment-settings-by-contract:${contract}`;
 
   let result = await redis
     .get(cacheKey)
