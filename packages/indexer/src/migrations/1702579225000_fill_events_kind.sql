6380386dd9eb866535d85a631017365825e3276c
d3or <deorrunner@gmail.com>
2023-12-19 16:24:54 -0500

    feat: add nft balances migration

--- packages/indexer/src/migrations/1702579225000_fill_events_kind.sql
+++ packages/indexer/src/migrations/1702579225000_fill_events_kind.sql
@@ -1,14 +1,16 @@
 -- Up Migration
-CREATE TYPE "fill_event_kind" AS ENUM (
+CREATE TYPE "transfer_event_kind" AS ENUM (
   'mint',
   'burn',
   'airdrop'
 );
 
-ALTER TABLE "fill_events_2" ADD COLUMN "kind" "fill_event_kind";
+ALTER TABLE "nft_transfer_events" ADD COLUMN "kind" "transfer_event_kind";
+
+ALTER TABLE "nft_balances" ADD COLUMN "is_airdropped" BOOLEAN;
 
 -- Down Migration
 
-ALTER TABLE "fill_events_2" DROP COLUMN "kind";
+ALTER TABLE "nft_transfer_events" DROP COLUMN "kind";
 
-DROP TYPE "fill_event_kind";
+DROP TYPE "transfer_event_kind";
