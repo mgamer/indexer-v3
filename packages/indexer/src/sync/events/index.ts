cddfc15c8e40a29b661130ba9297ea3fc66e2414
d3or <deorrunner@gmail.com>
2023-12-20 10:55:03 -0500

    feat: only save transactions that we have a log for

--- packages/indexer/src/sync/events/index.ts
+++ packages/indexer/src/sync/events/index.ts
@@ -443,6 +443,13 @@ export const syncEvents = async (
     throw new Error(`No logs found for blocks ${blocks.fromBlock} to ${blocks.toBlock}`);
   }
 
+  // filter out transactions that we have no log for (we dont want to save these transactions)
+  blockData.forEach((block) => {
+    block.transactions = block.transactions.filter((tx) =>
+      logs.find((log) => log.transactionHash === tx.hash)
+    );
+  });
+
   const saveDataTimes = await Promise.all([
     ...blockData.map(async (block) => {
       return await Promise.all([
