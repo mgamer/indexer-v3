704c7818059bb24801a2956841dc85c67bc85352
armando <armandomvillanueva@icloud.com>
2023-12-21 11:06:50 -0800

    feat: remove logging

--- packages/indexer/src/api/endpoints/collections/get-trending-mints/v1.ts
+++ packages/indexer/src/api/endpoints/collections/get-trending-mints/v1.ts
@@ -157,11 +157,6 @@ export const getTrendingMintsV1Options: RouteOptions = {
     try {
       const mintingCollections = await getMintingCollections(type);
 
-      logger.info(
-        `get-trending-mints-${version}-handler`,
-        `Minting collections: ${mintingCollections.length}`
-      );
-
       if (mintingCollections.length < 1) {
         const response = h.response({ mints: [] });
         return response;
@@ -172,11 +167,6 @@ export const getTrendingMintsV1Options: RouteOptions = {
         period,
         limit,
       });
-
-      logger.info(
-        `get-trending-mints-${version}-handler`,
-        `Trending mints: ${trendingMints.length}`
-      );
 
       if (trendingMints.length < 1) {
         const response = h.response({ mints: [] });
@@ -275,11 +265,6 @@ async function formatCollections(
   useNonFlaggedFloorAsk: boolean
 ): Promise<any[]> {
   const sources = await Sources.getInstance();
-
-  logger.info(
-    `get-trending-mints-${version}-handler`,
-    `Format collections: ${collectionsResult.length}`
-  );
 
   const collections = await Promise.all(
     collectionsResult.map(async (r) => {
