a8336f94ffabf848d12af5c4ad23d4fb258b5405
George Roman <30772943+georgeroman@users.noreply.github.com>
2023-12-27 14:14:10 +0100

    fix: tweaks

--- packages/indexer/src/jobs/orderbook/cron/pending-expired-orders-check-job.ts
+++ packages/indexer/src/jobs/orderbook/cron/pending-expired-orders-check-job.ts
@@ -3,8 +3,9 @@ import cron from "node-cron";
 import { ridb } from "@/common/db";
 import { logger } from "@/common/logger";
 import { redlock } from "@/common/redis";
-import { AbstractRabbitMqJobHandler } from "@/jobs/abstract-rabbit-mq-job-handler";
 import { config } from "@/config/index";
+import { AbstractRabbitMqJobHandler } from "@/jobs/abstract-rabbit-mq-job-handler";
+import * as backfillExpiredOrders from "@/jobs/backfill/backfill-expired-orders-2";
 
 export class PendingExpiredOrdersCheckJob extends AbstractRabbitMqJobHandler {
   queueName = "pending-expired-orders-check-queue";
@@ -16,18 +17,38 @@ export class PendingExpiredOrdersCheckJob extends AbstractRabbitMqJobHandler {
   protected async process() {
     const result = await ridb.oneOrNone(
       `
-              SELECT
-                count(*) AS expired_count
-              FROM orders
-              WHERE upper(orders.valid_between) < now()
-                AND (orders.fillability_status = 'fillable' OR orders.fillability_status = 'no-balance')
-            `
+        SELECT
+          count(*) AS expired_count
+        FROM orders
+        WHERE upper(orders.valid_between) < now()
+          AND (orders.fillability_status = 'fillable' OR orders.fillability_status = 'no-balance')
+      `
     );
 
     logger.info(
       "pending-expired-orders-check",
       JSON.stringify({ pendingExpiredOrdersCount: result.expired_count })
     );
+
+    if (result.expired_count > 0) {
+      const minAndMax = await ridb.oneOrNone(
+        `
+          SELECT
+            floor(extract(epoch FROM min(upper(orders.valid_between)))) AS min_timestamp
+          FROM orders
+          WHERE upper(orders.valid_between) < now()
+            AND (orders.fillability_status = 'fillable' OR orders.fillability_status = 'no-balance')
+        `
+      );
+      if (minAndMax) {
+        await backfillExpiredOrders.addToQueue([
+          {
+            from: minAndMax.min_timestamp,
+            to: minAndMax.min_timestamp + 10000,
+          },
+        ]);
+      }
+    }
   }
 
   public async addToQueue() {
