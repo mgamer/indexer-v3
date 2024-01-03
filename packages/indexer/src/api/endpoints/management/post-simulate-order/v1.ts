3643b85fb6469604f4b330a00673f76aaee99b9c
lljxx1 <lljxx1@gmail.com>
2024-01-03 12:04:08 +0800

    feat: simulate order with token and collection

--- packages/indexer/src/api/endpoints/management/post-simulate-order/v1.ts
+++ packages/indexer/src/api/endpoints/management/post-simulate-order/v1.ts
@@ -38,9 +38,15 @@ export const postSimulateOrderV1Options: RouteOptions = {
   },
   validate: {
     payload: Joi.object({
-      id: Joi.string().lowercase().required(),
+      id: Joi.string().lowercase(),
+      token: Joi.string().lowercase(),
+      collection: Joi.string().lowercase(),
+      kind: Joi.string().lowercase().valid("floor-ask", "top-bid").default("floor-ask"),
       skipRevalidation: Joi.boolean().default(false),
-    }),
+    })
+      .xor("collection", "token", "id")
+      .with("token", "kind")
+      .with("collection", "kind"),
   },
   response: {
     schema: Joi.object({
@@ -109,7 +115,61 @@ export const postSimulateOrderV1Options: RouteOptions = {
     };
 
     try {
-      const id = payload.id;
+      let id = payload.id;
+      const token = payload.token;
+      const collection = payload.collection;
+
+      // Lookup Order id by token
+      if (!id && token) {
+        const response = await inject({
+          method: "GET",
+          url: `/tokens/v7?tokens[]=` + token,
+          headers: {
+            "Content-Type": "application/json",
+            "X-Api-Key": request.headers["x-api-key"],
+          },
+          payload: { token },
+        }).then((response) => JSON.parse(response.payload));
+
+        if (response.tokens.length) {
+          const tokenMarket = response.tokens[0].market;
+          if (tokenMarket) {
+            if (payload.kind === "floor-ask") {
+              id = tokenMarket.floorAsk?.id;
+            } else {
+              id = tokenMarket.topBid?.id;
+            }
+          }
+        }
+      }
+
+      // Lookup Order id by collection
+      if (!id && collection) {
+        const response = await inject({
+          method: "GET",
+          url: `/collections/v7?id=` + collection,
+          headers: {
+            "Content-Type": "application/json",
+            "X-Api-Key": request.headers["x-api-key"],
+          },
+          payload: { token },
+        }).then((response) => JSON.parse(response.payload));
+
+        if (response.collections.length) {
+          const collectionMeta = response.collections[0];
+          if (collectionMeta) {
+            if (payload.kind === "floor-ask") {
+              id = collectionMeta.floorAsk?.id;
+            } else {
+              id = collectionMeta.topBid?.id;
+            }
+          }
+        }
+      }
+
+      if (!id) {
+        throw Boom.badRequest("Could not find order");
+      }
 
       const orderResult = await idb.oneOrNone(
         `
