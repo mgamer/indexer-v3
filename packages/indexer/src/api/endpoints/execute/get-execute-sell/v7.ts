508b67932646512687dc0850893aa7d5b5d774b7
George Roman <30772943+georgeroman@users.noreply.github.com>
2024-01-24 12:59:24 +0100

    fix: tweaks

--- packages/indexer/src/api/endpoints/execute/get-execute-sell/v7.ts
+++ packages/indexer/src/api/endpoints/execute/get-execute-sell/v7.ts
@@ -147,6 +147,7 @@ export const getExecuteSellV7Options: RouteOptions = {
           "If passed, all fills will be executed through the trusted trusted forwarder (where possible)"
         )
         .optional(),
+      currency: Joi.string().lowercase().description("Currency to be received when selling."),
       maxFeePerGas: Joi.string()
         .pattern(regex.number)
         .description(
@@ -1208,6 +1209,7 @@ export const getExecuteSellV7Options: RouteOptions = {
         result = await router.fillBidsTx(bidDetails, payload.taker, {
           source: payload.source,
           partial: payload.partial,
+          sellOutCurrency: payload.currency,
           forceApprovalProxy,
           onError: async (kind, error, data) => {
             errors.push({
