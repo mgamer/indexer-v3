18ee20e74f9a4bcd306683a2a1c714d65fc3da95
lljxx1 <lljxx1@gmail.com>
2024-01-25 14:20:50 +0800

    feat: support sudoswap base

--- packages/sdk/src/sudoswap-v2/addresses.ts
+++ packages/sdk/src/sudoswap-v2/addresses.ts
@@ -3,11 +3,13 @@ import { ChainIdToAddress, Network } from "../utils";
 export const PairFactory: ChainIdToAddress = {
   [Network.Ethereum]: "0xa020d57ab0448ef74115c112d18a9c231cc86000",
   [Network.EthereumGoerli]: "0x967544b2dd5c1c7a459e810c9b60ae4fc8227201",
+  [Network.Base]: "0x605145d263482684590f630e9e581b21e4938eb8",
 };
 
 export const VeryFastRouter = {
   [Network.Ethereum]: "0x090c236b62317db226e6ae6cd4c0fd25b7028b65",
   [Network.EthereumGoerli]: "0xb3d6192e9940bba479c32596431d215faee5f723",
+  [Network.Base]: "0xa07eBD56b361Fe79AF706A2bF6d8097091225548",
 };
 
 export const GDACurve = {
