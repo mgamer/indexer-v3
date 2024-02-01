621ef6228bafd1a2865fb46651eec8431b562624
lljxx1 <lljxx1@gmail.com>
2024-02-01 08:08:02 +0800

    fix: change names

--- packages/indexer/src/api/endpoints/collections/get-collection-marketplace-configurations/v1.ts
+++ packages/indexer/src/api/endpoints/collections/get-collection-marketplace-configurations/v1.ts
@@ -44,7 +44,7 @@ type Marketplace = {
       traitBidSupported: boolean;
       orderKind: OrderKind | null;
       customFeesSupported: boolean;
-      multipleFeesSupported: boolean;
+      numFeesSupported?: number;
       collectionBidSupported?: boolean;
       partialOrderSupported: boolean;
       minimumBidExpiry?: number;
@@ -110,7 +110,7 @@ export const getCollectionMarketplaceConfigurationsV1Options: RouteOptions = {
                 orderKind: Joi.string().allow(null),
                 enabled: Joi.boolean(),
                 customFeesSupported: Joi.boolean(),
-                multipleFeesSupported: Joi.boolean(),
+                numFeesSupported: Joi.number().optional(),
                 minimumBidExpiry: Joi.number(),
                 minimumPrecision: Joi.string(),
                 collectionBidSupported: Joi.boolean(),
@@ -201,7 +201,6 @@ export const getCollectionMarketplaceConfigurationsV1Options: RouteOptions = {
               orderKind: "looks-rare-v2",
               minimumBidExpiry: 15 * 60,
               customFeesSupported: false,
-              multipleFeesSupported: false,
               supportedBidCurrencies: [Sdk.Common.Addresses.WNative[config.chainId]],
               partialOrderSupported: false,
               traitBidSupported: false,
@@ -212,7 +211,6 @@ export const getCollectionMarketplaceConfigurationsV1Options: RouteOptions = {
               orderKind: "seaport",
               minimumBidExpiry: 15 * 60,
               customFeesSupported: false,
-              multipleFeesSupported: false,
               supportedBidCurrencies: [Sdk.Common.Addresses.WNative[config.chainId]],
               partialOrderSupported: false,
               traitBidSupported: false,
@@ -238,7 +236,6 @@ export const getCollectionMarketplaceConfigurationsV1Options: RouteOptions = {
               orderKind: "x2y2",
               enabled: false,
               customFeesSupported: false,
-              multipleFeesSupported: false,
               supportedBidCurrencies: [Sdk.Common.Addresses.WNative[config.chainId]],
               partialOrderSupported: false,
               traitBidSupported: false,
@@ -270,7 +267,6 @@ export const getCollectionMarketplaceConfigurationsV1Options: RouteOptions = {
               orderKind: "seaport-v1.5",
               enabled: true,
               customFeesSupported: true,
-              multipleFeesSupported: true,
               collectionBidSupported:
                 Number(collectionResult.token_count) <= config.maxTokenSetSize,
               supportedBidCurrencies: Object.keys(ns.supportedBidCurrencies),
@@ -284,7 +280,7 @@ export const getCollectionMarketplaceConfigurationsV1Options: RouteOptions = {
               orderKind: "payment-processor",
               enabled: true,
               customFeesSupported: true,
-              multipleFeesSupported: false,
+              numFeesSupported: 1,
               collectionBidSupported:
                 Number(collectionResult.token_count) <= config.maxTokenSetSize,
               supportedBidCurrencies:
@@ -300,7 +296,7 @@ export const getCollectionMarketplaceConfigurationsV1Options: RouteOptions = {
               orderKind: "payment-processor-v2",
               enabled: true,
               customFeesSupported: true,
-              multipleFeesSupported: false,
+              numFeesSupported: 1,
               collectionBidSupported:
                 Number(collectionResult.token_count) <= config.maxTokenSetSize,
               supportedBidCurrencies:
@@ -355,7 +351,6 @@ export const getCollectionMarketplaceConfigurationsV1Options: RouteOptions = {
               orderKind: "seaport-v1.5",
               enabled: false,
               customFeesSupported: false,
-              multipleFeesSupported: true,
               minimumBidExpiry: 15 * 60,
               supportedBidCurrencies: Object.keys(ns.supportedBidCurrencies),
               paymentTokens: collectionResult.payment_tokens?.opensea,
@@ -395,7 +390,6 @@ export const getCollectionMarketplaceConfigurationsV1Options: RouteOptions = {
                 orderKind: "blur",
                 enabled: false,
                 customFeesSupported: false,
-                multipleFeesSupported: false,
                 minimumPrecision: "0.01",
                 minimumBidExpiry: 10 * 24 * 60 * 60,
                 supportedBidCurrencies: [Sdk.Blur.Addresses.Beth[config.chainId]],
