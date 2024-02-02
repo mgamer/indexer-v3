69b19dd1960ef60a072b7c2719c4f6500256421a
lljxx1 <lljxx1@gmail.com>
2024-02-02 09:03:52 +0800

    feat: add blend

--- packages/indexer/src/utils/pending-txs/parser/blur.ts
+++ packages/indexer/src/utils/pending-txs/parser/blur.ts
@@ -1,21 +1,25 @@
 import { Interface } from "@ethersproject/abi";
 import { PendingToken } from "@/utils/pending-txs/types";
 import ExchangeAbi from "@reservoir0x/sdk/dist/blur/abis/Exchange.json";
+import BlendAbi from "@reservoir0x/sdk/dist/blend/abis/Blend.json";
 
 export const parseTokensFromCalldata = async (calldata: string): Promise<PendingToken[]> => {
   const parsedTokens: PendingToken[] = [];
   try {
     const exchangeIface = new Interface(ExchangeAbi);
-    // Parse top level call
-    const { name: funcName, args } = exchangeIface.parseTransaction({
-      data: calldata,
-    });
 
     let executions = [];
-    if (["execute"].includes(funcName)) {
-      executions = [args];
-    } else if (["bulkExecute"].includes(funcName)) {
-      executions = args.executions;
+    try {
+      const { name: funcName, args } = exchangeIface.parseTransaction({
+        data: calldata,
+      });
+      if (["execute"].includes(funcName)) {
+        executions = [args];
+      } else if (["bulkExecute"].includes(funcName)) {
+        executions = args.executions;
+      }
+    } catch {
+      // Skip error
     }
 
     for (let i = 0; i < executions.length; i++) {
@@ -31,6 +35,39 @@ export const parseTokensFromCalldata = async (calldata: string): Promise<Pending
       } catch {
         // Skip erros
       }
+    }
+
+    // Blend
+    const blendIface = new Interface(BlendAbi);
+    const { name: funcName, args } = blendIface.parseTransaction({
+      data: calldata,
+    });
+    if (["buyToBorrow", "buyToBorrowV2", "buyToBorrowETH", "buyToBorrowV2ETH"].includes(funcName)) {
+      const { offer, execution } = args;
+      parsedTokens.push({
+        contract: offer.collection.toLowerCase(),
+        tokenId: execution.listing
+          ? execution.listing.tokenId.toString()
+          : execution.makerOrder.order.tokenId.toString(),
+      });
+    } else if (["buyLocked", "buyLockedETH"].includes(funcName)) {
+      const { lien } = args;
+      parsedTokens.push({
+        contract: lien.collection.toLowerCase(),
+        tokenId: lien.tokenId.toString(),
+      });
+    } else if (["borrow"].includes(funcName)) {
+      const { offer, collateralTokenId } = args;
+      parsedTokens.push({
+        contract: offer.collection.toLowerCase(),
+        tokenId: collateralTokenId.toString(),
+      });
+    } else if (["repay", "takeBid", "takeBidV2"].includes(funcName)) {
+      const { lien } = args;
+      parsedTokens.push({
+        contract: lien.collection.toLowerCase(),
+        tokenId: lien.tokenId.toString(),
+      });
     }
   } catch {
     // Skip errors
