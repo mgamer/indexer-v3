24f6dbb9ab4dc854557e55dc90089c76a2c1a35e
Idan Peleg <ipeleg@hotmail.com>
2024-02-06 18:34:02 -0500

    feat: log only if key not exists

--- packages/indexer/src/sync/events/index.ts
+++ packages/indexer/src/sync/events/index.ts
@@ -763,17 +763,14 @@ export const checkForMissingBlocks = async (block: number) => {
     if (block - latestBlockNumber > 1) {
       // if we are missing more than 1 block, we need to sync the missing blocks
       for (let i = latestBlockNumber + 1; i <= block; i++) {
-        if (
-          config.chainId !== 324 &&
-          !(await redis.exists(`${eventsSyncRealtimeJob.queueName}:${block}`))
-        ) {
+        if (!(await redis.exists(`${eventsSyncRealtimeJob.queueName}:${block}`))) {
           logger.info(
             "sync-events-realtime",
             `Found missing block: ${i} latest block ${block} latestBlock ${latestBlockNumber}`
           );
-        }
 
-        await eventsSyncRealtimeJob.addToQueue({ block: i });
+          await eventsSyncRealtimeJob.addToQueue({ block: i });
+        }
       }
     }
   } else {
