e2ad6a4f1740f1d92e8a8cb4e9388b2ae4c67d98
Idan Peleg <ipeleg@hotmail.com>
2024-02-16 15:02:22 -0500

    feat: set name as string

--- packages/indexer/src/jobs/metadata-index/metadata-write-job.ts
+++ packages/indexer/src/jobs/metadata-index/metadata-write-job.ts
@@ -195,7 +195,7 @@ export default class MetadataIndexWriteJob extends AbstractRabbitMqJobHandler {
       {
         contract: toBuffer(contract),
         tokenId,
-        name: name || null,
+        name: `${name}` || null,
         description: description || null,
         image: imageUrl || null,
         tokenURI: tokenURI || null,
