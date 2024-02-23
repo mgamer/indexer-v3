c4eb2b5aa033da8e3ff32777559ab3a0b1bcf6ee
George Roman <30772943+georgeroman@users.noreply.github.com>
2024-02-23 14:24:23 +0100

    fix: pp-v2 tweaks

--- packages/indexer/src/orderbook/orders/payment-processor-v2/check.ts
+++ packages/indexer/src/orderbook/orders/payment-processor-v2/check.ts
@@ -1,5 +1,6 @@
 import * as Sdk from "@reservoir0x/sdk";
 
+import { idb } from "@/common/db";
 import { baseProvider } from "@/common/provider";
 import { bn } from "@/common/utils";
 import { config } from "@/config/index";
@@ -39,6 +40,56 @@ export const offChainCheck = async (
     const quantityFilled = await commonHelpers.getQuantityFilled(id);
     if (quantityFilled.gte(order.params.amount)) {
       throw new Error("filled");
+    }
+
+    if (
+      order.params.protocol === Sdk.PaymentProcessorV2.Types.OrderProtocols.ERC1155_FILL_PARTIAL
+    ) {
+      try {
+        const exchange = new Sdk.PaymentProcessorV2.Exchange(config.chainId);
+        const result = await exchange.contract
+          .connect(baseProvider)
+          .remainingFillableQuantity(order.params.sellerOrBuyer, order.hashDigest());
+        if (result.state === 0 && result.remainingFillableQuantity.gt(0)) {
+          await idb.none(
+            `
+              UPDATE orders SET
+                quantity_remaining = $/quantityRemaining/,
+                quantity_filled = $/quantityFilled/,
+                updated_at = now()
+              WHERE orders.id = $/id/
+                AND orders.quantity_remaining != $/quantityRemaining/
+                AND orders.quantity_filled != $/quantityFilled/
+            `,
+            {
+              id,
+              quantityRemaining: result.remainingFillableQuantity.toString(),
+              quantityFilled: bn(order.params.amount)
+                .sub(result.remainingFillableQuantity)
+                .toString(),
+            }
+          );
+        } else if (result.state === 1) {
+          await idb.none(
+            `
+              UPDATE orders SET
+                fillability_status = 'filled',
+                quantity_remaining = $/quantityRemaining/,
+                quantity_filled = $/quantityFilled/,
+                updated_at = now()
+              WHERE orders.id = $/id/
+                AND orders.fillability_status = 'fillable'
+            `,
+            {
+              id,
+              quantityRemaining: 0,
+              quantityFilled: order.params.amount,
+            }
+          );
+        }
+      } catch {
+        // Skip errors
+      }
     }
   }
 
