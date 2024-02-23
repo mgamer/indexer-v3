a9f5da2ff591bcd6eb7b2413914853e98a0514b7
George Roman <30772943+georgeroman@users.noreply.github.com>
2024-02-23 11:24:39 +0100

    feat: change default royalty ordering

--- packages/indexer/src/utils/royalties/index.ts
+++ packages/indexer/src/utils/royalties/index.ts
@@ -281,10 +281,12 @@ export const refreshDefaultRoyalties = async (collection: string) => {
   let defaultRoyalties: Royalty[] = [];
   if (royaltiesResult.new_royalties["custom"]) {
     defaultRoyalties = royaltiesResult.new_royalties["custom"];
-  } else if (royaltiesResult.new_royalties["onchain"]) {
-    defaultRoyalties = royaltiesResult.new_royalties["onchain"];
+  } else if (royaltiesResult.new_royalties["eip2981"]) {
+    defaultRoyalties = royaltiesResult.new_royalties["eip2981"];
   } else if (royaltiesResult.new_royalties["pp-v2-backfill"]) {
     defaultRoyalties = royaltiesResult.new_royalties["pp-v2-backfill"];
+  } else if (royaltiesResult.new_royalties["onchain"]) {
+    defaultRoyalties = royaltiesResult.new_royalties["onchain"];
   } else if (royaltiesResult.new_royalties["opensea"]) {
     defaultRoyalties = royaltiesResult.new_royalties["opensea"];
   }
