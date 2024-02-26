fc9a799dd1fab9e9ecb735d963cda8bde503c2e1
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2024-02-26 16:26:59 -0500

    feat: add exception handling (#8161)
    * feat: add exception handling
    
    * feat: add error handling

--- packages/indexer/src/metadata/extend/kanpai-pandas/index.ts
+++ packages/indexer/src/metadata/extend/kanpai-pandas/index.ts
@@ -1,29 +1,46 @@
 import axios from "axios";
 import { TokenMetadata } from "@/metadata/types";
+import { logger } from "@/common/logger";
 
 const metadataBaseURI = "https://metadata-api-snag-solutions-ad3b2d55c570.herokuapp.com/api/token";
 
 export const extend = async (metadata: TokenMetadata) => {
-  const response = await axios.get(`${metadataBaseURI}/${metadata.tokenId}`);
+  try {
+    const response = await axios.get(`${metadataBaseURI}/${metadata.tokenId}`);
 
-  const { Attached, Inventory, Rank } = response.data.data;
-  const attributes = (Attached || [])
-    .map((a: { Category: string; Name: string }) => ({
-      key: a.Category,
-      value: a.Name,
-      kind: "string",
-      rank: Rank,
-    }))
-    .concat(
-      (Inventory || []).map((a: { Category: string; Name: string }) => ({
+    const { Attached, Inventory, Rank } = response.data.data;
+    const attributes = (Attached || [])
+      .map((a: { Category: string; Name: string }) => ({
         key: a.Category,
         value: a.Name,
         kind: "string",
         rank: Rank,
       }))
+      .concat(
+        (Inventory || []).map((a: { Category: string; Name: string }) => ({
+          key: a.Category,
+          value: a.Name,
+          kind: "string",
+          rank: Rank,
+        }))
+      );
+    return {
+      ...metadata,
+      attributes,
+    };
+  } catch (error) {
+    logger.error(
+      "kanpai-pandas-fetcher",
+      JSON.stringify({
+        message: `fetchToken get json error. error:${error}`,
+        contract: metadata.contract,
+        tokenId: metadata.tokenId,
+        error,
+      })
     );
+  }
+
   return {
     ...metadata,
-    attributes,
   };
 };
