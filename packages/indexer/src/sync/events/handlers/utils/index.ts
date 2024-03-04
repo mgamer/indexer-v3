a1a73502acc6ce62f29163e85a58b9823662848c
Idan Peleg <ipeleg@hotmail.com>
2024-03-04 09:17:13 -0500

    feat: update log

--- packages/indexer/src/sync/events/handlers/utils/index.ts
+++ packages/indexer/src/sync/events/handlers/utils/index.ts
@@ -184,6 +184,7 @@ export const processOnChainData = async (data: OnChainData, backfill?: boolean) 
 
   const endPersistOtherEvents = Date.now();
 
+  const startAddingToQueues = Date.now();
   // Trigger further processes:
   // - revalidate potentially-affected orders
   // - store on-chain orders
@@ -200,11 +201,14 @@ export const processOnChainData = async (data: OnChainData, backfill?: boolean) 
       orderbookOrdersJob.addToQueue(data.orders),
     ]);
   }
+  const endAddingToQueues = Date.now();
 
   // Mints and last sales
+  const startAddingToQueuesMintAndLastSales = Date.now();
   await transferUpdatesJob.addToQueue(nonFillTransferEvents);
   await mintQueueJob.addToQueue(data.mintInfos);
   await fillUpdatesJob.addToQueue(data.fillInfos);
+  const endAddingToQueuesMintAndLastSales = Date.now();
 
   const startMintProcess = Date.now();
   if (!backfill) {
@@ -285,6 +289,9 @@ export const processOnChainData = async (data: OnChainData, backfill?: boolean) 
   const endProcessTransferActivityEvent = Date.now();
 
   return {
+    addingToQueues: endAddingToQueues - startAddingToQueues,
+    addingToQueuesMintAndLastSales:
+      endAddingToQueuesMintAndLastSales - startAddingToQueuesMintAndLastSales,
     mintProcess: endMintProcess - startMintProcess,
     // Return the time it took to process each step
     processRecalcOwnerCount: endProcessRecalcOwnerCount - startProcessRecalcOwnerCount,
