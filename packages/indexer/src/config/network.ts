07308dfe832990fb92d2739e61a15e2f37d038ad
Idan Peleg <ipeleg@hotmail.com>
2024-03-05 16:22:37 -0500

    feat: update network

--- packages/indexer/src/config/network.ts
+++ packages/indexer/src/config/network.ts
@@ -1852,6 +1852,9 @@ export const getNetworkSettings = (): NetworkSettings => {
           "0xaf57145e0c09a75ca4a2dc65ac80c91920e537ce",
           "0x32a93c3e9a3be8f0fdd0835aba7299cba3624b13",
         ],
+        coingecko: {
+          networkId: "apex",
+        },
         onStartup: async () => {
           // Insert the native currency
           await Promise.all([
@@ -1884,6 +1887,9 @@ export const getNetworkSettings = (): NetworkSettings => {
         realtimeSyncMaxBlockLag: 32,
         realtimeSyncFrequencySeconds: 5,
         lastBlockLatency: 5,
+        coingecko: {
+          networkId: "blast",
+        },
         onStartup: async () => {
           // Insert the native currency
           await Promise.all([
