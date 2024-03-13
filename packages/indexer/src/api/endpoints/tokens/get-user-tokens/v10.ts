d26f10bfa3d4176398a91f1285aa9097adc976f0
Jaden <jadendurnford@gmail.com>
2024-03-13 17:55:55 -0400

    feat: making getUserTokens v10 active and deprecating v9

--- packages/indexer/src/api/endpoints/tokens/get-user-tokens/v10.ts
+++ packages/indexer/src/api/endpoints/tokens/get-user-tokens/v10.ts
@@ -40,11 +40,10 @@ export const getUserTokensV10Options: RouteOptions = {
   description: "User Tokens",
   notes:
     "Get tokens held by a user, along with ownership information such as associated orders and date acquired.",
-  tags: ["api", "x-deprecated"],
+  tags: ["api", "Tokens"],
   plugins: {
     "hapi-swagger": {
       order: 9,
-      deprecated: true,
     },
   },
   validate: {
