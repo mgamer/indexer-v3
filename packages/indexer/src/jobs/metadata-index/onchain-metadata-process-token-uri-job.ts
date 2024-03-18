8078aa7bf4b1cc33087c2350dcc2ae772d1dbae0
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2024-03-18 12:58:09 -0400

    Ofir/platf 3180 metadata indexing cases (#8317)
    * feat: handle ar
    
    * feat: handle id.json
    
    * feat: handle json uri
    
    * feat: fix usecase
    
    * feat: skip invalid uri
    
    * feat: change invalud uri logic
    
    * feat: handle attributes usecases
    
    * feat: added fallback limit logic
    
    * feat: add try catch
    
    * feat: add try catch
    
    * feat: enable skip

--- packages/indexer/src/jobs/metadata-index/onchain-metadata-process-token-uri-job.ts
+++ packages/indexer/src/jobs/metadata-index/onchain-metadata-process-token-uri-job.ts
@@ -275,7 +275,7 @@ export default class OnchainMetadataProcessTokenUriJob extends AbstractRabbitMqJ
               })
             );
 
-            // return;
+            return;
           }
         }
       } catch (error) {
